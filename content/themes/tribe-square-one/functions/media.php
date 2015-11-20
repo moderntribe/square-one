2214a95f62f56c7ddc3d2cb993f44606a22bccdc
Ryan Urban <ryan@fringewebdesign.com>
2015-11-20 12:34:50 -0500

    Implement some base style guide tweaks

--- content/themes/tribe-square-one/functions/media.php
+++ content/themes/tribe-square-one/functions/media.php
@@ -8,6 +8,9 @@
  */
 
 
+add_filter( 'the_content', 'customize_wp_image_output', 12, 1 );
+
+
 // WP SEO
 if( function_exists( 'wpseo_auto_load' ) ) {
 	add_filter( 'wpseo_opengraph_image_size', 'customize_wpseo_image_size' );
@@ -31,5 +34,35 @@ add_image_size( 'social-share', 1200, 630, true );
 
 function customize_wpseo_image_size( $size ) {
     return 'social-share';
+}
+
+
+/**
+ * Customize WP image output
+ */
+
+function customize_wp_image_output( $html ) {
+
+	$regex = '#((<\s*figure[^>]*?>)(.*?))?((<\s*a\s[^>]*?>)(.*?))?((<\s*img[^>]+)(src\s*=\s*"[^"]+")([^>]+>))((.*?)(</a>))?((.*?)(</figure>))?#i';
+	$html = preg_replace_callback( $regex, 'image_wrap_regex_callback', $html );
+	return $html;
+
+}
+
+function image_wrap_regex_callback( $matches ) {
+
+	$full_match  = $matches[0];
+	$the_figure  = $matches[2];
+	$the_img     = $matches[7];
+	$the_img_src = $matches[9];
+
+	$updated_image = str_replace( $the_img_src, $the_img_src, $the_img );
+
+	if ( empty( $the_figure ) ) {
+		$full_match = sprintf( '<figure class="wp-image-wrap">%s</figure>', $full_match );
+	}
+
+	return str_replace( $the_img, $updated_image, $full_match );
+
 }
 
