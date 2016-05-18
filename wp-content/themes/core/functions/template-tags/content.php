dad7e6cd4d467f7bb305af1c87bae3fd0349e93d
Jonathan Brinley <jbrinley@flightless.us>
2016-05-18 09:45:42 -0400

    move page title logic to core plugin

--- wp-content/themes/core/functions/template-tags/content.php
+++ wp-content/themes/core/functions/template-tags/content.php
@@ -12,54 +12,8 @@
  */
 
 function get_page_title() {
-
-	if ( is_front_page() ) {
-		return '';
-	}
-
-	// Blog
-	if ( is_home() ) {
-		$title = 'Blog';
-	}
-
-	// Category
-	elseif ( is_category() ) {
-		$title = single_cat_title( '', false );
-	}
-
-	// Tags
-	elseif ( is_tag() ) {
-		$title = single_tag_title( '', false );
-	}
-
-	// Tax
-	elseif ( is_tax() ) {
-		$title = single_term_title( '', false );
-	}
-
-	// Post Type Archive
-	elseif ( is_post_type_archive() ) {
-		$title = post_type_archive_title( '', false );
-	}
-
-	// Search
-	elseif ( is_search() ) {
-		$title = 'Search Results';
-	}
-
-	// 404
-	elseif ( is_404() ) {
-		$title = 'Page Not Found (404)';
-	}
-
-	else {
-		$title = get_the_title();
-	}
-
-	if ( ! empty( $title ) ) {
-		return $title;
-	}
-
+	$title = new \Tribe\Project\Theme\Page_Title();
+	return $title->get_title();
 }
 
 
