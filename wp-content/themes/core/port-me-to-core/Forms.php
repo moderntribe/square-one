1c35e1e8148521dce5e8a2a8474cd210cd1d327c
Ryan Urban <ryan@fringewebdesign.com>
2016-07-14 15:22:05 -0400

    Implement input icons & GF other option

--- wp-content/themes/core/port-me-to-core/Forms.php
+++ wp-content/themes/core/port-me-to-core/Forms.php
@@ -9,6 +9,7 @@ class Forms {
 	public function __construct() {
 
 		add_filter( 'gform_enqueue_scripts', [ $this, 'enqueue_gravity_forms_jquery_ui_styles' ] );
+		add_filter( 'gform_field_choice_markup_pre_render', [ $this, 'customize_gf_choice_other' ], 10, 4 );
 		add_filter( 'gform_field_css_class', [ $this, 'add_gf_select_field_class' ], 10, 3 );
 		add_filter( 'gform_pre_render', [ $this, 'deactivate_gf_animations' ] );
 		add_filter( 'gform_confirmation_anchor', '__return_false' );
@@ -29,11 +30,31 @@ class Forms {
 	}
 
 	/**
+	 * Add some custom markup to other option for radio & checkbox controls
+	 * @link https://www.gravityhelp.com/documentation/article/gform_field_choice_markup_pre_render/
+	 */
+
+	public function customize_gf_choice_other( $choice_markup, $choice, $field, $value ) {
+
+	    if ( ( $field->get_input_type() == 'radio' || $field->get_input_type() == 'checkbox' ) && ( rgar( $choice, 'text' ) === 'Other' ) ) {
+	        $index = count( $field['choices'] ) - 1;
+	        return str_replace( '</li>', '<label for="choice_' . $field['formId'] . '_' . $field['id'] . '_' . $index .'" class="gf-radio-checkbox-other-placeholder"><span class="visual-hide">Other</span></label></li>', $choice_markup );
+	    }
+
+	    return $choice_markup;
+
+	}
+
+	/**
 	 * Add a custom class to the Gravity Forms select field
 	 * @link http://www.gravityhelp.com/documentation/page/Gform_field_css_class
 	 */
 
 	public function add_gf_select_field_class( $classes, $field, $form ) {
+
+		$activate_icons     = false;
+		$class_icon_simple  = $activate_icons ? ' form-control-icon' : '';
+		$class_icon_complex = $activate_icons ? ' form-control-icon-complex' : '';
 
 		if( $field['type'] == 'multiselect' || ( $field['type'] == 'post_custom_field' && $field['inputType'] == 'multiselect' ) ) {
 
@@ -62,7 +83,9 @@ class Forms {
 
 		} elseif( $field['type'] == 'date' ) {
 
-			$classes .= ' gf-date gf-date-layout-'. $field['dateType'];
+			$class_date_icon = ( $field['dateType'] === 'datepicker' ) ? $class_icon_simple : '';
+
+			$classes .= ' gf-date gf-date-layout-'. $field['dateType'] . $class_date_icon;
 
 		} elseif( $field['type'] == 'time' ) {
 
@@ -70,23 +93,23 @@ class Forms {
 
 		} elseif( $field['type'] == 'phone' ) {
 
-			$classes .= ' gf-phone';
+			$classes .= ' gf-phone' . $class_icon_simple;
 
 		} elseif( $field['type'] == 'name' ) {
 
-			$classes .= ' gf-name';
+			$classes .= ' gf-name' . $class_icon_complex;
 
 		} elseif( $field['type'] == 'address' ) {
 
-			$classes .= ' gf-address';
+			$classes .= ' gf-address' . $class_icon_complex;
 
 		} elseif( $field['type'] == 'email' ) {
 
-			$classes .= ' gf-email';
+			$classes .= ' gf-email' . $class_icon_simple;
 
 		} elseif( $field['type'] == 'website' ) {
 
-			$classes .= ' gf-url';
+			$classes .= ' gf-url' . $class_icon_simple;
 
 		} elseif( $field['type'] == 'fileupload' ) {
 
