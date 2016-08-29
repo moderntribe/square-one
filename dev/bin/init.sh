ca1d80be7dd8eda73988c2463f4d14a0888a44ce
Patrick Force <patrickforce@gmail.com>
2016-08-29 16:05:18 -0600

    script made simpler by not wiping square-one history and base git config/data from clone

--- dev/bin/init.sh
+++ dev/bin/init.sh
@@ -4,32 +4,10 @@ set -e
 
 cd "$(dirname "$0")/../../"
 
-read -p "Running this script will initialize/re-initialize the repository.  Are you sure you want to continue? [y/n] " -n 1 -r
-echo
-if ! [[ $REPLY =~ ^[Yy]$ ]]
-then
-    exit
-fi
-
 if [ -z "$1" ]; then
     echo "Please provide a new name for this project, like: init.sh new-project-name, it should match the project name on GitHub"
     exit
 fi
-
-# re-add submodules
-git config -f .gitmodules --get-regexp '^submodule\..*\.path$' |
-while read path_key path
-do
-    url_key=$(echo $path_key | sed 's/\.path/.url/')
-    url=$(git config -f .gitmodules --get "$url_key")
-    if [ -d "$path" ]; then
-        rm -rf "$path"
-    fi
-    git submodule add $url $path
-done
-
-git add .
-git commit -m "Initial commit after copy from square-one"
 
 git remote set-url origin "git@github.com:moderntribe/$1.git"
 git remote add upstream "git@github.com:moderntribe/square-one.git"
