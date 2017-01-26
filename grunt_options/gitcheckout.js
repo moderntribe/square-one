950b7560537a660126807ef48e0bc97d2b605be7
Sam Estok <sam@tri.be>
2017-01-25 22:28:30 -0800

    Implements current git deploy system for wpengine

---
+++ grunt_options/gitcheckout.js
@@ -0,0 +1,26 @@
+/**
+ *
+ * Module: grunt-git
+ * Documentation: https://www.npmjs.org/package/grunt-git
+ *
+ *
+ */
+
+module.exports = {
+
+	options: {
+		verbose:true,
+	},
+
+	staging: {
+		options: {
+			branch:'staging',
+		}
+	},
+
+	production: {
+		options: {
+			branch:'production',
+		}
+	}
+};
