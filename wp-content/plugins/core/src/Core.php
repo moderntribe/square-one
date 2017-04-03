e1911a8a3d05ed1bed4f5646505db3f53c084fd0
Jonathan Brinley <jonathanbrinley@gmail.com>
2017-04-03 16:26:03 -0400

    fix copy/paste error
    I was loading the post type service providers twice, rather than loading the appropriate taxonomy service providers.

--- wp-content/plugins/core/src/Core.php
+++ wp-content/plugins/core/src/Core.php
@@ -13,7 +13,9 @@ use Tribe\Project\Service_Providers\Post_Types\Page_Service_Provider;
 use Tribe\Project\Service_Providers\Post_Types\Post_Service_Provider;
 use Tribe\Project\Service_Providers\Post_Types\Sample_Post_Type_Service_Provider;
 use Tribe\Project\Service_Providers\Post_Types\Venue_Service_Provider;
+use Tribe\Project\Service_Providers\Taxonomies\Category_Service_Provider;
 use Tribe\Project\Service_Providers\Taxonomies\Example_Taxonomy_Service_Provider;
+use Tribe\Project\Service_Providers\Taxonomies\Post_Tag_Service_Provider;
 use Tribe\Project\Service_Providers\Theme_Customizer_Provider;
 use Tribe\Project\Service_Providers\Global_Service_Provider;
 use Tribe\Project\Service_Providers\Theme_Provider;
@@ -86,11 +88,8 @@ class Core {
 		$this->container->register( new Example_Taxonomy_Service_Provider() );
 
 		// externally registered taxonomies
-		$this->container->register( new Event_Service_Provider() );
-		$this->container->register( new Organizer_Service_Provider() );
-		$this->container->register( new Page_Service_Provider() );
-		$this->container->register( new Post_Service_Provider() );
-		$this->container->register( new Venue_Service_Provider() );
+		$this->container->register( new Category_Service_Provider() );
+		$this->container->register( new Post_Tag_Service_Provider() );
 	}
 
 	public function container() {
