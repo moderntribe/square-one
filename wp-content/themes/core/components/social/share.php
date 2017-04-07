b9aa3465962664a442b731d27f075ae83ea429bf
Aaron Brazell <aaron@echnosailor.com>
2017-04-07 17:35:01 +0000

    Use esc_html_e() where applicable in context

--- wp-content/themes/core/components/social/share.php
+++ wp-content/themes/core/components/social/share.php
@@ -1,6 +1,6 @@
 <aside class="social-share">
 
-	<h6 class="social-share__title"><?php esc_attr_e( 'Share This', 'tribe' ); ?></h6>
+	<h6 class="social-share__title"><?php esc_html_e( 'Share This', 'tribe' ); ?></h6>
 
 	<?php
 	$social = new \Tribe\Project\Theme\Social_Links( [ 'facebook', 'twitter', 'google', 'linkedin', 'email' ], false );
--- wp-content/themes/core/content/search/searchform.php
+++ wp-content/themes/core/content/search/searchform.php
@@ -1,6 +1,6 @@
 <form role="search" method="get" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 	
-	<label for="s"><?php esc_attr_e( 'Search', 'tribe' ); ?></label>
+	<label for="s"><?php esc_html_e( 'Search', 'tribe' ); ?></label>
 	<input type="text" name="s" />
 	<input type="submit" name="submit" value="<?php esc_attr_e( 'Search', 'tribe' ); ?>" />
 
