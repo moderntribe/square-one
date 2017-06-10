d74c4677b0f0a358daa1ed9222729f9e22c6f7e3
Jonathan Brinley <jbrinley@flightless.us>
2017-06-10 18:26:29 -0400

    escape URLs/attributes, remove unnecessary data for unsupported browser template

--- wp-content/plugins/core/src/Templates/Unsupported_Browser.php
+++ wp-content/plugins/core/src/Templates/Unsupported_Browser.php
@@ -8,7 +8,6 @@ class Unsupported_Browser extends Base {
 	public function get_data(): array {
 		$data                     = parent::get_data();
 		$data['post']             = $this->get_post();
-		$data['comments']         = $this->get_comments();
 		$data['css']              = $this->get_css();
 		$data['favicon']          = $this->get_favicon();
 		$data['legacy_image_url'] = trailingslashit( get_template_directory_uri() ) . 'img/logos/logo-legacy.png';
@@ -20,29 +19,7 @@ class Unsupported_Browser extends Base {
 		the_post();
 		return [
 			'content'        => apply_filters( 'the_content', get_the_content() ),
-			'permalink'      => get_the_permalink(),
-			'featured_image' => $this->get_featured_image(),
 		];
-	}
-
-	protected function get_featured_image() {
-		$options = [
-			'wrapper_class' => 'page__image',
-			'echo'          => false,
-		];
-
-		return the_tribe_image( get_post_thumbnail_id(), $options );
-	}
-
-	protected function get_comments() {
-		if ( comments_open() || get_comments_number() > 0 ) {
-			ob_start();
-			comments_template();
-
-			return ob_get_clean();
-		}
-
-		return '';
 	}
 
 	protected function get_favicon() {
