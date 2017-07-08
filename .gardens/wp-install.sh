c0b2d47d0ba8dbe48a6840e6632bb8ae2fa35741
Patrick Force <patrickforce@gmail.com>
2017-07-07 19:57:21 -0600

    replace PRIMARY_DOMAIN on build

--- .gardens/wp-install.sh
+++ .gardens/wp-install.sh
@@ -2,6 +2,7 @@
 
 multisite_enabled="$1"
 multisite_use_subdomains="$2"
+domain="$3"
 
 multisite_subdomains_flag=""
 if $multisite_use_subdomains; then
@@ -11,6 +12,7 @@ fi
 cd /srv/site/wp
 if ! $(wp --allow-root core is-installed); then
   if $multisite_enabled; then
+    sed -i 's/%%PRIMARY_DOMAIN%%/'"${domain}"'/g' /srv/site/wp-config.php
     wp --allow-root core multisite-install --admin_user=$admin_user --admin_password=$admin_password --admin_email=$admin_email --title=$title --url=$url --skip-email
   else
     wp --allow-root core install --admin_user=$admin_user --admin_password=$admin_password --admin_email=$admin_email --title=$title --url=$url --skip-email "$multisite_subdomains_flag"
