3a6100d7aecb3142f0dd07d01c9ae6670b95d3da
Patrick Force <patrickforce@gmail.com>
2017-07-13 12:24:49 -0600

    update the way we set domain_current_site

--- .gardens/wp-install.sh
+++ .gardens/wp-install.sh
@@ -7,7 +7,7 @@ domain="$2"
 cd /srv/site/wp
 if ! $(wp --allow-root core is-installed); then
   if $multisite_enabled; then
-    sed -i 's/%%PRIMARY_DOMAIN%%/'"${domain}"'/g' /srv/site/wp-config.php
+    grep -q "^DOMAIN_CURRENT_SITE" /srv/site/.env && perl -pi -e 's/^DOMAIN_CURRENT_SITE.*/DOMAIN_CURRENT_SITE='"${domain}"'/' /srv/site/.env || printf "\nDOMAIN_CURRENT_SITE=${domain}" >> /srv/site/.env
     wp --allow-root core multisite-install --admin_user=$admin_user --admin_password=$admin_password --admin_email=$admin_email --title=$title --url=$url --skip-email
   else
     wp --allow-root core install --admin_user=$admin_user --admin_password=$admin_password --admin_email=$admin_email --title=$title --url=$url --skip-email
