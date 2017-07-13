9221c7730c4b03549edf69382827700bc92ca87c
Patrick Force <patrickforce@gmail.com>
2017-07-13 11:55:59 -0600

    move root autoload to wp-config

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php
 
-require_once __DIR__ . "/vendor/autoload.php";
-
 // WordPress view bootstrapper
 define( 'WP_USE_THEMES', true );
 require( './wp/wp-blog-header.php' );
--- wp-config.php
+++ wp-config.php
@@ -5,6 +5,8 @@
  * @link https://github.com/markjaquith/WordPress-Skeleton
  */
 
+require_once __DIR__ . "/vendor/autoload.php";
+
 $dotenv = new Dotenv\Dotenv(__DIR__);
 $dotenv->load();
 
