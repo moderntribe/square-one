5794238ab88468513e227d725766eda56c995569
Nicholas Smith <nicholaswebdev@gmail.com>
2017-08-07 12:31:39 -0500

    interstitial updates

--- wp-content/plugins/core/src/Templates/Content/Panels/Interstitial.php
+++ wp-content/plugins/core/src/Templates/Content/Panels/Interstitial.php
@@ -15,27 +15,7 @@ class Interstitial extends Panel {
 		return $data;
 	}
 
-	protected function get_mapped_panel_data(): array {
-
-		$data = [
-			'wrapper_classes'      => $this->get_panel_classes(),
-			'image'                => $this->get_panel_image(),
-			'image_classes'        => $this->get_image_classes(),
-			'card_content_classes' => $this->get_card_content_classes(),
-			'description'          => $this->panel_vars[ Interstice::FIELD_DESCRIPTION ],
-			'desc_classes'         => $this->get_description_classes(),
-			'desc_attrs'           => $this->get_card_desc_attrs(),
-			'title'                => $this->get_panel_title(),
-			'url'                  => esc_url( $this->panel_vars[ Interstice::FIELD_CTA ]['url'] ),
-			'label'                => esc_html( $this->panel_vars[ Interstice::FIELD_CTA ]['label'] ),
-			'target'               => esc_attr( $this->panel_vars[ Interstice::FIELD_CTA ]['target'] ),
-			'cta_classes'          => $this->get_cta_classes(),
-		];
-
-		return $data;
-	}
-
-	protected function get_panel_image() {
+	protected function get_image() {
 
 		if ( empty( $this->panel_vars[ Interstice::FIELD_IMAGE ] ) ) {
 			return false;
@@ -51,7 +31,7 @@ class Interstitial extends Panel {
 		return the_tribe_image( $this->panel_vars[ Interstice::FIELD_IMAGE ], $options );
 	}
 
-	protected function get_panel_classes() {
+	protected function get_layout() {
 
 		$classes = [];
 
@@ -66,71 +46,22 @@ class Interstitial extends Panel {
 		return implode( ' ', $classes );
 	}
 
-	protected function get_panel_title() {
+	protected function get_title() {
 
 		return the_panel_title( esc_html( $this->panel_vars[ Interstice::FIELD_TITLE ] ), 'c-text__title', 'title', true, 0, 0 );
 	}
 
-	protected function get_image_classes() {
+	public function get_mapped_panel_data(): array {
+		$data = [
+			'image'       => $this->get_image(),
+			'title'       => $this->get_title(),
+			'description' => ! empty( $this->panel_vars[ Interstice::FIELD_DESCRIPTION ] ) ? $this->panel_vars[ Interstice::FIELD_DESCRIPTION ] : false,
+			'btn_url'     => esc_url( $this->panel_vars[ Interstice::FIELD_CTA ]['url'] ),
+			'btn_target'  => esc_attr( $this->panel_vars[ Interstice::FIELD_CTA ]['target'] ),
+			'btn_label'   => esc_attr( $this->panel_vars[ Interstice::FIELD_CTA ]['label'] ),
+			'layout'      => $this->get_layout(),
+		];
 
-		$classes = [ 'c-image' ];
-
-		if ( ! empty( Interstice::NAME ) ) {
-			$classes[] = '';
-		}
-
-		return implode( ' ', $classes );
-	}
-
-	protected function get_card_content_classes() {
-
-		$classes = [ 'c-card__content' ];
-
-		if ( ! empty( Interstice::NAME ) ) {
-			$classes[] = '';
-		}
-
-		return implode( ' ', $classes );
-	}
-
-	protected function get_title_classes() {
-
-		$classes = [ 'c-title' ];
-
-		if ( ! empty( Interstice::NAME ) ) {
-			$classes[] = '';
-		}
-
-		return implode( ' ', $classes );
-	}
-
-	protected function get_description_classes() {
-
-		$classes = [ 'c-description' ];
-
-		if ( ! empty( Interstice::NAME ) ) {
-			$classes[] = '';
-		}
-
-		return implode( ' ', $classes );
-	}
-
-	protected function get_card_desc_attrs() {
-		$card_desc_attrs = sprintf( 'data-depth="0" data-name="%s" data-autop="true" data-livetext',
-			esc_attr( Interstice::FIELD_DESCRIPTION )
-		);
-
-		return $card_desc_attrs;
-	}
-
-	protected function get_cta_classes() {
-
-		$classes = [ 'c-cta' ];
-
-		if ( ! empty( Interstice::NAME ) ) {
-			$classes[] = '';
-		}
-
-		return implode( ' ', $classes );
+		return $data;
 	}
 }
