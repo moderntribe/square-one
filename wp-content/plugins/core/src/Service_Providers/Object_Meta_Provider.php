db60746e63accdab8998ac9109ed74dd94c30e17
Jonathan Brinley <jbrinley@flightless.us>
2017-08-10 22:19:14 -0400

    refactor meta registration
    • Avoid magic strings
    • Delay instantiation until required
    • More flexible file system

--- wp-content/plugins/core/src/Service_Providers/Object_Meta_Provider.php
+++ wp-content/plugins/core/src/Service_Providers/Object_Meta_Provider.php
@@ -7,51 +7,49 @@ namespace Tribe\Project\Service_Providers;
 use Pimple\Container;
 use Pimple\ServiceProviderInterface;
 use Tribe\Libs\Object_Meta\Meta_Repository;
+use Tribe\Project\Object_Meta\Example;
 use Tribe\Project\Post_Types;
-use Tribe\Project\Taxonomies;
 use Tribe\Project\Settings;
+use Tribe\Project\Taxonomies;
 
 class Object_Meta_Provider implements ServiceProviderInterface {
+	const REPO    = 'object_meta.collection_repo';
+	const EXAMPLE = 'object_meta.example';
 
-	protected $field_groups = [];
+	private $keys = [
+		self::EXAMPLE,
+	];
 
-	public function __construct() {
-		$this->field_groups = [
-			'Example' => [
+	public function register( Container $container ) {
+		$this->example( $container );
+
+		$container[ self::REPO ] = function ( Container $container ) {
+			$meta_repo = array_map( function ( $key ) use ( $container ) {
+				return $container[ $key ];
+			}, $this->keys );
+
+			return new Meta_Repository( $meta_repo );
+		};
+
+		add_action( 'init', function () use ( $container ) {
+			$container[ self::REPO ]->hook();
+		} );
+
+		add_action( 'acf/init', function () use ( $container ) {
+			array_walk( $this->keys, function ( $key ) use ( $container ) {
+				$container[ $key ]->register_group();
+			} );
+		}, 10, 0 );
+	}
+
+	private function example( Container $container ) {
+		$container[ self::EXAMPLE ] = function ( Container $container ) {
+			return new Example( [
 				'post_types'     => [ Post_Types\Page\Page::NAME, Post_Types\Post\Post::NAME ],
 				'taxonomies'     => [ Taxonomies\Category\Category::NAME ],
 				'settings_pages' => [ Settings\General::instance()->get_slug() ],
 				'users'          => true,
-			],
-		];
-	}
-
-	public function register( Container $container ) {
-
-		$meta_repo = [];
-
-		foreach ( $this->field_groups as $group => $object_types ) {
-
-			$key                  = 'object_meta.' . strtolower( $group );
-			$object_meta_class_name = '\\Tribe\\Project\\Object_Meta\\' . $group;
-
-			$container[ $key ] = function ( $container ) use ( $object_meta_class_name, $object_types ) {
-				return new $object_meta_class_name( $object_types );
-			};
-
-			$meta_repo[] = $container[ $key ];
-
-			add_action( 'acf/init', function() use ( $container, $key ) {
-				$container[ $key ]->register_group();
-			}, 10, 0 );
-		}
-
-		$container['object_meta.collection_repo'] = function ( Container $container ) use ( $meta_repo ) {
-			return new Meta_Repository( $meta_repo );
+			] );
 		};
-
-		add_action( 'init', function() use ( $container ) {
-			$container['object_meta.collection_repo']->hook();
-		});
 	}
 }
