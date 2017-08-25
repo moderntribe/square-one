094d3ed3421389ae403022ac5e88f57e456a4538
Jonathan Brinley <jbrinley@flightless.us>
2017-08-25 14:28:42 -0400

    set target domain name directly in db before running wp-cli

--- dev/docker/local_domain_migration.sh
+++ dev/docker/local_domain_migration.sh
@@ -15,5 +15,9 @@ SOURCE_DOMAIN=$(docker exec tribe-mysql \
  -Ne "SELECT option_value FROM ${DB_PREFIX}options WHERE option_name='siteurl'" \
  | cut -d'/' -f3)
 
+docker exec tribe-mysql \
+ mysql -uroot -ppassword $DB_NAME \
+ -Ne "UPDATE ${DB_PREFIX}options SET option_value=REPLACE( 'option_value', '$SOURCE_DOMAIN', '$TARGET_DOMAIN' ) WHERE option_name='siteurl'"
+
 /bin/bash $SCRIPTDIR/wp.sh \
  search-replace "$SOURCE_DOMAIN" "$TARGET_DOMAIN" --all-tables-with-prefix --verbose
