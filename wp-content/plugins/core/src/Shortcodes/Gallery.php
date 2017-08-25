aca2b8124556ef53824b54d1bd6da540ce7293be
speerface <aaron@tri.be>
2017-08-25 09:28:16 -0500

    Update gallery shortcode to just return IDs from attachments

--- wp-content/plugins/core/src/Shortcodes/Gallery.php
+++ wp-content/plugins/core/src/Shortcodes/Gallery.php
@@ -26,11 +26,9 @@ class Gallery implements Shortcode {
 			return '';
 		}
 
-		$ids = wp_list_pluck( $attachments, 'ID' );
-
 		$options = [
-			Slider::SLIDES          => $this->get_slides( $ids ),
-			Slider::THUMBNAILS      => $this->get_slides( $ids, 'thumbnail' ),
+			Slider::SLIDES          => $this->get_slides( $attachments ),
+			Slider::THUMBNAILS      => $this->get_slides( $attachments, 'thumbnail' ),
 			Slider::SHOW_CAROUSEL   => $atts['show_carousel'],
 			Slider::SHOW_ARROWS     => $atts['show_arrows'],
 			Slider::SHOW_PAGINATION => $atts['show_pagination'],
@@ -46,19 +44,15 @@ class Gallery implements Shortcode {
 		$id = (int) $atts['id'];
 
 		if ( ! empty( $atts['include'] ) ) {
-			$_attachments = get_posts( [
+			$attachments = get_posts( [
 				'include'        => $atts['include'],
 				'post_status'    => 'inherit',
 				'post_type'      => 'attachment',
 				'post_mime_type' => 'image',
 				'order'          => $atts['order'],
 				'orderby'        => $atts['orderby'],
+				'fields'         => 'ids',
 			] );
-
-			$attachments = [];
-			foreach ( $_attachments as $key => $val ) {
-				$attachments[ $val->ID ] = $_attachments[ $key ];
-			}
 		} elseif ( ! empty( $atts['exclude'] ) ) {
 			$attachments = get_children( [
 				'post_parent'    => $id,
@@ -68,6 +62,7 @@ class Gallery implements Shortcode {
 				'post_mime_type' => 'image',
 				'order'          => $atts['order'],
 				'orderby'        => $atts['orderby'],
+				'fields'         => 'ids',
 			] );
 		} else {
 			$attachments = get_children( [
@@ -77,6 +72,7 @@ class Gallery implements Shortcode {
 				'post_mime_type' => 'image',
 				'order'          => $atts['order'],
 				'orderby'        => $atts['orderby'],
+				'fields'         => 'ids',
 			] );
 		}
 
