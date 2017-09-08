f89749c04bcd27e1306b1b5667c0edabdb18e2e2
Ryan Urban <ryan@fringewebdesign.com>
2017-09-08 11:47:48 -0400

    Implement first pass on global legacy browser page template, styles, and vars

--- wp-content/plugins/core/src/Templates/Unsupported_Browser.php
+++ wp-content/plugins/core/src/Templates/Unsupported_Browser.php
@@ -10,6 +10,8 @@ class Unsupported_Browser extends Base {
 		$data['post']                        = $this->get_post();
 		$data['css']                         = $this->get_css();
 		$data['favicon']                     = $this->get_favicon();
+		$data['legacy_browser_title']        = $this->get_legacy_page_title();
+		$data['legacy_browser_content']      = $this->get_legacy_page_content();
 		$data['legacy_logo_header']          = tribe_assets_url( 'theme/img/legacy-browser/logo-header.png' );
 		$data['legacy_logo_footer']          = tribe_assets_url( 'theme/img/legacy-browser/logo-footer.png' );
 		$data['legacy_browser_icon_chrome']  = tribe_assets_url( 'theme/img/legacy-browser/chrome.png' );
@@ -33,5 +35,17 @@ class Unsupported_Browser extends Base {
 		$css_legacy = ( ! defined( 'SCRIPT_DEBUG' ) || SCRIPT_DEBUG === false ) ? 'dist/legacy.min.css' : 'legacy.css';
 
 		return $css_dir . $css_legacy;
+	}
+
+	protected function get_legacy_page_title() {
+		return sprintf( '%s %s', __(  'Welcome to', 'tribe' ), get_bloginfo( 'name' ) );
+	}
+
+	protected function get_legacy_page_content() {
+		return sprintf(
+			'%s <a href="http://browsehappy.com/" rel="external">%s</a>.',
+			__( 'You are viewing this site in a browser that is no longer supported or secure. For the best possible experience, we recommend that you', 'tribe' ),
+			__( 'update or use a modern browser', 'tribe' )
+		);
 	}
 }
