e1e081655b5211334907d1622a4492a512fbc3b5
Daniel Dvorkin <daniel@tri.be>
2017-09-25 21:59:42 -0300

    Add `wp pimple dump` command

--- wp-content/plugins/core/src/CLI/Pimple_Dump.php
+++ wp-content/plugins/core/src/CLI/Pimple_Dump.php
@@ -3,6 +3,7 @@
 namespace Tribe\Project\CLI;
 
 use Pimple\Container;
+use JBZoo\PimpleDumper\PimpleDumper;
 
 class Pimple_Dump extends \WP_CLI_Command {
 
@@ -11,6 +12,23 @@ class Pimple_Dump extends \WP_CLI_Command {
 	public function __construct( Container $container ) {
 		$this->container = $container;
 		parent::__construct();
+	}
+
+	/**
+	 * Dumps the files needed to autocomplete Pimple names
+	 * to be able to use this plugin: https://plugins.jetbrains.com/plugin/7809-silex-pimple-plugin
+	 *
+	 * ## EXAMPLES
+	 *
+	 *     wp pimple dump
+	 *
+	 * @when after_wp_load
+	 */
+	public function dump() {
+		$dumper = new PimpleDumper();
+		$dumper->dumpPimple( $this->container );
+		$dumper->dumpPhpstorm( $this->container );
+		\WP_CLI::success( 'Done!' );
 	}
 
 }
