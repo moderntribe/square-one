bbdad58efe7a6eb6130a9d2a49aba3fbab6ef474
Gary Kovar <gkovar@gmail.com>
2017-10-03 08:27:19 -0400

    registers the s1 command

--- wp-content/plugins/core/src/Service_Providers/CLI_Provider.php
+++ wp-content/plugins/core/src/Service_Providers/CLI_Provider.php
@@ -5,6 +5,7 @@ namespace Tribe\Project\Service_Providers;
 
 use Pimple\Container;
 use Pimple\ServiceProviderInterface;
+use Tribe\Project\CLI\CPT_Generator;
 use Tribe\Project\CLI\Pimple_Dump;
 
 class CLI_Provider implements ServiceProviderInterface {
@@ -14,6 +15,9 @@ class CLI_Provider implements ServiceProviderInterface {
 		$container['cli.pimple_dump'] = function ( $container ) {
 			return new Pimple_Dump( $container );
 		};
+		$container['cli.cpt-generator'] = function ( $container ) {
+			return new CPT_Generator( $container );
+		};
 
 		add_action( 'init', function () use ( $container ) {
 			if ( ! defined( 'WP_CLI' ) || ! WP_CLI ) {
@@ -21,7 +25,7 @@ class CLI_Provider implements ServiceProviderInterface {
 			}
 
 			\WP_CLI::add_command( 'pimple', $container['cli.pimple_dump'] );
-
+			\WP_CLI::add_command( 's1', $container['cli.cpt-generator'] );
 		}, 0, 0 );
 	}
 }
