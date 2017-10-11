67303e042300a0b64d00624588c42d61cf234d8c
Nicholas Smith <nicholaswebdev@gmail.com>
2017-10-11 14:08:34 -0500

    Add search component controller and twig template

---
+++ wp-content/plugins/core/src/Templates/Components/Search.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Tribe\Project\Templates\Components;
+
+class Search extends Component {
+
+	const TEMPLATE_NAME = 'components/search.twig';
+
+	const FORM_CLASSES  = 'form_classes';
+	const FORM_ATTRS    = 'form_attrs';
+	const LABEL_CLASSES = 'label_classes';
+	const LABEL_ATTRS   = 'label_attrs';
+	const INPUT_CLASSES = 'input_classes';
+	const INPUT_ATTRS   = 'input_attrs';
+
+	protected function parse_options( array $options ): array {
+		$defaults = [
+			static::FORM_CLASSES  => [],
+			static::FORM_ATTRS    => [],
+			static::LABEL_CLASSES => [],
+			static::LABEL_ATTRS   => [],
+			static::INPUT_CLASSES => [],
+			static::INPUT_ATTRS   => [],
+		];
+
+		return wp_parse_args( $options, $defaults );
+	}
+
+	public function get_data(): array {
+		$data = [
+			static::FORM_CLASSES  => $this->merge_classes( [], $this->options[ static::FORM_CLASSES ], true ),
+			static::FORM_ATTRS    => $this->merge_attrs( [], $this->options[ self::FORM_ATTRS ], true ),
+			static::LABEL_CLASSES => $this->merge_classes( [], $this->options[ static::LABEL_CLASSES ], true ),
+			static::LABEL_ATTRS   => $this->merge_attrs( [], $this->options[ self::LABEL_ATTRS ], true ),
+			static::INPUT_CLASSES => $this->merge_classes( [], $this->options[ static::INPUT_CLASSES ], true ),
+			static::INPUT_ATTRS   => $this->merge_attrs( [], $this->options[ self::INPUT_ATTRS ], true ),
+		];
+
+		return $data;
+	}
+}
