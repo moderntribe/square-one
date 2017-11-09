a12cdc9178ee5bfa7f9a74bad751d55256032a45
Gary Kovar <gkovar@gmail.com>
2017-11-09 10:06:17 -0500

    creates method to parse assoc args and to set post type

--- wp-content/plugins/core/src/CLI/Taxonomy_Generator.php
+++ wp-content/plugins/core/src/CLI/Taxonomy_Generator.php
@@ -2,8 +2,22 @@
 
 namespace Tribe\Project\CLI;
 
+use Tribe\Project\Post_Types\Page\Page;
+use Tribe\Project\Post_Types\Post\Post;
+
 class Taxonomy_Generator extends Square_One_Command {
 	use File_System;
+
+	const POST_TYPES = [
+		Post::NAME,
+		Page::NAME,
+	];
+
+	protected $slug               = '';
+	protected $class_name         = '';
+	protected $namespace          = '';
+	protected $taxonomy_directory = '';
+	protected $assoc_args         = [];
 
 	public function command() {
 		return 'tax';
@@ -54,6 +68,49 @@ class Taxonomy_Generator extends Square_One_Command {
 	}
 
 	public function taxonomy( $args, $assoc_args ) {
+		$this->setup( $args, $assoc_args );
 
+		$this->create_taxonomy_directory();
 	}
+
+	protected function setup( $args, $assoc_args ) {
+		$this->slug = $this->sanitize_slug( $args );
+
+		$this->class_name = ucfirst( $this->slug );
+		$this->namespace  = 'Tribe\Project\Taxonomies\\' . $this->class_name;
+
+		$this->assoc_args = $this->parse_assoc_args( $assoc_args );
+	}
+
+	private function sanitize_slug( $args ) {
+		list( $slug ) = $args;
+
+		return sanitize_title( $slug );
+	}
+
+	private function parse_assoc_args( $assoc_args ) {
+		$defaults = [
+			'single'     => ucfirst( $this->slug ),
+			'plural'     => ucfirst( $this->slug ) . 's',
+			'config'     => true,
+			'post_types' => $this->get_post_types( $assoc_args ),
+		];
+
+		return wp_parse_args( $assoc_args, $defaults );
+	}
+
+	private function get_post_types( $assoc_args ) {
+		if ( ! isset( $assoc_args['post_types'] ) ) {
+			return self::POST_TYPES;
+		}
+
+		return explode( ',', $assoc_args['post_types'] );
+	}
+
+	private function create_taxonomy_directory() {
+		$directory = trailingslashit( dirname( __DIR__ ) ) . 'Taxonomies/' . ucfirst( $this->slug );
+		$this->taxonomy_directory = $directory;
+		$this->create_directory( $directory );
+	}
+
 }
