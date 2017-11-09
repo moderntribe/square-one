ba095c9623abd13e8abedfe7e25170c9f6518b17
Gary Kovar <gkovar@gmail.com>
2017-11-09 09:46:09 -0500

    Sets up the arguments

--- wp-content/plugins/core/src/CLI/Taxonomy_Generator.php
+++ wp-content/plugins/core/src/CLI/Taxonomy_Generator.php
@@ -6,18 +6,54 @@ class Taxonomy_Generator extends Square_One_Command {
 	use File_System;
 
 	public function command() {
-		// TODO: Implement command() method.
+		return 'tax';
 	}
 
 	public function callback() {
-		// TODO: Implement callback() method.
+		return [ $this, 'taxonomy' ];
 	}
 
 	public function description() {
-		// TODO: Implement description() method.
+		return 'Generates a Taxonomy';
 	}
 
 	public function arguments() {
-		// TODO: Implement arguments() method.
+		return [
+			[
+				'type'        => 'positional',
+				'name'        => 'taxonomy',
+				'optional'    => false,
+				'description' => 'The name of the Taxonomy.',
+			],
+			[
+				'type'        => 'generic',
+				'name'        => 'post_types',
+				'optional'    => true,
+				'description' => 'Comma seperated list of post types to register this taxonomy to.',
+			],
+			[
+				'type'        => 'flag',
+				'name'        => 'config',
+				'optional'    => true,
+				'description' => 'Whether or not to create a config file by default. Defaults to true, pass --no-config if you don\'t need one.',
+				'default'     => true,
+			],
+			[
+				'type'        => 'generic',
+				'name'        => 'single',
+				'optional'    => true,
+				'description' => 'Singular taxonomy.',
+			],
+			[
+				'type'        => 'generic',
+				'name'        => 'plural',
+				'optional'    => true,
+				'description' => 'Plural taxonomy.',
+			],
+		];
+	}
+
+	public function taxonomy( $args, $assoc_args ) {
+
 	}
 }
