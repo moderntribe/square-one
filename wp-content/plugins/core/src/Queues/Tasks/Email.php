e49582b33332712958c79e9ae056dcf2058db922
Gary Kovar <gkovar@gmail.com>
2017-11-22 10:19:13 -0500

    builds out wp_mail task

--- wp-content/plugins/core/src/Queues/Tasks/Email.php
+++ wp-content/plugins/core/src/Queues/Tasks/Email.php
@@ -7,10 +7,12 @@ use Tribe\Project\Queues\Contracts\Task;
 class Email implements Task {
 
 	public function handle( array $args ) {
-		wp_mail( ... );
+		wp_mail( $args['to'], $args['subject'], $args['message'], $args['headers'], $args['attachments'] );
 	}
 
-	public static function mail( $args, $queue = 'default' ) {
-
+	public static function mail( $args, $queue_name = 'default' ) {
+		if( $queue = Queue::get_instance( $queue_name ) ) {
+			$queue->dispatch( self::class, $args );
+		}
 	}
 }
