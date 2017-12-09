3ffa2a96763c65dc5f3276ffa97332b43fc1d00a
Jonathan Brinley <jbrinley@flightless.us>
2017-12-08 21:44:19 -0500

    add the word 'title' to the 'enter_title_here' string

--- wp-content/plugins/core/assets/templates/cli/post_type/config.php
+++ wp-content/plugins/core/assets/templates/cli/post_type/config.php
@@ -8,7 +8,7 @@ class Config extends Post_Type_Config {
 	public function get_args() {
 		return [
 			'hierarchical'     => false,
-			'enter_title_here' => __( '%2$s', 'tribe' ),
+			'enter_title_here' => __( '%2$s title', 'tribe' ),
 			'menu_icon'        => 'dashicons-warning',
 			'map_meta_cap'     => true,
 			'supports'         => [ 'title', 'editor', 'author', 'thumbnail', 'page-attributes', 'excerpt', 'revisions', ],
