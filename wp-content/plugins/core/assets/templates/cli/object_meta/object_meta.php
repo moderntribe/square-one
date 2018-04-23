620727e4fac1e06c971f06a322ddf378c43df2e1
Gary Kovar <gkovar@gmail.com>
2018-04-23 15:42:10 -0400

    moves around the template so we can build constants last

--- wp-content/plugins/core/assets/templates/cli/object_meta/object_meta.php
+++ wp-content/plugins/core/assets/templates/cli/object_meta/object_meta.php
@@ -8,22 +8,22 @@ class %1$s extends ACF\ACF_Meta_Group {
 
 	const NAME = '%2$s';
 
-%3$s
+%7$s
 
 	public function get_keys() {
 		return [
-%4$s    ];
+%3$s    ];
 	}
 
 	public function get_group_config() {
 		$group = new ACF\Group( self::NAME, $this->object_types );
-		$group->set( 'title', __( '%5$s', 'tribe' ) );
+		$group->set( 'title', __( '45$s', 'tribe' ) );
 
-		%6$s
+		%5$s
 
 		return $group->get_attributes();
 	}
 
-%7$s
+%6$s
 
 }
