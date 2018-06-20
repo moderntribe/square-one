63ae87587e3bd7f22e1b5e0e596f7ca40eb8cb83
Aaron Brazell <aaronb@tri.be>
2018-06-20 09:32:47 -0400

    Handle exceptions for wp_mail supplied by third parties that don't return a boolean

--- wp-content/plugins/core/src/Queues/Tasks/Email.php
+++ wp-content/plugins/core/src/Queues/Tasks/Email.php
@@ -6,8 +6,22 @@ use Tribe\Project\Queues\Contracts\Task;
 
 class Email implements Task {
 
+	/**
+	 * The wp_mail() function is pluggable and not all third parties respect the core signature that
+	 * returns a boolean. So we exception handle to ensure an expected return.
+	 * 
+	 * @param array $args
+	 *
+	 * @return bool
+	 */
 	public function handle( array $args ) : bool {
-		return wp_mail( $args['to'], $args['subject'], $args['message'], $args['headers'], $args['attachments'] );
+		try {
+			wp_mail( $args['to'], $args['subject'], $args['message'], $args['headers'], $args['attachments'] );
+		} catch ( \Exception $e ) {
+			return false;
+		}
+
+		return true;
 	}
 
 }
