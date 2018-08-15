8af7633c3d589c1175d48e271423cb91b63c1259
jfrydman <justin.frydman@gmail.com>
2018-08-14 19:29:45 -0600

    Remove path from wp calls (not sure why this wasn't working before, but works fine now)

--- dev/docker/wpx.sh
+++ dev/docker/wpx.sh
@@ -16,10 +16,9 @@
 # cd dev/docker
 # ./exec.sh /application/www/dev/docker/wpx.sh <command>
 
-APP="/application/www"
 WP_CLI="/usr/local/bin/wp"
 SCRIPTDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd "$SCRIPTDIR";
 PROJECT_ID=$(cat ./.projectID)
 
-PHP_IDE_CONFIG="serverName=${PROJECT_ID}.tribe" php -dxdebug.remote_autostart=1 -dxdebug.remote_host=host.tribe -dxdebug.remote_enable=1 ${WP_CLI} --allow-root --path=${APP} "$@"
+PHP_IDE_CONFIG="serverName=${PROJECT_ID}.tribe" php -dxdebug.remote_autostart=1 -dxdebug.remote_host=host.tribe -dxdebug.remote_enable=1 ${WP_CLI} --allow-root "$@"
