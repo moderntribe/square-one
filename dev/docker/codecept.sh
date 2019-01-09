7167e4c900585ed567b32be79ea26f326ee74fdd
Aaron Brazell <aaronb@tri.be>
2019-01-09 17:02:59 -0500

    consolidate composer

--- dev/docker/codecept.sh
+++ dev/docker/codecept.sh
@@ -30,4 +30,4 @@ cd "../../dev/tests";
 
 echo $PATH;
 
-PHP_IDE_CONFIG="serverName=${PROJECT_ID}.tribe" php -dxdebug.remote_autostart=1 -dxdebug.remote_host=host.tribe -dxdebug.remote_enable=1 vendor/bin/codecept "$@"
+PHP_IDE_CONFIG="serverName=${PROJECT_ID}.tribe" php -dxdebug.remote_autostart=1 -dxdebug.remote_host=host.tribe -dxdebug.remote_enable=1 ../../vendor/bin/codecept "$@"
