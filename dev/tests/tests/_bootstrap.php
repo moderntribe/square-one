616e99df53a47d93efec4bff6491eddcaf4ca1be
Aaron Brazell <aaronb@tri.be>
2019-01-09 15:52:30 -0500

    Move function mocking init to unit testing bootstrap

--- dev/tests/tests/_bootstrap.php
+++
@@ -1,6 +0,0 @@
-<?php
-// This is global bootstrap for autoloading
-
-use tad\FunctionMocker\FunctionMocker;
-
-FunctionMocker::init();
--- dev/tests/tests/unit/_bootstrap.php
+++ dev/tests/tests/unit/_bootstrap.php
@@ -1,3 +1,6 @@
 <?php
+use tad\FunctionMocker\FunctionMocker;
 
 \Codeception\Util\Autoload::addNamespace('Tribe\\Project', __DIR__ . '/../../../../wp-content/plugins/core/src');
+
+FunctionMocker::init();
