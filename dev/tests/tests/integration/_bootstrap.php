95434ba52d5607066444c850844c60ce49249c43
Aaron Brazell <aaronb@tri.be>
2019-01-09 17:10:11 -0500

    Fix include path for wp cli

--- dev/tests/tests/integration/_bootstrap.php
+++ dev/tests/tests/integration/_bootstrap.php
@@ -2,4 +2,4 @@
 // Here you can initialize variables that will be available to your tests
 
 // WP-CLI classes are not autoloaded if not running a WP-CLI command so we load it here
-include_once( __DIR__ . '/../../vendor/wp-cli/wp-cli/php/class-wp-cli-command.php');
+include_once( __DIR__ . '/../../../../vendor/wp-cli/wp-cli/php/class-wp-cli-command.php');
