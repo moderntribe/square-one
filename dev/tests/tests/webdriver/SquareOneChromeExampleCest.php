3e059b6614416dac5295a7e3b7ec2c83065e828a
jfrydman <justin.frydman@gmail.com>
2019-01-28 23:26:08 -0700

    Add sample webdriver test that uses the customizer

---
+++ dev/tests/tests/webdriver/SquareOneChromeExampleCest.php
@@ -0,0 +1,20 @@
+<?php 
+
+class SquareOneChromeExampleCest {
+
+	const SITE_TITLE        = 'This is Sparta';
+	const FIELD_SITE_TITLE  = '#_customize-input-blogname';
+
+	public function _before(AcceptanceTester $I) {
+    }
+
+    public function i_can_set_the_site_title_in_the_customizer( AcceptanceTester $I ) {
+		$I->loginAsAdmin();
+		$I->amOnPage('/');
+		$I->click( '#wp-admin-bar-customize a' );
+		$I->click( '#accordion-section-title_tagline' );
+		$I->fillField(self::FIELD_SITE_TITLE, self::SITE_TITLE );
+		$I->click( '#save' );
+		$I->seeInField( self::FIELD_SITE_TITLE, self::SITE_TITLE );
+    }
+}
