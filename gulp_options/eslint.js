0484d7ee38b9333e1fa774c9da9061600ec4ca15
Samuel Estok <sam@tri.be>
2019-02-25 19:13:07 -0800

    Lint app js only

--- gulp_options/eslint.js
+++ gulp_options/eslint.js
@@ -24,7 +24,7 @@ module.exports = {
 	},
 	apps() {
 		return lint( [
-			`${ pkg._core_apps_js_src_path }**/*`,
+			`${ pkg._core_apps_js_src_path }**/*.js`,
 		], pkg._core_apps_js_src_path );
 	},
 	utils() {
