73b147c474962fb67d142f9a556b9ab4dea420be
Samuel Estok <sam@tri.be>
2019-02-28 19:34:09 -0800

    Patch and reformat all js tests

--- wp-content/themes/core/js/test/tests/tests.test.js
+++ wp-content/themes/core/js/test/tests/tests.test.js
@@ -1,46 +1,46 @@
 import 'utils/__mocks__/localStorage';
 import * as tests from 'utils/tests';
 
-describe('isJson', () => {
-	it('returns true if json string is valid', () => {
+describe( 'isJson', () => {
+	it( 'returns true if json string is valid', () => {
 		const str = '{ "test": true }';
 
-		expect(tests.isJson(str)).toBe(true);
-	});
+		expect( tests.isJson( str ) ).toBe( true );
+	} );
 
-	it('returns false if json string is not valid', () => {
+	it( 'returns false if json string is not valid', () => {
 		const str = 'lorem ipsum';
 
-		expect(tests.isJson(str)).toBe(false);
-	});
-});
+		expect( tests.isJson( str ) ).toBe( false );
+	} );
+} );
 
-describe('canLocalStore', () => {
-	it('returns true if browser supports local storage', () => {
-		expect(tests.canLocalStore()).toBe(true);
-	});
+describe( 'canLocalStore', () => {
+	it( 'returns true if browser supports local storage', () => {
+		expect( tests.canLocalStore() ).toBe( true );
+	} );
 
-	it('returns false if browser does not support local storage', () => {
+	it( 'returns false if browser does not support local storage', () => {
 		localStorage.setItem = () => {
 			throw new Error();
 		};
-		expect(tests.canLocalStore()).toBe(false);
-	});
+		expect( tests.canLocalStore() ).toBe( false );
+	} );
 
 	// aditional test mainly for coverage
-	it('local storage returns null when item is not found', () => {
-		expect(localStorage.getItem('lorem')).toBe(null);
-	});
+	it( 'local storage returns null when item is not found', () => {
+		expect( localStorage.getItem( 'lorem' ) ).toBe( null );
+	} );
 
 	// aditional test mainly for coverage
-	it('local storage clears all items', () => {
-		expect(localStorage.clear()).toBe(undefined);
-	});
-});
+	it( 'local storage clears all items', () => {
+		expect( localStorage.clear() ).toBe( undefined );
+	} );
+} );
 
-describe('browserTests', () => {
-	it('returns browser test object', () => {
-		expect(tests.browserTests()).toMatchObject({
+describe( 'browserTests', () => {
+	it( 'returns browser test object', () => {
+		expect( tests.browserTests() ).toMatchObject( {
 			android: false,
 			chrome: false,
 			edge: false,
@@ -51,6 +51,6 @@ describe('browserTests', () => {
 			opera: false,
 			safari: true,
 			os: '',
-		});
-	});
-});
+		} );
+	} );
+} );
--- wp-content/themes/core/js/test/tests/utils/data/query-to-json.test.js
+++ wp-content/themes/core/js/test/tests/utils/data/query-to-json.test.js
@@ -1,35 +1,33 @@
 import queryToJson from 'utils/data/query-to-json';
 
-describe('queryToJson', () => {
-	it('returns json object from a valid query string', () => {
+describe( 'queryToJson', () => {
+	it( 'returns json object from a valid query string', () => {
 		const query = 'param1=true&param2=1024&param3=';
 
-		expect(queryToJson(query)).toMatchObject({
+		expect( queryToJson( query ) ).toMatchObject( {
 			param1: 'true',
 			param2: '1024',
 			param3: '',
-		});
-	});
+		} );
+	} );
 
-	it('returns json object from the current location query string', () => {
-		Object.defineProperty(location, 'search', {
-			value: '?param1=true&param2=1024&param3=',
-			configurable: true,
-		});
+	it( 'returns json object from the current location query string', () => {
+		jsdom.reconfigure( {
+			url: 'http://localhost/?param1=true&param2=1024&param3=',
+		} );
 
-		expect(queryToJson()).toMatchObject({
+		expect( queryToJson() ).toMatchObject( {
 			param1: 'true',
 			param2: '1024',
 			param3: '',
-		});
-	});
+		} );
+	} );
 
-	it('returns empty json object from the current location if there isnt any query string', () => {
-		Object.defineProperty(location, 'search', {
-			value: '',
-			configurable: true,
-		});
+	it( 'returns empty json object from the current location if there isnt any query string', () => {
+		jsdom.reconfigure( {
+			url: 'http://localhost/',
+		} );
 
-		expect(queryToJson()).toMatchObject({});
-	});
-});
+		expect( queryToJson() ).toMatchObject( {} );
+	} );
+} );
