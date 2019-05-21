72be7c144fa2af18a9d9011fde50495f5747b849
Lucas Bustamante <lucasfbustamante@gmail.com>
2019-05-21 14:25:32 -0300

    Removed "Pimple" word from SquareOneTestCase
    To decouple the tests from Pimple Container, now SquareOneTestCase refers to the container simply as the "Container".

--- dev/tests/tests/_support/Classes/SquareOneTestCase.php
+++ dev/tests/tests/_support/Classes/SquareOneTestCase.php
@@ -18,35 +18,34 @@ class SquareOneTestCase extends WPTestCase {
 		parent::setUp();
 
 		// your set up methods here
-		$this->save_original_pimple_container();
+		$this->save_original_container();
 	}
 
 	public function tearDown() {
 		// your tear down methods here
-		$this->reset_pimple_container();
+		$this->reset_container();
 
 		// then
 		parent::tearDown();
 	}
 
 	/**
-	 * Saves a copy of the original Pimple container collection before tests begin.
+	 * Saves a copy of the original Container collection before tests begin.
 	 */
-	private function save_original_pimple_container() {
+	private function save_original_container() {
 		if ( $this->original_providers === null ) {
 			$this->original_providers = tribe_project()->container()->keys();
 		}
 	}
 
 	/**
-	 * Resets the Pimple container after each test.
+	 * Resets the Container after each test.
 	 */
-	protected function reset_pimple_container() {
+	protected function reset_container() {
 		$providers_after_test = tribe_project()->container()->keys();
 		$new_keys             = array_diff( $providers_after_test, $this->original_providers );
-		$container            = tribe_project()->container();
-		array_map( function ( $key ) use ( $container ) {
-			unset( $container[ $key ] );
+		array_map( function ( $key ) {
+			unset( tribe_project()->container()[ $key ] );
 		}, $new_keys );
 	}
 }
