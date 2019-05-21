8f6baf2af738f6dbd77277d2b08e348e414ba35e
Jonathan Brinley <jbrinley@flightless.us>
2019-05-20 22:01:56 -0400

    monorepo work in progress

--- wp-content/plugins/core/src/Queues/Contracts/Task.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Tribe\Project\Queues\Contracts;
-
-interface Task {
-
-	public function handle( array $args ) : bool;
-}
--- wp-content/plugins/core/src/Service_Providers/CLI_Provider.php
+++ wp-content/plugins/core/src/Service_Providers/CLI_Provider.php
@@ -4,32 +4,13 @@
 namespace Tribe\Project\Service_Providers;
 
 use Pimple\Container;
-use Tribe\Project\CLI\Container_Export;
 use Tribe\Project\Container\Service_Provider;
-use Tribe\Project\CLI\CLI_Generator;
-use Tribe\Project\CLI\Settings_Generator;
-use Tribe\Project\CLI\CPT_Generator;
-use Tribe\Project\CLI\File_System;
-use Tribe\Project\CLI\Meta\Importer;
-use Tribe\Project\CLI\Taxonomy_Generator;
 use Tribe\Project\CLI\Cache_Prime;
 
 class CLI_Provider extends Service_Provider {
 	const CACHE_PRIME      = 'cli.cache-prime';
-	const CONTAINER_EXPORT = 'cli.container_export';
-	const FILE_SYSTEM      = 'cli.file_system';
-	const GENERATE_CPT     = 'cli.generator.cpt';
-	const GENERATE_TAX     = 'cli.generator.taxonomy';
-	const GENERATE_CLI     = 'cli.generator.cli';
-	const GENERATE_SETTING = 'cli.generator.setting';
-	const GENERATE_META    = 'cli.generator.meta';
 
 	public function register( Container $container ) {
-		$this->generators( $container );
-
-		$container[ self::CONTAINER_EXPORT ] = function ( $container ) {
-			return new Container_Export( tribe_project() );
-		};
 
 		$container[ self::CACHE_PRIME ] = function () {
 			return new Cache_Prime();
@@ -40,39 +21,7 @@ class CLI_Provider extends Service_Provider {
 				return;
 			}
 
-			$container[ self::CONTAINER_EXPORT ]->register();
 			$container[ self::CACHE_PRIME ]->register();
-			$container[ self::GENERATE_CPT ]->register();
-			$container[ self::GENERATE_TAX ]->register();
-			$container[ self::GENERATE_CLI ]->register();
-			$container[ self::GENERATE_SETTING ]->register();
-			$container[ self::GENERATE_META ]->register();
 		}, 0, 0 );
-	}
-
-	private function generators( Container $container ) {
-
-		$container[ self::FILE_SYSTEM ] = function ( $container ) {
-			return new File_System();
-		};
-
-		$container[ self::GENERATE_CPT ] = function ( $container ) {
-			return new CPT_Generator( $container[ self::FILE_SYSTEM ] );
-		};
-
-		$container[ self::GENERATE_TAX ] = function ( $container ) {
-			return new Taxonomy_Generator( $container[ self::FILE_SYSTEM ] );
-		};
-
-		$container[ self::GENERATE_CLI ]     = function ( $container ) {
-			return new CLI_Generator( $container[ self::FILE_SYSTEM ] );
-		};
-		$container[ self::GENERATE_SETTING ] = function ( $container ) {
-			return new Settings_Generator( $container[ self::FILE_SYSTEM ] );
-		};
-
-		$container[ self::GENERATE_META ] = function ( $container ) {
-			return new Importer( $container[ self::FILE_SYSTEM ] );
-		};
 	}
 }
