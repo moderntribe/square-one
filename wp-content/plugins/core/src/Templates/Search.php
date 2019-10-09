a88a2fc791d33f2fa4399624322bbd1f1890ac80
Lucas B <lucasfbustamante@gmail.com>
2019-10-09 14:37:09 -0300

    Improving search when no results found

--- wp-content/plugins/core/src/Templates/Search.php
+++ wp-content/plugins/core/src/Templates/Search.php
@@ -4,10 +4,37 @@
 namespace Tribe\Project\Templates;
 
 class Search extends Index {
+
+	public function get_data(): array {
+		$data                   = parent::get_data();
+		$data['search_details'] = $this->get_search_details( count( $data['posts'] ) );
+
+		return $data;
+	}
+
 	protected function get_single_post() {
 		$template = new Content\Search\Search( $this->template, $this->twig );
-		$data    = $template->get_data();
+		$data     = $template->get_data();
 
-		return $data[ 'post' ];
+		return $data['post'];
+	}
+
+	protected function get_search_details( int $posts_found ): array {
+		$query = get_search_query();
+
+		$result_text = _n(
+			'result',
+			'results',
+			$posts_found,
+			'tribe'
+		);
+
+		$search_details = [
+			'query'   => esc_html( $query ),
+			'results' => esc_html( $result_text ),
+			'count'   => $posts_found,
+		];
+
+		return $search_details;
 	}
 }
