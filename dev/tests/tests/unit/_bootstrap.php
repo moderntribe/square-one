7b5192eaeee3e7a5612ea60ca3f24defcfb9a7a5
Jonathan Brinley <jbrinley@flightless.us>
2019-10-20 16:06:11 -0400

    create the cache dir

--- dev/tests/tests/unit/_bootstrap.php
+++ dev/tests/tests/unit/_bootstrap.php
@@ -3,6 +3,10 @@ use tad\FunctionMocker\FunctionMocker;
 
 \Codeception\Util\Autoload::addNamespace('Tribe\\Project', __DIR__ . '/../../../../wp-content/plugins/core/src');
 
+$cache_path = realpath( sys_get_temp_dir() ) . DIRECTORY_SEPARATOR . 'function-mocker';
+if ( ! file_exists( $cache_path ) ) {
+	mkdir( $cache_path );
+}
 FunctionMocker::init( [
 	'cache-path' => realpath( sys_get_temp_dir() ) . DIRECTORY_SEPARATOR . 'function-mocker',
 	'blacklist'  => dirname( __DIR__ ),
