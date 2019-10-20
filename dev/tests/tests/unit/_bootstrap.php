2c1eb7a06e5e6acdc7fc7e8c5bfc3ec2e84b4517
Jonathan Brinley <jbrinley@flightless.us>
2019-10-20 16:20:33 -0400

    just disable the FacadeTest, along with FunctionMocker

--- dev/tests/tests/unit/_bootstrap.php
+++ dev/tests/tests/unit/_bootstrap.php
@@ -3,11 +3,11 @@ use tad\FunctionMocker\FunctionMocker;
 
 \Codeception\Util\Autoload::addNamespace('Tribe\\Project', __DIR__ . '/../../../../wp-content/plugins/core/src');
 
-$cache_path = realpath( sys_get_temp_dir() ) . DIRECTORY_SEPARATOR . 'function-mocker';
-if ( ! file_exists( $cache_path ) ) {
-	mkdir( $cache_path );
-}
-FunctionMocker::init( [
-	'cache-path' => realpath( sys_get_temp_dir() ) . DIRECTORY_SEPARATOR . 'function-mocker',
-	'blacklist'  => dirname( __DIR__ ),
-] );
+//$cache_path = realpath( sys_get_temp_dir() ) . DIRECTORY_SEPARATOR . 'function-mocker';
+//if ( ! file_exists( $cache_path ) ) {
+//	mkdir( $cache_path );
+//}
+//FunctionMocker::init( [
+//	'cache-path' => $cache_path,
+//	'blacklist'  => dirname( __DIR__ ),
+//] );
