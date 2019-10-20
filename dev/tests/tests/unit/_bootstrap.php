3f21e12fc546cc90bada3a7d3eaad85f18d6f115
Jonathan Brinley <jbrinley@flightless.us>
2019-10-20 15:54:55 -0400

    blacklist tests dir for function mocker

--- dev/tests/tests/unit/_bootstrap.php
+++ dev/tests/tests/unit/_bootstrap.php
@@ -3,4 +3,7 @@ use tad\FunctionMocker\FunctionMocker;
 
 \Codeception\Util\Autoload::addNamespace('Tribe\\Project', __DIR__ . '/../../../../wp-content/plugins/core/src');
 
-FunctionMocker::init();
+FunctionMocker::init( [
+	'cache-path' => realpath( sys_get_temp_dir() ) . DIRECTORY_SEPARATOR . 'function-mocker',
+	'blacklist'  => dirname( __DIR__ ),
+] );
