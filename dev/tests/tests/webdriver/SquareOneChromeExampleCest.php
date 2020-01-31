50c092ad97d1caec16e9a5c3128ab3a2d8536582
Lucas B <lucasfbustamante@gmail.com>
2020-01-31 00:37:21 -0300

    Add Page Object examples

--- dev/tests/tests/webdriver/SquareOneChromeExampleCest.php
+++ dev/tests/tests/webdriver/SquareOneChromeExampleCest.php
@@ -1,15 +1,18 @@
-<?php 
+<?php
+
+use Page\Webdriver\Home_Page_Example;
+use Page\Webdriver\Search_Page_Example;
 
 class SquareOneChromeExampleCest extends Base_Webdriver_Cest {
 
 	public function i_can_set_the_site_title_in_the_customizer( WebDriverTester $I ) {
-		$site_title = wp_generate_password();
+		$site_title = __METHOD__ . rand( 0, 1000 );
 
-		$I->makeScreenshot( __LINE__ . ' - Start' );
+		$I->makeScreenshot();
 		$I->loginAsAdmin();
 		$I->amOnPage( '/' );
 		$I->click( '#wp-admin-bar-customize a' );
-		$I->makeScreenshot( __LINE__ . ' - On Customizer' );
+		$I->makeScreenshot();
 		$I->click( '#accordion-section-title_tagline' );
 		$I->waitForElementVisible( '#_customize-input-blogname', 3 );
 		$I->fillField( '#_customize-input-blogname', $site_title );
@@ -17,5 +20,11 @@ class SquareOneChromeExampleCest extends Base_Webdriver_Cest {
 		$I->waitForJqueryAjax();
 		$I->makeScreenshot( __LINE__ . ' - After Saving' );
 		$I->assertEquals( $site_title, $I->grabOptionFromDatabase( 'blogname' ) );
+	}
+
+	public function can_search_on_homepage( WebDriverTester $I, Home_Page_Example $home_page, Search_Page_Example $search_page ) {
+		$search_query = 'FooBarBaz';
+		$home_page->search( $search_query );
+		$search_page->see_search_query( $search_query );
 	}
 }
