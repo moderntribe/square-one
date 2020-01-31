e9aae301c0dc3df8b70e09f57436c77c92a3d1b3
Lucas B <lucasfbustamante@gmail.com>
2020-01-30 22:15:25 -0300

    Cleanup output folder before running tests

--- dev/docker/testx.sh
+++ dev/docker/testx.sh
@@ -6,6 +6,15 @@ SCRIPTDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd "$SCRIPTDIR";
 PROJECT_ID=$(cat ./.projectID)
 
+# Cleanup debug files
+${SCRIPTDIR}/exec.sh \
+php \
+-dxdebug.remote_autostart=0 \
+-dxdebug.remote_enable=0 \
+/application/www/vendor/bin/codecept \
+--config "/application/www/dev/tests" clean
+
+# Run the codeception command with tests
 ${SCRIPTDIR}/exec.sh \
 php \
 -dxdebug.remote_autostart=1 \
