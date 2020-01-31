63d0ace4b89e9be6b3bc7ce85c4e64e60862d04a
Lucas B <lucasfbustamante@gmail.com>
2020-01-31 01:48:26 -0300

    Fix Full_Size_GifsTest

--- dev/tests/tests/integration/Tribe/Project/Theme/Full_Size_GifsTest.php
+++ dev/tests/tests/integration/Tribe/Project/Theme/Full_Size_GifsTest.php
@@ -2,8 +2,7 @@
 
 namespace Tribe\Project\Theme;
 
-use Tribe\Project\Theme\Full_Size_Gif;
-use Tribe\Project\Theme\Image;
+use Tribe\Project\Templates\Components\Image;
 
 use Tribe\Tests\Test_Case;
 
@@ -17,34 +16,39 @@ class Full_Size_GifsTest extends Test_Case {
 		$full_size_gif = new Full_Size_Gif();
 		add_filter( 'image_downsize', [ $full_size_gif, 'full_size_only_gif' ], 10, 3 );
 
-		$filename = codecept_data_dir( 'test.gif' );
+		$filename       = codecept_data_dir( 'test.gif' );
 		$parent_post_id = wp_insert_post( [ 'title' => 'test_gifs', 'post_type' => 'post' ] );
 
-		$filetype = wp_check_filetype( basename( $filename ), null );
+		$filetype      = wp_check_filetype( basename( $filename ), null );
 		$wp_upload_dir = wp_upload_dir();
-		$attachment = array(
+		$attachment    = [
 			'guid'           => $wp_upload_dir['url'] . '/' . basename( $filename ),
 			'post_mime_type' => $filetype['type'],
 			'post_title'     => preg_replace( '/\.[^.]+$/', '', basename( $filename ) ),
 			'post_content'   => '',
-			'post_status'    => 'inherit'
-		);
+			'post_status'    => 'inherit',
+		];
 		$attachment_id = wp_insert_attachment( $attachment, $filename, $parent_post_id );
 
 		require_once( ABSPATH . 'wp-admin/includes/image.php' );
 		$attach_data = wp_generate_attachment_metadata( $attachment_id, $filename );
 		wp_update_attachment_metadata( $attachment_id, $attach_data );
 
-
-		$full_size = new Image( $attachment_id, [ 'src_size' => 'full' ] );
+		$full_size      = Image::factory( [
+			Image::IMG_ID   => $attachment_id,
+			Image::SRC_SIZE => 'full',
+		] );
 		$full_size_html = $full_size->render();
-		$xml = simplexml_load_string( $full_size_html );
-		$full_size_src = (string) $xml->img[0]['data-src'];
+		$xml            = simplexml_load_string( $full_size_html );
+		$full_size_src  = (string) $xml->img[0]['data-src'];
 
-		$thumbnail = new Image( $attachment_id, [ 'src_size' => 'thumbnail' ] );
+		$thumbnail      = Image::factory( [
+			Image::IMG_ID   => $attachment_id,
+			Image::SRC_SIZE => 'thumbnail',
+		] );
 		$thumbnail_html = $thumbnail->render();
-		$xml = simplexml_load_string( $thumbnail_html );
-		$thumbnail_src = (string) $xml->img[0]['data-src'];
+		$xml            = simplexml_load_string( $thumbnail_html );
+		$thumbnail_src  = (string) $xml->img[0]['data-src'];
 
 		$this->assertSame( $full_size_src, $thumbnail_src );
 
