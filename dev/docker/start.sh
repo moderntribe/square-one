c88703a9e63b61268eef0dda42919f160321d229
Lucas B <lucasfbustamante@gmail.com>
2020-02-27 15:04:00 -0300

    Does Travis run with composer update?

--- dev/docker/start.sh
+++ dev/docker/start.sh
@@ -100,4 +100,4 @@ if [ ! -f "${COMPOSER_DIR}/composer.json" ]; then
   bash ${SCRIPTDIR}/exec.sh composer global require hirak/prestissimo --classmap-authoritative --update-no-dev
 fi
 
-bash ${SCRIPTDIR}/composer.sh install
+bash ${SCRIPTDIR}/composer.sh update
