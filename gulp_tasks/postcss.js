cb19d6a0b8adef8af35511c07873bf7635818174
Neal Fennimore <hi@neal.codes>
2020-03-27 12:24:17 -0400

    Update cssProcess to use bundle name to concat glob of files

--- gulp_tasks/postcss.js
+++ gulp_tasks/postcss.js
@@ -3,6 +3,7 @@ const postcss = require( 'gulp-postcss' );
 const sourcemaps = require( 'gulp-sourcemaps' );
 const rename = require( 'gulp-rename' );
 const gulpif = require( 'gulp-if' );
+const concat = require( 'gulp-concat' );
 const browserSync = require( 'browser-sync' );
 const postcssFunctions = require( '../dev_components/theme/pcss/functions' );
 const pkg = require( '../package.json' );
@@ -39,12 +40,36 @@ const legacyPlugins = [
 	require( 'postcss-assets' )( { loadPaths: [ `${ pkg._core_theme_path }/` ] } ),
 ];
 
-function cssProcess( src = [], dest = pkg._core_admin_css_path, plugins = compilePlugins ) {
+/**
+ *
+ *
+ * @param {Object} options {
+ * 	src = [],
+ * 	dest = pkg._core_admin_css_path,
+ * 	plugins = compilePlugins,
+ * 	bundleName = null,
+ * }
+ * @param {Array<string>} options.src
+ * @param {string} options.dest
+ * @param {Array<Function>} options.plugins
+ * @param {string} options.bundleName
+ * @returns
+ */
+function cssProcess( {
+	src = [],
+	dest = pkg._core_admin_css_path,
+	plugins = compilePlugins,
+	bundleName = 'empty.css',
+} ) {
 	const server = browserSync.get( 'Tribe Dev' );
 	return gulp.src( src )
 		.pipe( sourcemaps.init() )
 		.pipe( postcss( plugins ) )
 		.pipe( rename( { extname: '.css' } ) )
+		.pipe( gulpif(
+			bundleName !== 'empty.css',
+			concat( bundleName )
+		) )
 		.pipe( sourcemaps.write( '.' ) )
 		.pipe( gulp.dest( dest ) )
 		.pipe( gulpif( server.active, server.reload( { stream: true } ) ) );
@@ -52,30 +77,51 @@ function cssProcess( src = [], dest = pkg._core_admin_css_path, plugins = compil
 
 module.exports = {
 	theme() {
-		return cssProcess( [
-			`${ pkg._core_theme_pcss_path }master.pcss`,
-			`${ pkg._core_theme_pcss_path }print.pcss`,
-			`${ pkg._core_theme_components_path }/**/index.pcss`,
-		], pkg._core_theme_css_path );
+		return cssProcess( {
+			src: [
+				`${ pkg._core_theme_pcss_path }master.pcss`,
+				`${ pkg._core_theme_pcss_path }print.pcss`,
+			],
+			dest: pkg._core_theme_css_path,
+		} );
+	},
+	themeComponents() {
+		return cssProcess( {
+			src: [
+				`${ pkg._core_theme_components_path }**/index.pcss`,
+			],
+			dest: `${ pkg._core_theme_css_path }`,
+			bundleName: 'components.css',
+		} );
 	},
 	themeLegacy() {
-		return cssProcess( [
-			`${ pkg._core_theme_pcss_path }legacy.pcss`,
-		], pkg._core_theme_css_path, legacyPlugins );
+		return cssProcess( {
+			src: [
+				`${ pkg._core_theme_pcss_path }legacy.pcss`,
+			],
+			dest: pkg._core_theme_css_path,
+			plugins: legacyPlugins,
+		} );
 	},
 	themeWPEditor() {
-		return cssProcess( [
-			`${ pkg._core_admin_pcss_path }editor-style.pcss`,
-		] );
+		return cssProcess( {
+			src: [
+				`${ pkg._core_admin_pcss_path }editor-style.pcss`,
+			],
+		} );
 	},
 	themeWPLogin() {
-		return cssProcess( [
-			`${ pkg._core_admin_pcss_path }login.pcss`,
-		] );
+		return cssProcess( {
+			src: [
+				`${ pkg._core_admin_pcss_path }login.pcss`,
+			],
+		} );
 	},
 	themeWPAdmin() {
-		return cssProcess( [
-			`${ pkg._core_admin_pcss_path }master.pcss`,
-		] );
+		return cssProcess( {
+			src: [
+				`${ pkg._core_admin_pcss_path }master.pcss`,
+			],
+		} );
 	},
 };
