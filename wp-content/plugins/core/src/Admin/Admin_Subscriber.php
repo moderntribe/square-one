08e6bd17d18954dfb7dd8a38da6a31e61ead5a15
Jonathan Brinley <jbrinley@flightless.us>
2020-04-01 22:10:45 -0400

    disable deprecation notices from 3rd-party plugins for deprecated filter usage

--- wp-content/plugins/core/src/Admin/Admin_Subscriber.php
+++ wp-content/plugins/core/src/Admin/Admin_Subscriber.php
@@ -14,6 +14,9 @@ class Admin_Subscriber implements Subscriber_Interface {
 			$container->get( Scripts::class )->enqueue_scripts();
 			$container->get( Styles::class )->enqueue_styles();
 		}, 10, 0 );
+
+		// several 3rd-party plugins trigger errors in the admin due to use of deprecated hooks
+		add_filter( 'deprecated_hook_trigger_error', '__return_false' );
 	}
 
 }
