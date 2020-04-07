f1eda98c11332111f84c986b2103343bb8f0087e
Jonathan Brinley <jbrinley@flightless.us>
2020-04-07 15:03:28 -0400

    add a class to filter the block blacklist

---
+++ wp-content/plugins/core/src/Blocks/Allowed_Blocks.php
@@ -0,0 +1,22 @@
+<?php
+declare( strict_types=1 );
+
+namespace Tribe\Project\Blocks;
+
+class Allowed_Blocks {
+	private $blacklist = [
+		'core/buttons',
+	];
+
+	/**
+	 * Add block types to the blacklist to disable in the block editor
+	 *
+	 * @param string[]
+	 *
+	 * @return array
+	 * @filter tribe/project/blocks/blacklist
+	 */
+	public function filter_block_blacklist( array $types ): array {
+		return array_unique( array_merge( $types, $this->blacklist ) );
+	}
+}
--- wp-content/plugins/core/src/Blocks/Blocks_Subscriber.php
+++ wp-content/plugins/core/src/Blocks/Blocks_Subscriber.php
@@ -9,16 +9,20 @@ use Tribe\Libs\Container\Subscriber_Interface;
 
 class Blocks_Subscriber implements Subscriber_Interface {
 	public function register( ContainerInterface $container ): void {
-		add_action( 'tribe/gutenpanels/register', function ( Registry $registry ) use ( $container ) {
+		add_action( 'tribe/gutenpanels/register', static function ( Registry $registry ) use ( $container ) {
 			foreach ( $container->get( Blocks_Definer::TYPES ) as $type ) {
 				/** @var Block_Type_Config $type */
 				$registry->register( $type->build() );
 			}
 		}, 10, 1 );
 
-		add_action( 'tribe/gutenpanels/block/render', function ( $prefiltered, $attributes, $content, $block_type ) use ( $container ) {
+		add_action( 'tribe/gutenpanels/block/render', static function ( $prefiltered, $attributes, $content, $block_type ) use ( $container ) {
 			return $container->get( Render_Filter::class )->render( $prefiltered, $attributes, $content, $block_type );
 		}, 10, 4 );
+
+		add_filter( 'tribe/project/blocks/blacklist', static function( $types ) use ( $container ) {
+			return $container->get( Allowed_Blocks::class )->filter_block_blacklist( $types );
+		}, 10, 2 );
 	}
 
 }
