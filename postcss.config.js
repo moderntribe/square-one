fc9ed887beb4d805adeaae14677752ad8e8a10c0
Neal Fennimore <hi@neal.codes>
2020-04-07 13:43:05 -0400

    Upate postcss config to match gulp config

--- postcss.config.js
+++ postcss.config.js
@@ -1,21 +1,26 @@
+const postcssFunctions = require( './dev_components/theme/pcss/functions' );
 const pkg = require( './package.json' );
 
 module.exports = ( { file, options, env } = {} ) => ( { // eslint-disable-line no-unused-vars
 	plugins: {
 		'postcss-import': {
 			path: [
-				`./${ pkg.square1.paths.core_theme }/`,
+				`./${ pkg.square1.paths.core_theme }`,
 			],
 		},
 		'postcss-mixins': {},
-		'postcss-custom-properties': {},
+		'postcss-custom-properties': { preserve: false },
 		'postcss-simple-vars': {},
 		'postcss-custom-media': {},
+		'postcss-functions': { functions: postcssFunctions },
 		'postcss-quantity-queries': {},
 		'postcss-aspect-ratio': {},
 		'postcss-nested': {},
 		'postcss-inline-svg': {},
-		'postcss-preset-env': { stage: 0 },
+		'postcss-preset-env': { stage: 0, autoprefixer: { grid: true } },
 		'postcss-calc': {},
+		'postcss-assets': {
+			loadPaths: [ `${ pkg.square1.paths.core_theme }/` ],
+		},
 	},
 } );
