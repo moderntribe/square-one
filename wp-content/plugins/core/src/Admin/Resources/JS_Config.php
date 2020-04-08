f610f03c20125ecaee502d37182727a75a813d02
Jonathan Brinley <jbrinley@flightless.us>
2020-04-08 08:31:53 -0400

    add block blacklist to admin JS config

--- wp-content/plugins/core/src/Admin/Resources/JS_Config.php
+++ wp-content/plugins/core/src/Admin/Resources/JS_Config.php
@@ -8,9 +8,10 @@ class JS_Config {
 	private $data;
 
 	public function get_data() {
-		if ( !isset( $this->data ) ) {
+		if ( ! isset( $this->data ) ) {
 			$this->data = [
-				'images_url'   => trailingslashit( get_stylesheet_directory_uri() ) . 'assets/img/admin/',
+				'images_url'      => trailingslashit( get_stylesheet_directory_uri() ) . 'assets/img/admin/',
+				'block_blacklist' => apply_filters( 'tribe/project/blocks/blacklist', [] ),
 			];
 			$this->data = apply_filters( 'core_admin_js_config', $this->data );
 		}
