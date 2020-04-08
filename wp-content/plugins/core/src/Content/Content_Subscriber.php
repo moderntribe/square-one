352e798013700804904a993dde0d7528a87b02fa
Jonathan Brinley <jbrinley@flightless.us>
2020-04-08 17:29:36 -0400

    implement the change to the subscriber interface from tribe-libs #37

--- wp-content/plugins/core/src/Content/Content_Subscriber.php
+++ wp-content/plugins/core/src/Content/Content_Subscriber.php
@@ -3,37 +3,36 @@ declare( strict_types=1 );
 
 namespace Tribe\Project\Content;
 
-use Psr\Container\ContainerInterface;
-use Tribe\Libs\Container\Subscriber_Interface;
+use Tribe\Libs\Container\Abstract_Subscriber;
 
-class Content_Subscriber implements Subscriber_Interface {
-	public function register( ContainerInterface $container ): void {
-		$this->required_pages( $container );
+class Content_Subscriber extends Abstract_Subscriber {
+	public function register(): void {
+		$this->required_pages();
 	}
 
-	private function required_pages( ContainerInterface $container ): void {
-		add_action( 'admin_init', function () use ( $container ) {
-			foreach ( $container->get( Content_Definer::REQUIRED_PAGES ) as $page ) {
+	private function required_pages(): void {
+		add_action( 'admin_init', function () {
+			foreach ( $this->container->get( Content_Definer::REQUIRED_PAGES ) as $page ) {
 				$page->ensure_page_exists();
 			}
 		}, 10, 0 );
-		add_action( 'trashed_post', function ( $post_id ) use ( $container ) {
-			foreach ( $container->get( Content_Definer::REQUIRED_PAGES ) as $page ) {
+		add_action( 'trashed_post', function ( $post_id ) {
+			foreach ( $this->container->get( Content_Definer::REQUIRED_PAGES ) as $page ) {
 				$page->clear_option_on_delete( $post_id );
 			}
 		}, 10, 1 );
-		add_action( 'deleted_post', function ( $post_id ) use ( $container ) {
-			foreach ( $container->get( Content_Definer::REQUIRED_PAGES ) as $page ) {
+		add_action( 'deleted_post', function ( $post_id ) {
+			foreach ( $this->container->get( Content_Definer::REQUIRED_PAGES ) as $page ) {
 				$page->clear_option_on_delete( $post_id );
 			}
 		}, 10, 1 );
-		add_action( 'acf/init', function () use ( $container ) {
-			foreach ( $container->get( Content_Definer::REQUIRED_PAGES ) as $page ) {
+		add_action( 'acf/init', function () {
+			foreach ( $this->container->get( Content_Definer::REQUIRED_PAGES ) as $page ) {
 				$page->register_setting();
 			}
 		}, 10, 0 );
-		add_filter( 'display_post_states', function ( $post_states, $post ) use ( $container ) {
-			foreach ( $container->get( Content_Definer::REQUIRED_PAGES ) as $page ) {
+		add_filter( 'display_post_states', function ( $post_states, $post ) {
+			foreach ( $this->container->get( Content_Definer::REQUIRED_PAGES ) as $page ) {
 				$post_states = $page->indicate_post_state( $post_states, $post );
 			}
 
--- wp-content/plugins/core/src/Theme_Customizer/Theme_Customizer_Subscriber.php
+++ wp-content/plugins/core/src/Theme_Customizer/Theme_Customizer_Subscriber.php
@@ -3,13 +3,12 @@ declare( strict_types=1 );
 
 namespace Tribe\Project\Theme_Customizer;
 
-use Psr\Container\ContainerInterface;
-use Tribe\Libs\Container\Subscriber_Interface;
+use Tribe\Libs\Container\Abstract_Subscriber;
 
-class Theme_Customizer_Subscriber implements Subscriber_Interface {
-	public function register( ContainerInterface $container ): void {
-		add_action( 'customize_register', function(  \WP_Customize_Manager $wp_customize  ) use ( $container ) {
-			$container->get( Customizer_Loader::class )->register_customizer_controls( $wp_customize );
+class Theme_Customizer_Subscriber extends Abstract_Subscriber {
+	public function register(): void {
+		add_action( 'customize_register', function(  \WP_Customize_Manager $wp_customize  ) {
+			$this->container->get( Customizer_Loader::class )->register_customizer_controls( $wp_customize );
 		}, 10, 1 );
 	}
 }
