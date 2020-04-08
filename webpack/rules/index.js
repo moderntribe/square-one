94c60cf232b45befd96119d377c24835a8e36513
Neal Fennimore <hi@neal.codes>
2020-04-08 17:42:47 -0400

    Update styles to not use css modules

--- webpack/rules/index.js
+++ webpack/rules/index.js
@@ -1,28 +1,7 @@
-const MiniCssExtractPlugin = require( 'mini-css-extract-plugin' );
+const scripts = require('./scripts');
+const styles = require('./styles');
 
 module.exports = [
-	{
-		test: /\.js$/,
-		exclude: [ /(node_modules)/ ],
-		use: [
-			{
-				loader: 'babel-loader',
-			},
-		],
-	},
-	{
-		test: /\.p?css$/,
-		use: [
-			MiniCssExtractPlugin.loader,
-			{
-				loader: 'css-loader',
-				options: {
-					modules: {
-						localIdentName: '[name]__[local]___[hash:base64:5]',
-					},
-				},
-			},
-			{ loader: 'postcss-loader' },
-		],
-	},
+	scripts,
+	styles
 ];
