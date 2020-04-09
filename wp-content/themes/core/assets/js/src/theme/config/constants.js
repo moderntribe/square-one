04c00a5d347b1cc785c282f95a21137d0f9128d8
Samuel Estok <sam@tri.be>
2020-04-08 21:03:46 -0700

    clean constants

--- wp-content/themes/core/assets/js/src/theme/config/constants.js
+++ wp-content/themes/core/assets/js/src/theme/config/constants.js
@@ -1,22 +1,4 @@
-export const MAPS_API_KEY = 'AIzaSyDCRe8RZgTIU9OihX2X1fEf89P1ad1P_kI';
-export const TIDAL_API_KEY = 'b2de108b-7e73-41e0-9785-f3f3ffa134ae';
-export const OPENWEATHER_API_KEY = '09c411fc04f45a147a8aa4f2fcb247b2';
-
-export const POST_TYPE_PARK = 'park';
-export const POST_TYPE_CAMPGROUND = 'campground';
-export const POST_TYPE_TRAIL = 'trail';
-export const POST_TYPE_LOCATION = 'location';
-export const POST_TYPE_JOURNEY = 'journey';
-
-export const AJAX_ACTION_CHECK_WEATHER_DATA = 'get_weather_data';
-export const AJAX_ACTION_STORE_WEATHER_DATA = 'update_weather_data';
-export const AJAX_ACTION_CHECK_WEATHER_FORECAST_DATA = 'get_weather_forecast_data';
-export const AJAX_ACTION_STORE_WEATHER_FORECAST_DATA = 'update_weather_forecast_data';
-
-export const AJAX_ACTION_STORE_PHOTO_DATA = 'update_photo_data';
-
 export const SERVICE_WORKER_CACHE_NAME_PAGES = 'pages';
-export const SERVICE_WORKER_CACHE_NAME_EXPLORE_API = 'explore-api';
 export const SERVICE_WORKER_CACHE_NAME_TYPEKIT_CSS = 'typekit-fonts-stylesheets';
 export const SERVICE_WORKER_CACHE_NAME_TYPEKIT_FONTS = 'typekit-fonts-webfonts';
 export const SERVICE_WORKER_CACHE_NAME_GOOGLE_FONTS_CSS = 'google-fonts-stylesheets';
@@ -25,6 +7,3 @@ export const SERVICE_WORKER_CACHE_NAME_IMAGES = 'images';
 export const SERVICE_WORKER_CACHE_NAME_SCRIPTS = 'scripts';
 export const SERVICE_WORKER_CACHE_NAME_STYLES = 'styles';
 export const SERVICE_WORKER_CACHE_NAME_LOCAL_FONTS = 'local-webfonts';
-
-export const INDEXED_DB_MAIN_DATABASE = 'square-one';
-export const INDEXED_DB_OS_INSTALL_PROMPT = 'install-prompt';
