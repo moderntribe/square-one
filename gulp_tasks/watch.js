8a91f529cea426317f558ee539a929b9149bdb2d
David Ellenwood <davee@tri.be>
2020-04-09 10:02:23 -0500

    FF CSS Revamp: revert watch change for components. No touching! :D

--- gulp_tasks/watch.js
+++ gulp_tasks/watch.js
@@ -55,12 +55,6 @@ module.exports = {
 			`!${ pkg.square1.paths.core_admin_pcss }**/*.pcss`,
 		], gulp.parallel( 'postcss:theme' ) );
 
-		// watch the components postcss
-
-		gulp.watch( [
-			`${ pkg.square1.paths.core_theme_components }**/*.pcss`,
-		], gulp.parallel( 'postcss:themeComponents' ) );
-
 		// watch the legacy postcss
 
 		gulp.watch( [
