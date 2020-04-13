6685eb1b3285dbd12e5dabebc3556d6fe029d375
David Ellenwood <davee@tri.be>
2020-04-13 12:03:26 -0500

    FF: update search controller to use document component structure.

--- wp-content/plugins/core/src/Templates/Controllers/Pages/Search.php
+++ wp-content/plugins/core/src/Templates/Controllers/Pages/Search.php
@@ -4,18 +4,16 @@ namespace Tribe\Project\Templates\Controllers\Pages;
 
 use Tribe\Project\Templates\Component_Factory;
 use Tribe\Project\Templates\Controllers\Content;
-use Tribe\Project\Templates\Controllers\Footer\Footer_Wrap;
-use Tribe\Project\Templates\Controllers\Header\Header_Wrap;
+use Tribe\Project\Templates\Controllers\Document\Document;
 use Tribe\Project\Templates\Controllers\Header\Subheader;
 
 class Search extends Index {
 	public function __construct(
 		Component_Factory $factory,
-		Header_Wrap $header,
-		Subheader $subheader,
-		Content\Search_Item $item, // Different item template, but otherwise the same as Index
-		Footer_Wrap $footer
+		Document $document,
+		Subheader $header,
+		Content\Search_Item $item // Different item template, but otherwise the same as Index
 	) {
-		parent::__construct( $factory, $header, $subheader, $item, $footer );
+		parent::__construct( $factory, $document, $header, $item );
 	}
 }
