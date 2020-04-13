6246ea636e629e45083687fdeb9b7831ee22c55e
Jonathan Brinley <jbrinley@flightless.us>
2020-04-13 17:16:30 -0400

    move json-ld filter to core plugin

--- wp-content/themes/core/functions.php
+++ wp-content/themes/core/functions.php
@@ -1,26 +1,6 @@
 <?php
-/**
- * Theme functions
- *
- * @since core 1.0
- */
-
-
-// Core Theme Plugin
-add_action( 'after_setup_theme', 'core_theme_setup' );
 
 /**
- * Theme Core Functionality "Plugin"
+ * Nothing should be here. If you are tempted to add functions here, consult with your
+ * team to determine how best to include that functionality in the core plugin.
  */
-
-if ( ! function_exists( 'core_theme_setup' ) ):
-
-function core_theme_setup() {
-
-
-	// Remove WP SEO json-ld output in favor of the included functions
-	add_filter( 'wpseo_json_ld_output', '__return_false' );
-
-}
-
-endif; // core_theme_setup
