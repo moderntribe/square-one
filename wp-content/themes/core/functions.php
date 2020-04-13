6f368e5ade72edd3a1401a4beb9a4e4231e646ee
Jonathan Brinley <jbrinley@flightless.us>
2020-04-13 17:14:38 -0400

    remove the_tribe_image() template tag

--- wp-content/themes/core/functions.php
+++ wp-content/themes/core/functions.php
@@ -17,8 +17,6 @@ if ( ! function_exists( 'core_theme_setup' ) ):
 
 function core_theme_setup() {
 
-	// Template Tags
-	include_once 'template-tags/images.php';
 
 	// Remove WP SEO json-ld output in favor of the included functions
 	add_filter( 'wpseo_json_ld_output', '__return_false' );
