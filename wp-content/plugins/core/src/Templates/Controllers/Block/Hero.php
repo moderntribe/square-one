b6ae204b6c6a25111303f3d002d4bc62cdf31959
Samuel Estok <sam@tri.be>
2020-04-16 21:40:05 -0700

    Hero styles and code for working in gutenpanels.

--- wp-content/plugins/core/src/Templates/Controllers/Block/Hero.php
+++ wp-content/plugins/core/src/Templates/Controllers/Block/Hero.php
@@ -10,6 +10,18 @@ use Tribe\Project\Templates\Components\Panels\Accordion as Accordion_Context;
 
 class Hero extends Block_Controller {
 	public function render( string $path = '' ): string {
-		return '<pre>' . print_r( $this->attributes, true ) . '</pre>';
+		$hero_style_tag = ! empty( $this->attributes[ 'icon' ] ) && ! empty( $this->attributes[ 'icon' ][ 'url' ] ) ? ' style="background-image: url(' .$this->attributes[ 'icon' ][ 'url' ]  . ')"' : '';
+		$title = ! empty( $this->attributes[ 'title' ] ) ? '<h1 class="test-hero__title">' . $this->attributes[ 'title' ] . '</h1>' : '';
+		$description = ! empty( $this->attributes[ 'description' ] ) ? '<div class="test-hero__description">' . $this->attributes[ 'description' ] . '</div>' : '';
+		$cta = ! empty( $this->attributes[ 'cta' ] ) && ! empty( $this->attributes[ 'cta' ][ 'text' ] ) ? '<a class="test-hero__cta" href="' . $this->attributes[ 'cta' ][ 'text' ] . '">Click Here for More Clicks</a>' : '';
+		return '
+			<div class="test-hero"' . $hero_style_tag . '>
+				<div class="test-hero__content content-wrap t-content t-content--light">
+					' . $title . '
+					' . $description . '
+					' . $cta . '
+				</div>
+			</div>
+';
 	}
 }
