aa3fa2e77fbbac1d7ba83073dc3d7c1e99eff35e
Neal Fennimore <hi@neal.codes>
2020-04-17 15:16:53 -0400

    Add in icon and align support to hero

--- wp-content/plugins/core/src/Templates/Controllers/Block/Hero.php
+++ wp-content/plugins/core/src/Templates/Controllers/Block/Hero.php
@@ -3,20 +3,28 @@ declare( strict_types=1 );
 
 namespace Tribe\Project\Templates\Controllers\Block;
 
-use Tribe\Project\Blocks\Types\Accordion as Accordion_Block;
-use Tribe\Project\Blocks\Types\Accordion_Section as Accordion_Section_Block;
-use Tribe\Project\Templates\Components\Accordion as Accordion_Component;
-use Tribe\Project\Templates\Components\Panels\Accordion as Accordion_Context;
-
 class Hero extends Block_Controller {
 	public function render( string $path = '' ): string {
-		$hero_style_tag = ! empty( $this->attributes[ 'icon' ] ) && ! empty( $this->attributes[ 'icon' ][ 'url' ] ) ? ' style="background-image: url(' .$this->attributes[ 'icon' ][ 'url' ]  . ')"' : '';
 		$title = ! empty( $this->attributes[ 'title' ] ) ? '<h1 class="test-hero__title">' . $this->attributes[ 'title' ] . '</h1>' : '';
 		$description = ! empty( $this->attributes[ 'description' ] ) ? '<div class="test-hero__description">' . $this->attributes[ 'description' ] . '</div>' : '';
 		$cta = ! empty( $this->attributes[ 'cta' ] ) && ! empty( $this->attributes[ 'cta' ][ 'text' ] ) ? '<a class="test-hero__cta" href="' . $this->attributes[ 'cta' ][ 'text' ] . '">Click Here for More Clicks</a>' : '';
+		$icon = ! empty( $this->attributes[ 'icon' ] ) && ! empty( $this->attributes[ 'icon' ][ 'url' ] )
+			? '<img src="'. $this->attributes[ 'icon' ][ 'url' ] .'" />'
+			: '';
+
+		$hero_bg = ! empty( $this->attributes[ 'bg-image' ] ) && ! empty( $this->attributes[ 'bg-image' ][ 'url' ] )
+			? 'background-image: url(' . $this->attributes[ 'bg-image' ][ 'url' ]  . ');'
+			: '';
+
+		$align = ! empty( $this->attributes[ 'align' ] )
+			? 'display: flex; align-items:' . $this->attributes[ 'align' ] . '; justify-content:' . $this->attributes[ 'align' ] . ';'
+			: '';
+
+
 		return '
-			<div class="test-hero"' . $hero_style_tag . '>
+			<div class="test-hero" style="' . $hero_bg . $align . '">
 				<div class="test-hero__content content-wrap t-content t-content--light">
+					' . $icon . '
 					' . $title . '
 					' . $description . '
 					' . $cta . '
