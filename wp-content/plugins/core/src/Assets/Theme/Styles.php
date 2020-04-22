731708041545fcd1970abb633f49c449cec2ed83
David Ellenwood <davee@tri.be>
2020-04-22 14:02:23 -0500

    SQONE-173 DequeueWP Core's block styles from the FE.

--- wp-content/plugins/core/src/Assets/Theme/Styles.php
+++ wp-content/plugins/core/src/Assets/Theme/Styles.php
@@ -37,4 +37,13 @@ class Styles {
 			wp_enqueue_style( $handle );
 		}
 	}
+
+	/**
+	 * Dequeue WP Core's default block styles from the public FE.
+	 *
+	 * @action wp_enqueue_scripts
+	 */
+	public function dequeue_block_styles(): void {
+		wp_dequeue_style( 'wp-block-library' );
+	}
 }
