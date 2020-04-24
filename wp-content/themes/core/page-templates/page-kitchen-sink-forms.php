814e6c3d7640db6948cf1e288adb0da6c360ae68
Jamie Paul <jamie@tri.be>
2020-04-24 15:14:09 -0500

    stub sink forms template

---
+++ wp-content/themes/core/page-templates/page-kitchen-sink-forms.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Template Name: Kitchen Sink Forms
+ */
+
+use Tribe\Project\Templates\Controllers\Page\Page;
+
+echo tribe_template( Page::class, 'components/page/page-kitchen-sink-forms/page-kitchen-sink-forms.twig' );
