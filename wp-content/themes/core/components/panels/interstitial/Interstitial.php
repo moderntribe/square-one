360fbde493d3839ee3778156242d75e45f552a24
David Ellenwood <davee@tri.be>
2020-05-21 13:52:36 -0500

    SQONE-224 Setup template, context, & controller for Interstitial.

--- wp-content/themes/core/components/panels/interstitial/Interstitial.php
+++ wp-content/themes/core/components/panels/interstitial/Interstitial.php
@@ -5,28 +5,68 @@ namespace Tribe\Project\Templates\Components\Panels;
 
 use Tribe\Project\Templates\Components\Context;
 
+/**
+ * Class Media_Text
+ *
+ * @property string   $layout
+ * @property string   $media
+ * @property string   $content
+ * @property string[] $container_classes
+ * @property string[] $media_classes
+ * @property string[] $content_classes
+ * @property string[] $classes
+ * @property string[] $attrs
+ */
 class Interstitial extends Context {
-	public const IMAGE         = 'image';
-	public const LAYOUT        = 'layout';
-	public const COLOR         = 'text_color';
-	public const CONTENT_BLOCK = 'content_block';
+	public const LAYOUT            = 'layout';
+	public const MEDIA             = 'media';
+	public const CONTENT           = 'content';
+	public const CONTAINER_CLASSES = 'container_classes';
+	public const MEDIA_CLASSES     = 'media_classes';
+	public const CONTENT_CLASSES   = 'content_classes';
+	public const CLASSES           = 'classes';
+	public const ATTRS             = 'attrs';
 
 	protected $path = __DIR__ . '/interstitial.twig';
 
 	protected $properties = [
-		self::IMAGE         => [
+		self::LAYOUT            => [
 			self::DEFAULT => '',
 		],
-		self::LAYOUT        => [
-			self::DEFAULT       => [],
-			self::MERGE_CLASSES => [],
-		],
-		self::COLOR         => [
-			self::DEFAULT       => [],
-			self::MERGE_CLASSES => [],
-		],
-		self::CONTENT_BLOCK => [
+		self::MEDIA             => [
 			self::DEFAULT => '',
+		],
+		self::CONTENT           => [
+			self::DEFAULT => '',
+		],
+		self::CONTAINER_CLASSES => [
+			self::DEFAULT       => [],
+			self::MERGE_CLASSES => [ 'interstitial__container' ],
+		],
+		self::MEDIA_CLASSES     => [
+			self::DEFAULT       => [],
+			self::MERGE_CLASSES => [ 'interstitial__media' ],
+		],
+		self::CONTENT_CLASSES   => [
+			self::DEFAULT       => [],
+			self::MERGE_CLASSES => [ 'interstitial__content' ],
+		],
+		self::CLASSES           => [
+			self::DEFAULT       => [],
+			self::MERGE_CLASSES => [ 'c-panel', 'c-panel--interstitial' ],
+		],
+		self::ATTRS             => [
+			self::DEFAULT          => [],
+			self::MERGE_ATTRIBUTES => [],
 		],
 	];
+
+	public function get_data(): array {
+		if ( $this->layout ) {
+			$this->properties[ self::CLASSES ][ self::MERGE_CLASSES ][] = 'c-panel--layout-text-' . $this->layout;
+		}
+
+		return parent::get_data();
+	}
+
 }
