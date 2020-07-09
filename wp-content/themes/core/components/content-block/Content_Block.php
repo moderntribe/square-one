6c9388a89c0c79412d7668637e221abc700464ae
David Ellenwood <davee@tri.be>
2020-07-09 11:37:23 -0500

    Allow Content Block action component to be modified & default to a Link.

--- wp-content/themes/core/components/content-block/Content_Block.php
+++ wp-content/themes/core/components/content-block/Content_Block.php
@@ -15,26 +15,29 @@ use Tribe\Project\Components\Component;
  * @property array    $title
  * @property array    $text
  * @property array    $action
+ * @property string   $action_component
  */
 class Content_Block extends Component {
 
-	public const TAG     = 'tag';
-	public const CLASSES = 'classes';
-	public const ATTRS   = 'attrs';
-	public const LEADIN  = 'leadin';
-	public const TITLE   = 'title';
-	public const TEXT    = 'text';
-	public const ACTION  = 'action';
+	public const TAG              = 'tag';
+	public const CLASSES          = 'classes';
+	public const ATTRS            = 'attrs';
+	public const LEADIN           = 'leadin';
+	public const TITLE            = 'title';
+	public const TEXT             = 'text';
+	public const ACTION           = 'action';
+	public const ACTION_COMPONENT = 'action_component';
 
 	protected function defaults(): array {
 		return [
-			self::TAG     => 'div',
-			self::CLASSES => [ 'c-content-block' ],
-			self::ATTRS   => [],
-			self::LEADIN  => [],
-			self::TITLE   => [],
-			self::TEXT    => [],
-			self::ACTION  => [],
+			self::TAG              => 'div',
+			self::CLASSES          => [ 'c-content-block' ],
+			self::ATTRS            => [],
+			self::LEADIN           => [],
+			self::TITLE            => [],
+			self::TEXT             => [],
+			self::ACTION           => [],
+			self::ACTION_COMPONENT => 'link/Link.php',
 		];
 	}
 }
