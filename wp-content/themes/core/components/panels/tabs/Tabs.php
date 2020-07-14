15d9661fd201063570ce5e838274c68e10f51b8d
David Ellenwood <davee@tri.be>
2020-07-14 15:40:23 -0500

    SQONE-233 Setup initial block component.

--- wp-content/themes/core/components/panels/tabs/Tabs.php
+++ wp-content/themes/core/components/panels/tabs/Tabs.php
@@ -3,16 +3,44 @@ declare( strict_types=1 );
 
 namespace Tribe\Project\Templates\Components\Panels;
 
-use Tribe\Project\Templates\Components\Context;
+use Tribe\Project\Components\Component;
+use \Tribe\Project\Blocks\Types\Tabs\Tabs as Tabs_Block;
 
-class Tabs extends Context {
-	public const TABS = 'tabs';
+/**
+ * Class Tabs
+ *
+ * @property string   $layout
+ * @property string   $content
+ * @property string   $header
+ * @property string[] $container_classes
+ * @property string[] $content_classes
+ * @property string[] $classes
+ * @property string[] $attrs
+ */
+class Tabs extends Component {
 
-	protected $path = __DIR__ . '/tabs.twig';
+	public const LAYOUT            = 'layout';
+	public const CONTENT           = 'content';
+	public const HEADER            = 'header';
+	public const CONTAINER_CLASSES = 'container_classes';
+	public const CONTENT_CLASSES   = 'content_classes';
+	public const CLASSES           = 'classes';
+	public const ATTRS             = 'attrs';
 
-	protected $properties = [
-		self::TABS => [
-			self::DEFAULT => [],
-		],
-	];
+	protected function defaults(): array {
+		return [
+			self::LAYOUT            => Tabs_Block::LAYOUT_HORIZONTAL,
+			self::CONTENT           => '',
+			self::HEADER            => '',
+			self::CONTAINER_CLASSES => [ 'tabs__container' ],
+			self::CONTENT_CLASSES   => [ 'tabs__content' ],
+			self::CLASSES           => [ 'c-panel', 'c-panel--tabs' ],
+			self::ATTRS             => [],
+		];
+	}
+
+	public function init() {
+		$this->data[ self::CLASSES ][] = 'c-panel--' . $this->data[ self::LAYOUT ];
+	}
+
 }
