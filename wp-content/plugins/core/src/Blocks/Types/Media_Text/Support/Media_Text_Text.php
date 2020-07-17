6f0560eb1d64b346968440ccf32c56268c74e1a7
David Ellenwood <davee@tri.be>
2020-07-17 15:19:04 -0500

    FF: QA repairs: Media + Text Block: Revert change as it breaks the editor (500 error on save).

--- wp-content/plugins/core/src/Blocks/Types/Media_Text/Support/Media_Text_Text.php
+++ wp-content/plugins/core/src/Blocks/Types/Media_Text/Support/Media_Text_Text.php
@@ -26,7 +26,8 @@ class Media_Text_Text extends Block_Type_Config {
 			->add_class( 'media-text__title h3' );
 
 		$content = $this->factory->content()->field()->flexible_container( Media_Text::CONTENT )
-			->add_class( 'media-text__text t-sink s-sink' ); // @TODO: This doesn't appear to work.
+			->add_class( 'media-text__text t-sink s-sink' ) // @TODO: This doesn't appear to work.
+			->add_template_block( 'core/paragraph' );
 
 		foreach ( $this->nested_block_types() as $type ) {
 			$content->add_block_type( $type )->capture_nested_content( $type, 'content' );
