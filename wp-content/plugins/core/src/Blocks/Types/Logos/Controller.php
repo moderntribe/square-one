fc25430da7dc0f3c063c7775c385b0e319f5e6c9
David Ellenwood <davee@tri.be>
2020-07-23 14:36:21 -0500

    SQONE-242 Handle the controller, component, & template.

---
+++ wp-content/plugins/core/src/Blocks/Types/Logos/Controller.php
@@ -0,0 +1,111 @@
+<?php
+declare( strict_types=1 );
+
+namespace Tribe\Project\Blocks\Types\Logos;
+
+use Tribe\Project\Blocks\Types\Logos\Logos as Logos_Block;
+use Tribe\Project\Blocks\Types\Logos\Support\Logo;
+use Tribe\Project\Controllers\Blocks\Block_Controller;
+use Tribe\Project\Templates\Components\Content_Block;
+use Tribe\Project\Templates\Components\Image as Image_Component;
+use Tribe\Project\Templates\Components\Link;
+use Tribe\Project\Templates\Components\Panels\Logos as Container;
+use Tribe\Project\Templates\Components\Text;
+use Tribe\Project\Templates\Models\Image;
+
+class Controller extends Block_Controller {
+
+	public function render( $attributes, $content, $block_type ) {
+		$this->attributes = $attributes;
+		$this->content    = $content;
+		$this->block_type = $block_type;
+
+		$args = [
+			Container::LOGOS  => $this->get_logos(),
+			Container::HEADER => $this->get_header(),
+		];
+
+		$this->render_component( 'panels/logos/Logos.php', $args );
+	}
+
+	private function get_logos(): array {
+		if ( empty( $this->attributes[ Logos_Block::LOGOS ] ) ) {
+			return [];
+		}
+
+		return array_map( function ( $logo ) {
+			if ( empty( $logo[ Logo::IMAGE ] ) ) {
+				return [];
+			}
+
+			$logo_row = [
+				Image_Component::ATTACHMENT      => Image::factory( (int) $logo[ Logo::IMAGE ]['id'] ),
+				Image_Component::USE_LAZYLOAD    => true,
+				Image_Component::WRAPPER_CLASSES => [ 'logo__figure' ],
+				Image_Component::IMG_CLASSES     => [ 'logo__img' ],
+				Image_Component::SRC_SIZE        => 'large',
+				Image_Component::SRCSET_SIZES    => [ 'medium', 'large' ],
+			];
+
+			$link = wp_parse_args( $logo[ Logo::LINK ] ?? [], [
+				'text'   => '',
+				'url'    => '',
+				'target' => '',
+			] );
+
+			if ( ! empty( $logo[ Logo::LINK ]['url'] ) ) {
+				$logo_row[ Image_Component::LINK_URL ]     = $link['url'];
+				$logo_row[ Image_Component::LINK_TARGET ]  = $link['target'];
+				$logo_row[ Image_Component::LINK_CLASSES ] = [ 'logo__link' ];
+				$logo_row[ Image_Component::LINK_ATTRS ]   = ! empty( $link['text'] ) ? [ 'aria-label' => $link['text'] ] : [];
+			}
+
+			return $logo_row;
+		}, $this->attributes[ Logos_Block::LOGOS ] );
+	}
+
+	private function get_header(): array {
+		return [
+			Content_Block::CLASSES => [ 'logos__content-container' ],
+			Content_Block::TITLE   => $this->get_headline(),
+			Content_Block::TEXT    => $this->get_text(),
+			Content_Block::ACTION  => $this->get_cta(),
+		];
+	}
+
+	private function get_headline(): array {
+		return [
+			Text::TAG     => 'h2',
+			Text::CLASSES => [ 'logos__title', 'h2' ],
+			Text::TEXT    => $this->attributes[ Logos_Block::TITLE ] ?? '',
+		];
+	}
+
+	private function get_text(): array {
+		return [
+			Text::CLASSES => [ 'logos__description', 't-sink', 's-sink' ],
+			Text::TEXT    => $this->attributes[ Logos_Block::DESCRIPTION ] ?? '',
+		];
+	}
+
+	private function get_cta(): array {
+		$cta = wp_parse_args( $this->attributes['cta'] ?? [], [
+			'text'   => '',
+			'url'    => '',
+			'target' => '',
+		] );
+
+		if ( empty( $cta[ 'url' ] ) ) {
+			return [];
+		}
+
+		return [
+			Link::URL             => $cta['url'],
+			Link::CONTENT         => $cta['text'] ?: $cta['url'],
+			Link::TARGET          => $cta['target'],
+			Link::CLASSES         => [ 'a-btn', 'a-btn--has-icon-after', 'icon-arrow-right' ],
+			Link::WRAPPER_TAG     => 'p',
+			Link::WRAPPER_CLASSES => [ 'logos__cta' ],
+		];
+	}
+}
