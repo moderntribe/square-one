a8ba48b292f7a89dfa4395c83db3eb4b70546524
David Ellenwood <davee@tri.be>
2020-07-24 15:30:23 -0500

    SQONE-242 Add some classes to the block to make it "usable" in the EditUI.

--- wp-content/plugins/core/src/Blocks/Types/Logos/Support/Logo.php
+++ wp-content/plugins/core/src/Blocks/Types/Logos/Support/Logo.php
@@ -19,13 +19,17 @@ class Logo extends Block_Type_Config {
 			->set_label( __( 'Logo', 'tribe' ) )
 			->set_dashicon( 'menu-alt' )
 			->set_parents( Logos::NAME )
+			->add_class( 'logo' )
 			->add_content_section( $this->content_area() )
 			->build();
 	}
 
 	private function content_area(): Content_Section {
-		$image = $this->factory->content()->field()->image( self::IMAGE );
-		$link  = $this->factory->content()->field()->link( self::LINK );
+		$image = $this->factory->content()->field()->image( self::IMAGE )
+					->add_class( 'logo__figure logo__img' );
+
+		$link  = $this->factory->content()->field()->link( self::LINK )
+					->add_class( 'logo__link' );
 
 
 		return $this->factory->content()->section()
