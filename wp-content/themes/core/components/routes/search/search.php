552fdf895c90dc16ccc466c7cf5ec63928d5d262
Nick Pelton <nickpelton@gmail.com>
2020-07-31 15:42:44 -0500

    de-twig Search content search_item component

--- wp-content/themes/core/components/routes/search/search.php
+++ wp-content/themes/core/components/routes/search/search.php
@@ -13,9 +13,10 @@ $controller->render_header();
 
 			<?php
 			if( have_posts() ) {
+				echo "Results";
 				while ( have_posts() ) {
 					the_post();
-					get_template_part( 'components/content/search/search', 'search' );
+					get_template_part( 'components/content/search_item/search_item', 'search' );
 				}
 				get_template_part( 'components/pagination/loop/loop', 'search' );
 			}else{
