c504f8705e60bb900dcbf419e87edef5dc5e7e39
Nick Pelton <nickpelton@gmail.com>
2020-08-03 15:06:28 -0500

    Route Post de-twig. Add single content component.

--- wp-content/themes/core/components/routes/single/single.php
+++ wp-content/themes/core/components/routes/single/single.php
@@ -1,38 +1,21 @@
 <?php
 declare( strict_types=1 );
-
 $controller = \Tribe\Project\Templates\Components\routes\single\Controller::factory();
 
-$controller->render_header();
+get_template_part( 'components/document/header/header', 'single' );
 ?>
 	<main id="main-content">
-		<?php $controller->render_breadcrumbs(); ?>
 
-		<?php get_template_part( 'components/header/subheader/subheader' ); ?>
+		<?php get_template_part( 'components/breadcrumbs/breadcrumbs', null, [ 'breadcrumbs' => $controller->get_breadcrumbs() ] ) ?>
+
+		<?php get_template_part( 'components/header/subheader/subheader' ) ?>
 
 		<div class="l-container">
 
-			<?php
-			if ( have_posts() ) :
-
-				while ( have_posts() ) :
-
-					the_post();
-					get_template_part( 'components/content/loop_items/single/single', 'single' );
-
-				endwhile;
-
-				get_template_part( 'components/pagination/loop/loop', 'single' );
-
-			else :
-
-				get_template_part( 'components/no_results/no_results', 'single' );
-
-			endif;
-			?>
+			<?php get_template_part( 'components/content/loop_items/single/single', null ) ?>
 
 		</div>
 	</main>
 <?php
-$controller->render_sidebar();
+get_template_part( 'components/sidebar/sidebar', 'single', [ 'sidebar_id' => $controller->get_sidebar() ] );
 $controller->render_footer();
