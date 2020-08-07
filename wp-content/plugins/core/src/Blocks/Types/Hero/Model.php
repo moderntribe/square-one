2731b72f9ad35473d3eb431989b998b2f7f63771
Mike Sawicki <mikes@tri.be>
2020-08-07 09:59:09 -0400

    Alternative that pushed more responsibility of component building to theme Controller

--- wp-content/plugins/core/src/Blocks/Types/Hero/Model.php
+++ wp-content/plugins/core/src/Blocks/Types/Hero/Model.php
@@ -4,22 +4,21 @@ declare( strict_types=1 );
 namespace Tribe\Project\Blocks\Types\Hero;
 
 use Tribe\Project\Blocks\Types\Base_Model;
-use Tribe\Project\Templates\Components\Content_Block;
-use Tribe\Project\Templates\Models\Image;
-use Tribe\Project\Theme\Config\Image_Sizes;
 
 class Model extends Base_Model {
+	const LAYOUT  = 'layout';
+	const MEDIA   = 'media';
+	const CONTENT = 'content';
+	const LEAD_IN = 'leadin';
+	const TITLE   = 'title';
+	const TEXT    = 'text';
+	const CTA     = 'cta';
 
-	public function get_data() {
+	public function get_data(): array {
 		return [
-			'layout'            => $this->get_layout(),
-			'media'             => $this->get_media(),
-			'content'           => $this->get_content(),
-			'container_classes' => [ 'b-hero__container', 'l-container' ],
-			'media_classes'     => [ 'b-hero__media' ],
-			'content_classes'   => [ 'b-hero__content' ],
-			'classes'           => [ 'c-block', 'c-block--full-bleed', 'b-hero' ],
-			'attr'              => [],
+			self::LAYOUT  => $this->get_layout(),
+			self::MEDIA   => $this->get_media(),
+			self::CONTENT => $this->get_content(),
 		];
 	}
 
@@ -31,35 +30,11 @@ class Model extends Base_Model {
 	}
 
 	/**
-	 * @return array
+	 * @return int|bool
 	 */
-	private function get_media(): array {
-		if ( ! $this->get( Hero::IMAGE ) ) {
-			return [];
-		}
+	private function get_media() {
+		return $this->get( Hero::IMAGE, false );
 
-		return $this->get_image( $this->get( Hero::IMAGE ) );
-	}
-
-	/**
-	 * @param $attachment_id
-	 *
-	 * @return array
-	 */
-	private function get_image( $attachment_id ): array {
-		return [
-			'attachment'    => Image::factory( (int) $attachment_id ),
-			'as_bg'         => true,
-			'use_lazyload'  => true,
-			'wrapper_tag'   => 'div',
-			'wrapper_class' => [ 'b-hero__figure' ],
-			'image_classes' => [ 'b-hero__img', 'c-image__bg' ],
-			'src_size'      => Image_Sizes::CORE_FULL,
-			'srcset_size'   => [
-				Image_Sizes::CORE_FULL,
-				Image_Sizes::CORE_MOBILE,
-			],
-		];
 	}
 
 	/**
@@ -67,68 +42,15 @@ class Model extends Base_Model {
 	 */
 	private function get_content(): array {
 		return [
-			'classes' => [ 'b-hero__content-container', 't-theme--light' ],
-			'leadin'  => $this->get_leadin(),
-			'title'   => $this->get_headline(),
-			'text'    => $this->get_text(),
-			'action'  => $this->get_cta(),
-			'layout'  => $this->get_layout(),
+			self::LEAD_IN => $this->get( Hero::LEAD_IN, '' ),
+			self::TITLE   => $this->get( Hero::TITLE, '' ),
+			self::TEXT    => $this->get( Hero::DESCRIPTION, '' ),
+			self::CTA     => wp_parse_args( $this->get( Hero::CTA, [] ), [
+				'title'  => '',
+				'url'    => '',
+				'target' => '',
+			] ),
 		];
 	}
 
-	/**
-	 * @return array
-	 */
-	private function get_leadin(): array {
-		return [
-			'tag'     => 'p',
-			'classes' => [ 'b-hero__leadin', 'h6' ],
-			'content' => $this->get( Hero::LEAD_IN, '' ),
-		];
-	}
-
-	/**
-	 * @return array
-	 */
-	private function get_headline(): array {
-		return [
-			'tag'     => 'h2',
-			'classes' => [ 'b-hero__title', 'h1' ],
-			'content' => $this->get( Hero::TITLE, '' ),
-		];
-	}
-
-	/**
-	 * @return array
-	 */
-	private function get_text(): array {
-		return [
-			'classes' => [ 'b-hero__description', 't-sink', 's-sink' ],
-			'content' => $this->get( Hero::DESCRIPTION, '' ),
-		];
-	}
-
-	/**
-	 * @return array
-	 */
-	private function get_cta(): array {
-		$cta = wp_parse_args( $this->get( Hero::CTA, [] ), [
-			'title'  => '',
-			'url'    => '',
-			'target' => '',
-		] );
-
-		if ( empty( $cta[ 'url' ] ) ) {
-			return [];
-		}
-
-		return [
-			'url'             => $cta[ 'url' ],
-			'content'         => $cta[ 'title' ] ? : $cta[ 'url' ],
-			'target'          => $cta[ 'target' ],
-			'classes'         => [ 'a-btn', 'a-btn--has-icon-after', 'icon-arrow-right' ],
-			'wrapper_tag'     => 'p',
-			'wrapper_classes' => [ 'b-hero__cta' ],
-		];
-	}
 }
--- wp-content/themes/core/components/blocks/hero/Controller.php
+++ wp-content/themes/core/components/blocks/hero/Controller.php
@@ -5,7 +5,11 @@ namespace Tribe\Project\Templates\Components\blocks\hero;
 
 use Tribe\Libs\Utils\Markup_Utils;
 use Tribe\Project\Blocks\Types\Hero\Hero as Hero_Block;
+use Tribe\Project\Blocks\Types\Hero\Hero;
+use Tribe\Project\Blocks\Types\Hero\Model;
 use Tribe\Project\Templates\Components\Abstract_Controller;
+use Tribe\Project\Templates\Models\Image;
+use Tribe\Project\Theme\Config\Image_Sizes;
 
 /**
  * Class Hero
@@ -58,8 +62,8 @@ class Controller extends Abstract_Controller {
 	 */
 	public function __construct( array $args = [] ) {
 		$this->layout            = $args[ 'layout' ] ?? Hero_Block::LAYOUT_LEFT;
-		$this->media             = $args[ 'media' ] ?? '';
-		$this->content           = $args[ 'content' ] ?? '';
+		$this->media             = $this->get_media( $args );
+		$this->content           = $this->get_content( $args );
 		$this->container_classes = (array) ( $args[ 'container_classes' ] ?? [ 'b-hero__container', 'l-container' ] );
 		$this->media_classes     = (array) ( $args[ 'media_classes' ] ?? [ 'b-hero__media' ] );
 		$this->content_classes   = (array) ( $args[ 'content_classes' ] ?? [ 'b-hero__content' ] );
@@ -70,6 +74,55 @@ class Controller extends Abstract_Controller {
 				'c-block--' . $this->layout,
 			] );
 		$this->attrs             = (array) ( $args[ 'attrs' ] ?? [] );
+	}
+
+	protected function get_content( $args ) {
+		if ( ! isset( $args[ 'content' ] ) || empty( $args[ 'content' ] ) ) {
+			return [];
+		}
+
+		return [
+			'classes' => [ 'b-hero__content-container', 't-theme--light' ],
+			'leadin'  => $args['content'][Model::LEAD_IN] ?? '',
+			'title'   => $args['content'][Model::TITLE] ?? '',
+			'text'    => $args['content'][Model::TEXT] ?? '',
+			'action'  => $args['content'][Model::CTA] ?? [],
+			'layout'  => $this->layout,
+		];
+	}
+
+	/**
+	 * @param $args
+	 *
+	 * @return array
+	 */
+	protected function get_media( $args ) {
+		if ( ! isset( $args[ 'media' ] ) ) {
+			return [];
+		}
+
+		return $this->get_image( $args[ 'media' ] );
+	}
+
+	/**
+	 * @param $attachment_id
+	 *
+	 * @return array
+	 */
+	protected function get_image( $attachment_id ): array {
+		return [
+			'attachment'    => Image::factory( (int) $attachment_id ),
+			'as_bg'         => true,
+			'use_lazyload'  => true,
+			'wrapper_tag'   => 'div',
+			'wrapper_class' => [ 'b-hero__figure' ],
+			'image_classes' => [ 'b-hero__img', 'c-image__bg' ],
+			'src_size'      => Image_Sizes::CORE_FULL,
+			'srcset_size'   => [
+				Image_Sizes::CORE_FULL,
+				Image_Sizes::CORE_MOBILE,
+			],
+		];
 	}
 
 	public function container_classes(): string {
