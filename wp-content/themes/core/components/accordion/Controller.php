6b19d4d0ede4c426480510f4554f3632587ca8ea
Mike Sawicki <mikes@tri.be>
2020-08-10 16:30:18 -0400

    CR feedback

--- wp-content/themes/core/components/accordion/Controller.php
+++ wp-content/themes/core/components/accordion/Controller.php
@@ -59,6 +59,11 @@ class Controller extends Abstract_Controller {
 	/**
 	 * @var string[]
 	 */
+	public $row_header_attrs;
+
+	/**
+	 * @var string[]
+	 */
 	public $row_header_container_classes;
 
 	/**
@@ -69,7 +74,17 @@ class Controller extends Abstract_Controller {
 	/**
 	 * @var string[]
 	 */
+	public $row_content_attrs;
+
+	/**
+	 * @var string[]
+	 */
 	public $row_content_container_classes;
+
+	/**
+	 * @var string[]
+	 */
+	public $row_content_container_attrs;
 
 	/**
 	 * @var string
@@ -82,20 +97,26 @@ class Controller extends Abstract_Controller {
 	public $row_content_name;
 
 	public function __construct( array $args ) {
-		$args                                = wp_parse_args( $args, $this->defaults() );
+		$args                                = $this->get_args( $args );
 		$this->rows                          = (array) $args[ 'rows' ];
 		$this->container_classes             = (array) $args[ 'container_classes' ];
 		$this->container_attrs               = (array) $args[ 'container_attrs' ];
 		$this->row_header_tag                = $args[ 'row_header_tag' ];
 		$this->row_classes                   = (array) $args[ 'row_classes' ];
 		$this->row_header_classes            = (array) $args[ 'row_header_classes' ];
+		$this->row_header_attrs              = (array) $args[ 'row_header_attrs' ];
 		$this->row_header_container_classes  = (array) $args[ 'row_header_container_classes' ];
 		$this->row_content_classes           = (array) $args[ 'row_content_classes' ];
+		$this->row_content_attrs             = (array) $args[ 'row_content_attrs' ];
 		$this->row_content_container_classes = (array) $args[ 'row_content_container_classes' ];
+		$this->row_content_container_attrs   = (array) $args[ 'row_content_container_attrs' ];
 		$this->row_header_name               = $args[ 'row_header_name' ];
 		$this->row_content_name              = $args[ 'row_content_name' ];
 	}
 
+	/**
+	 * @return array
+	 */
 	protected function defaults(): array {
 		return [
 			'rows'                          => [],
@@ -104,38 +125,133 @@ class Controller extends Abstract_Controller {
 			'row_header_tag'                => 'h3',
 			'row_classes'                   => [ 'c-accordion__row' ],
 			'row_header_classes'            => [ 'c-accordion__header', 'h5' ],
+			'row_header_attrs'              => [],
 			'row_header_container_classes'  => [ 'c-accordion__header-container' ],
 			'row_content_classes'           => [ 'c-accordion__content' ],
+			'row_content_classes_attrs'     => [],
+			'row_content_container_attrs'   => [],
 			'row_content_container_classes' => [ 'c-accordion__content-container', 't-sink', 's-sink' ],
 			'row_header_name'               => 'title',
 			'row_content_name'              => 'row_content',
 		];
 	}
 
+	/**
+	 * @return array
+	 */
+	protected function required(): array {
+		return [
+			'container_attrs'             => [
+				'role'                 => 'tablist',
+				'aria-multiselectable' => 'true',
+			],
+			'row_header_attrs'            => [
+				'aria-expanded' => 'false',
+				'aria-selected' => 'false',
+				'role'          => 'tab',
+			],
+			'row_content_attrs'           => [
+				'hidden'      => 'true',
+				'aria-hidden' => 'true',
+				'role'        => 'tabpanel',
+			],
+			'row_content_container_attrs' => [
+				'data-depth' => '0',
+				'data-autop' => 'true',
+			],
+		];
+	}
+
+	/**
+	 * @param array $args
+	 *
+	 * @return array
+	 */
+	protected function get_args( array $args ): array {
+		$args = wp_parse_args( $args, $this->defaults() );
+		foreach ( $this->required() as $key => $value ) {
+			$args[ $key ] = array_merge( $args[ $key ], $value );
+		}
+
+		return $args;
+	}
+
+	/**
+	 * @param array $row
+	 *
+	 * @return string
+	 */
+	public function row_content_attrs( array $row ): string {
+		return Markup_Utils::concat_attrs( array_merge( [
+			'id'              => esc_attr( $row[ 'content_id' ] ),
+			'aria-labelledby' => esc_attr( $row[ 'header_id' ] ),
+		], $this->row_content_attrs ) );
+	}
+
+	/**
+	 * @return string
+	 */
 	public function container_classes(): string {
 		return Markup_Utils::class_attribute( $this->container_classes );
 	}
 
+	/**
+	 * @return string
+	 */
 	public function container_attrs(): string {
-		return Markup_Utils::class_attribute( $this->container_attrs );
+		return Markup_Utils::concat_attrs( $this->container_attrs );
 	}
 
+	/**
+	 * @return string
+	 */
 	public function row_classes(): string {
 		return Markup_Utils::class_attribute( $this->row_classes );
 	}
 
+	/**
+	 * @return string
+	 */
 	public function row_header_classes(): string {
 		return Markup_Utils::class_attribute( $this->row_header_classes );
 	}
 
+	/**
+	 * @return string
+	 */
+	public function row_content_container_attrs() {
+		return Markup_Utils::concat_attrs( $this->row_content_container_attrs );
+	}
+
+	/**
+	 * @param array row
+	 *
+	 * @return string
+	 */
+	public function row_header_attrs( array $row ): string {
+		return Markup_Utils::concat_attrs( array_merge( [
+			'aria-controls' => esc_attr( $row[ 'content_id' ] ),
+			'id'            => esc_attr( $row[ 'header_id' ] ),
+		], $this->row_header_attrs ) );
+	}
+
+	/**
+	 * @return string
+	 */
 	public function row_header_container_classes(): string {
 		return Markup_Utils::class_attribute( $this->row_header_container_classes );
 	}
 
+	/**
+	 * @return string
+	 */
 	public function row_content_classes(): string {
 		return Markup_Utils::class_attribute( $this->row_content_classes );
 	}
 
+	/**
+	 * @return string
+	 */
 	public function row_content_container_classes(): string {
 		return Markup_Utils::class_attribute( $this->row_content_container_classes );
 	}
--- wp-content/themes/core/components/accordion/accordion.php
+++ wp-content/themes/core/components/accordion/accordion.php
@@ -1,55 +1,26 @@
 <?php
 // phpcs:ignore VariableAnalysis.CodeAnalysis.VariableAnalysis.UndefinedVariable
-$controller = \Tribe\Project\Templates\Components\accordion\Controller::factory( $args );
+$c = \Tribe\Project\Templates\Components\accordion\Controller::factory( $args );
 ?>
-<div
-	<?php echo $controller->container_classes(); ?>
-		role="tablist"
-		aria-multiselectable="true"
-	<?php echo $controller->container_attrs(); ?>
->
-	<?php foreach ( $controller->rows as $key => $row ) { ?>
-	<article <?php echo $controller->row_classes(); ?>>
-		<<?php echo esc_attr( $controller->row_header_tag ); ?>>
-		<button
-				aria-controls="<?php echo esc_attr( $row[ 'content_id' ] ); ?>"
-				aria-expanded="false"
-				aria-selected="false"
-			<?php echo $controller->row_header_classes(); ?>
-				id="<?php echo esc_attr( $row[ 'header_id' ] ); ?>"
-				data-index="<?php echo absint( $key ); ?>"
-				role="tab"
-		>
-				<span
-					<?php echo $controller->row_header_container_classes(); ?>
-					data-depth="0"
-					data-name="<?php echo esc_attr( $controller->row_header_name ); ?>"
-					data-index="<?php echo absint( $key ); ?>"
-					data-livetext
-				>
-					<?php echo $row[ 'header_text' ]; ?>
-				</span>
-		</button>
-		</<?php echo esc_attr( $controller->row_header_tag ); ?>>
-		<div
-				hidden
-				aria-hidden="true"
-				aria-labelledby="<?php echo esc_attr( $row[ 'header_id' ] ); ?>"
-			<?php echo $controller->row_content_classes(); ?>
-				id="<?php echo esc_attr( $row[ 'content_id' ] ); ?>"
-				role="tabpanel"
-		>
-			<div
-				<?php echo $controller->row_content_container_classes(); ?>
-					data-depth="0"
-					data-name="<?php echo esc_attr( $controller->row_content_name ); ?>"
-					data-index="<?php echo absint( $key ); ?>"
-					data-autop="true"
-					data-livetext
-			>
+<div <?php echo $c->container_classes(); ?>
+	<?php echo $c->container_attrs(); ?>>
+	<?php foreach ( $c->rows as $key => $row ) { ?>
+	<article <?php echo $c->row_classes(); ?>>
+		<<?php echo esc_attr( $c->row_header_tag ); ?>>
+			<button <?php echo $c->row_header_classes(); ?>
+				<?php echo $c->row_header_attrs($row); ?>>
+					<span <?php echo $c->row_header_container_classes(); ?>>
+						<?php echo $row[ 'header_text' ]; ?>
+					</span>
+			</button>
+		</<?php echo esc_attr( $c->row_header_tag ); ?>>
+		<div <?php echo $c->row_content_classes(); ?>
+			<?php echo $c->row_content_attrs($row); ?>>
+			<div <?php echo $c->row_content_container_classes(); ?>
+				<?php echo $c->row_content_container_attrs(); ?>>
 				<?php echo $row[ 'content' ]; ?>
 			</div>
 		</div>
-		</article>
+	</article>
 	<?php } ?>
 </div>
