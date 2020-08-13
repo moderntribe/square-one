42b5bc542dc31b81ccff0b530e144e9284fbefd0
Mike Sawicki <mikes@tri.be>
2020-08-13 12:18:00 -0400

    Changes consts

--- wp-content/plugins/core/src/Blocks/Types/Hero/Model.php
+++ wp-content/plugins/core/src/Blocks/Types/Hero/Model.php
@@ -6,19 +6,12 @@ namespace Tribe\Project\Blocks\Types\Hero;
 use Tribe\Project\Blocks\Types\Base_Model;
 
 class Model extends Base_Model {
-	const LAYOUT  = 'layout';
-	const MEDIA   = 'media';
-	const CONTENT = 'content';
-	const LEAD_IN = 'leadin';
-	const TITLE   = 'title';
-	const TEXT    = 'text';
-	const CTA     = 'cta';
 
 	public function get_data(): array {
 		return [
-			self::LAYOUT  => $this->get_layout(),
-			self::MEDIA   => $this->get_media(),
-			self::CONTENT => $this->get_content(),
+			'layout'  => $this->get_layout(),
+			'media'   => $this->get_media(),
+			'content' => $this->get_content(),
 		];
 	}
 
@@ -41,10 +34,10 @@ class Model extends Base_Model {
 	 */
 	private function get_content(): array {
 		return [
-			self::LEAD_IN => $this->get( Hero::LEAD_IN, '' ),
-			self::TITLE   => $this->get( Hero::TITLE, '' ),
-			self::TEXT    => $this->get( Hero::DESCRIPTION, '' ),
-			self::CTA     => wp_parse_args( $this->get( Hero::CTA, [] ), [
+			Hero::LEAD_IN     => $this->get( Hero::LEAD_IN, '' ),
+			Hero::TITLE       => $this->get( Hero::TITLE, '' ),
+			Hero::DESCRIPTION => $this->get( Hero::DESCRIPTION, '' ),
+			Hero::CTA         => wp_parse_args( $this->get( Hero::CTA, [] ), [
 				'title'  => '',
 				'url'    => '',
 				'target' => '',
