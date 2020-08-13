6ccb3a1e6b91950c67fa958c7e46b3c762fd1052
Mike Sawicki <mikes@tri.be>
2020-08-13 10:22:38 -0400

    Adds space

--- wp-content/themes/core/components/content/single/index/index.php
+++ wp-content/themes/core/components/content/single/index/index.php
@@ -30,7 +30,7 @@ $c = \Tribe\Project\Templates\Components\content\single\index\Controller::factor
 
 		</ul>
 
-		<?php get_template_part('components/share/share') ?>
+		<?php get_template_part( 'components/share/share' ) ?>
 
 	</footer>
 
