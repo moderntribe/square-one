94203735668252b2011ab6bb1267ed26a6931cf5
Jonathan Brinley <jbrinley@flightless.us>
2020-08-15 15:10:52 -0400

    method to consistently parse args in template controllers

--- wp-content/plugins/core/src/Templates/Components/Abstract_Controller.php
+++ wp-content/plugins/core/src/Templates/Components/Abstract_Controller.php
@@ -4,6 +4,25 @@ declare( strict_types=1 );
 namespace Tribe\Project\Templates\Components;
 
 abstract class Abstract_Controller {
+
+	protected function parse_args( array $args ): array {
+		$args = wp_parse_args( $args, $this->defaults() );
+
+		foreach ( $this->required() as $key => $value ) {
+			$args[$key] = array_merge( $args[$key], $value );
+		}
+
+		return $args;
+	}
+
+	protected function defaults(): array {
+		return [];
+	}
+
+	protected function required(): array {
+		return [];
+	}
+
 	/**
 	 * @param array $args
 	 *
