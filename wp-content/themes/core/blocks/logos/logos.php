b28d198531d1237ad0951d9fb5ac3d2535d8eedd
Mike Sawicki <mikes@tri.be>
2020-08-19 07:11:03 -0400

    General cleanup/formatting

--- wp-content/themes/core/blocks/logos/logos.php
+++ wp-content/themes/core/blocks/logos/logos.php
@@ -1,5 +1,9 @@
 <?php
 declare( strict_types=1 );
 
-$model = new \Tribe\Project\Blocks\Types\Logos\LogosModel( $args['block'] );// phpcs:ignore VariableAnalysis.CodeAnalysis.VariableAnalysis.UndefinedVariable
+use \Tribe\Project\Blocks\Types\Logos\Logos_Model;
+
+// phpcs:ignore VariableAnalysis.CodeAnalysis.VariableAnalysis.UndefinedVariable
+$model = new Logos_Model( $args[ 'block' ] );
+
 get_template_part( 'components/blocks/logos/logos', null, $model->get_data() );
