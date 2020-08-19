fedc0a4cb2579deefe23093313bf9902d8e03bd2
Mike Sawicki <mikes@tri.be>
2020-08-19 16:01:46 -0400

    Phpdoc and formatting

--- wp-content/themes/core/blocks/accordion/accordion.php
+++ wp-content/themes/core/blocks/accordion/accordion.php
@@ -3,6 +3,9 @@ declare( strict_types=1 );
 
 use Tribe\Project\Blocks\Types\Accordion\Accordion_Model;
 
+/**
+ * @var array $args Arguments passed to the template
+ */
 // phpcs:ignore VariableAnalysis.CodeAnalysis.VariableAnalysis.UndefinedVariable
 $model = new Accordion_Model( $args[ 'block' ] );
 get_template_part(
--- wp-content/themes/core/blocks/quote/quote.php
+++ wp-content/themes/core/blocks/quote/quote.php
@@ -3,12 +3,9 @@
 declare( strict_types=1 );
 
 use Tribe\Project\Blocks\Types\Quote\Quote_Model;
-
+/**
+ * @var array $args Arguments passed to the template
+ */
 // phpcs:ignore VariableAnalysis.CodeAnalysis.VariableAnalysis.UndefinedVariable
 $model = new Quote_Model( $args[ 'block' ] );
-
-get_template_part(
-	'components/blocks/quote/quote',
-	null,
-	$model->get_data()
-);
+get_template_part( 'components/blocks/quote/quote', null, $model->get_data() );
