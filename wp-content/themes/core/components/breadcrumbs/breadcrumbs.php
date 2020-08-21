0910be33735f4d30af57f97433635680781f4554
Mike Sawicki <mikes@tri.be>
2020-08-21 15:06:55 -0400

    implements constants and method name changes

--- wp-content/themes/core/components/breadcrumbs/breadcrumbs.php
+++ wp-content/themes/core/components/breadcrumbs/breadcrumbs.php
@@ -1,23 +1,24 @@
 <?php
 declare( strict_types=1 );
+use Tribe\Project\Templates\Components\breadcrumbs\Breadcrumbs_Controller;
 
 /**
  * @var array $args Arguments passed to the template
  */
 // phpcs:ignore VariableAnalysis.CodeAnalysis.VariableAnalysis.UndefinedVariable
-$c = \Tribe\Project\Templates\Components\breadcrumbs\Breadcrumbs_Controller::factory( $args );
+$c = Breadcrumbs_Controller::factory( $args );
 
 ?>
 <div
-	<?php echo $c->classes(); ?>
-	<?php echo $c->attributes(); ?>
+	<?php echo $c->get_classes(); ?>
+	<?php echo $c->get_attrs(); ?>
 >
 
-	<ul <?php echo $c->main_classes(); ?> <?php echo $c->main_attrs(); ?>>
+	<ul <?php echo $c->get_main_classes(); ?> <?php echo $c->get_main_attrs(); ?>>
 
-		<?php foreach ( $c->items() as $item ) { ?>
-			<li <?php echo $c->item_classes(); ?> <?php echo $c->item_attrs(); ?>>
-				<a href="<?php echo esc_url( $item->url ); ?>" <?php echo $c->link_classes(); ?> <?php echo $c->link_attrs(); ?>>
+		<?php foreach ( $c->get_items() as $item ) { ?>
+			<li <?php echo $c->get_item_classes(); ?> <?php echo $c->get_item_attrs(); ?>>
+				<a href="<?php echo esc_url( $item->url ); ?>" <?php echo $c->get_link_classes(); ?> <?php echo $c->get_link_attrs(); ?>>
 					<?php echo esc_html( $item->label ); ?>
 				</a>
 			</li>
