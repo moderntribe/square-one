e3b299aa3f83873003b5f280e1bf43b3093a130c
Mike Sawicki <mikes@tri.be>
2020-08-21 11:05:08 -0400

    phpcbf

--- wp-content/themes/core/components/blocks/accordion/accordion.php
+++ wp-content/themes/core/components/blocks/accordion/accordion.php
@@ -14,8 +14,9 @@ $c = Accordion_Block_Controller::factory( $args );
 	<div <?php echo $c->get_container_classes(); ?>>
 
 		<?php get_template_part(
-				'components/content_block/content_block',
-				null, $c->get_header_args()
+			'components/content_block/content_block',
+			null,
+			$c->get_header_args()
 		); ?>
 
 		<div <?php echo $c->get_content_classes(); ?>>
