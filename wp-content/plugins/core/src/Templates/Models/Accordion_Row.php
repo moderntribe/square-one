9c5195b1de6629190aa2724894d7f458eb132a6d
Mike Sawicki <mikes@tri.be>
2020-08-21 10:24:32 -0400

    remove unique ids

--- wp-content/plugins/core/src/Templates/Models/Accordion_Row.php
+++ wp-content/plugins/core/src/Templates/Models/Accordion_Row.php
@@ -11,16 +11,6 @@ class Accordion_Row {
 	/**
 	 * @var string
 	 */
-	public $header_id;
-
-	/**
-	 * @var string
-	 */
-	public $content_id;
-
-	/**
-	 * @var string
-	 */
 	public $content;
 
 	/**
@@ -28,18 +18,12 @@ class Accordion_Row {
 	 *
 	 * @param string $header_text
 	 * @param string $content
-	 * @param string $header_id
-	 * @param string $content_id
 	 */
 	public function __construct(
 		string $header_text,
-		string $content,
-		string $header_id,
-		string $content_id
+		string $content
 	) {
 		$this->header_text = $header_text;
 		$this->content     = $content;
-		$this->header_id   = $header_id;
-		$this->content_id  = $content_id;
 	}
 }
