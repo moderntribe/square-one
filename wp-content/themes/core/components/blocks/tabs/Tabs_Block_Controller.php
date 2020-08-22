872a2d20afee89e6246af39e9094d985062e08aa
Ryan Urban <ryan@fringewebdevelopment.com>
2020-08-22 12:06:03 -0500

    Tabs block CCP

--- wp-content/themes/core/components/blocks/tabs/Tabs_Block_Controller.php
+++ wp-content/themes/core/components/blocks/tabs/Tabs_Block_Controller.php
@@ -8,11 +8,14 @@ use \Tribe\Project\Blocks\Types\Tabs\Tabs as Tabs_Block;
 use Tribe\Project\Templates\Components\Abstract_Controller;
 use Tribe\Project\Templates\Components\Deferred_Component;
 use Tribe\Project\Templates\Components\tabs\Tabs_Controller;
+use Tribe\Project\Templates\Components\container\Container_Controller;
+use Tribe\Project\Templates\Components\content_block\Content_Block_Controller;
+use Tribe\Project\Templates\Components\text\Text_Controller;
 
 class Tabs_Block_Controller extends Abstract_Controller {
 	private const LAYOUT            = 'layout';
 	private const TITLE             = 'title';
-	private const DESCRIPTION       = 'description';
+	private const CONTENT           = 'content';
 	private const TABS              = 'tabs';
 	private const CLASSES           = 'classes';
 	private const ATTRS             = 'attrs';
@@ -20,11 +23,11 @@ class Tabs_Block_Controller extends Abstract_Controller {
 
 	private string $layout;
 	private string $title;
-	private string $description;
-	private array $tabs;
-	private array $classes;
-	private array $attrs;
-	private array $container_classes;
+	private string $content;
+	private array  $tabs;
+	private array  $classes;
+	private array  $attrs;
+	private array  $container_classes;
 
 	/**
 	 * @param array $args
@@ -34,7 +37,7 @@ class Tabs_Block_Controller extends Abstract_Controller {
 
 		$this->layout            = (string) $args[ self::LAYOUT ];
 		$this->title             = (string) $args[ self::TITLE ];
-		$this->description       = (string) $args[ self::DESCRIPTION ];
+		$this->content           = (string) $args[ self::CONTENT ];
 		$this->tabs              = (array) $args[ self::TABS ];
 		$this->classes           = (array) $args[ self::CLASSES ];
 		$this->attrs             = (array) $args[ self::ATTRS ];
@@ -48,7 +51,7 @@ class Tabs_Block_Controller extends Abstract_Controller {
 		return [
 			self::LAYOUT            => Tabs_Block::LAYOUT_HORIZONTAL,
 			self::TITLE             => '',
-			self::DESCRIPTION       => '',
+			self::CONTENT           => '',
 			self::TABS              => [],
 			self::CLASSES           => [],
 			self::ATTRS             => [],
@@ -66,57 +69,10 @@ class Tabs_Block_Controller extends Abstract_Controller {
 		];
 	}
 
-	public function get_header_args(): array {
-		if ( empty( $this->title ) && empty( $this->description ) ) {
-			return [];
-		}
-
-		return [
-			'tag'     => 'header',
-			'classes' => [ 'b-tabs__header' ],
-			'title'   => $this->get_title(),
-			'content' => $this->get_description(),
-		];
-	}
-
-	public function get_tabs_args(): array {
-		return [
-			Tabs_Controller::TABS    => $this->tabs,
-			Tabs_Controller::LAYOUT  => $this->layout,
-			Tabs_Controller::CLASSES => [ 'b-tabs__content' ],
-		];
-	}
-
-	private function get_title(): Deferred_Component {
-		$args = [
-			'tag'     => 'h2',
-			'classes' => [ 'b-tabs__title', 'h3' ],
-			'content' => $this->title,
-		];
-
-		return defer_template_part( 'components/text/text', null, $args );
-	}
-
-	private function get_description(): Deferred_Component {
-		$args = [
-			'classes' => [ 'b-tabs__description', 't-sink', 's-sink' ],
-			'content' => $this->description,
-		];
-
-		return defer_template_part( 'components/container/container', null, $args );
-	}
-
 	/**
 	 * @return string
 	 */
-	public function container_classes(): string {
-		return Markup_Utils::class_attribute( $this->container_classes );
-	}
-
-	/**
-	 * @return string
-	 */
-	public function classes(): string {
+	public function get_classes(): string {
 		$this->classes[] = sprintf(  'c-block--layout-%s', $this->layout );
 		return Markup_Utils::class_attribute( $this->classes );
 	}
@@ -124,8 +80,62 @@ class Tabs_Block_Controller extends Abstract_Controller {
 	/**
 	 * @return string
 	 */
-	public function attrs(): string {
+	public function get_attrs(): string {
 		return Markup_Utils::class_attribute( $this->attrs );
 	}
 
+	/**
+	 * @return string
+	 */
+	public function get_container_classes(): string {
+		return Markup_Utils::class_attribute( $this->container_classes );
+	}
+
+	/**
+	 * @return array
+	 */
+	public function get_header_args(): array {
+		if ( empty( $this->title ) && empty( $this->description ) ) {
+			return [];
+		}
+
+		return [
+			Content_Block_Controller::TAG     => 'header',
+			Content_Block_Controller::CLASSES => [ 'b-tabs__header' ],
+			Content_Block_Controller::TITLE   => $this->get_title(),
+			Content_Block_Controller::CONTENT => $this->get_content(),
+		];
+	}
+
+	/**
+	 * @return Deferred_Component
+	 */
+	private function get_title(): Deferred_Component {
+		return defer_template_part( 'components/text/text', null, [
+			Text_Controller::TAG     => 'h2',
+			Text_Controller::CLASSES => [ 'b-tabs__title', 'h3' ],
+			Text_Controller::CONTENT => $this->title ?? '',
+		] );
+	}
+
+	/**
+	 * @return Deferred_Component
+	 */
+	private function get_content(): Deferred_Component {
+		return defer_template_part( 'components/container/container', null, [
+			Container_Controller::CLASSES => [ 'b-tabs__description', 't-sink', 's-sink' ],
+			Container_Controller::CONTENT => $this->content ?? '',
+		] );
+	}
+
+	/**
+	 * @return array
+	 */
+	public function get_tabs_args(): array {
+		return [
+			Tabs_Controller::TABS    => $this->tabs,
+			Tabs_Controller::LAYOUT  => $this->layout,
+			Tabs_Controller::CLASSES => [ 'b-tabs__content' ],
+		];
+	}
 }
--- wp-content/themes/core/components/blocks/tabs/tabs.php
+++ wp-content/themes/core/components/blocks/tabs/tabs.php
@@ -10,9 +10,19 @@ use \Tribe\Project\Templates\Components\blocks\tabs\Tabs_Block_Controller;
 $c = Tabs_Block_Controller::factory( $args );
 ?>
 
-<section <?php echo $c->classes(); ?> <?php echo $c->attrs(); ?>>
-	<div <?php echo $c->container_classes(); ?>>
-		<?php get_template_part( 'components/content_block/content_block', null, $c->get_header_args() ); ?>
-		<?php get_template_part( 'components/tabs/tabs', null, $c->get_tabs_args() ); ?>
+<section <?php echo $c->get_classes(); ?> <?php echo $c->get_attrs(); ?>>
+	<div <?php echo $c->get_container_classes(); ?>>
+		<?php
+		get_template_part(
+			'components/content_block/content_block',
+			null,
+			$c->get_header_args()
+		);
+		get_template_part(
+			'components/tabs/tabs',
+			null,
+			$c->get_tabs_args()
+		);
+		?>
 	</div>
 </section>
