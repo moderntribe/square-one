0da33cd47ecb6494357e8bdc035b75f250fd31fd
Jamie Paul <jamie@tri.be>
2020-08-24 11:09:21 -0500

    remove old lead form controller and update method in template

--- wp-content/themes/core/components/blocks/lead_form/lead_form.php
+++ wp-content/themes/core/components/blocks/lead_form/lead_form.php
@@ -13,7 +13,7 @@ $c = Lead_Form_Block_Controller::factory( $args );
 <section <?php echo $c->get_classes(); ?> <?php echo $c->get_attrs(); ?>>
 	<div <?php echo $c->get_container_classes(); ?>>
 
-		<?php if ( ! empty( $c->get_content_args() ) ) { ?>
+		<?php if ( ! empty( $c->get_header_args() ) ) { ?>
 			<?php get_template_part(
 				'components/content_block/content_block',
 				null,
