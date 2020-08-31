858ccf1aad86db9e35681a82691189cb8860f8e8
David Ellenwood <davee@tri.be>
2020-08-31 16:50:29 -0500

    Setup base Buttons block.

---
+++ wp-content/themes/core/components/blocks/buttons/Buttons_Block_Controller.php
@@ -0,0 +1,100 @@
+<?php
+declare( strict_types=1 );
+
+namespace Tribe\Project\Templates\Components\blocks\buttons;
+
+use Tribe\Libs\Utils\Markup_Utils;
+use Tribe\Project\Blocks\Types\Buttons\Buttons;
+use Tribe\Project\Templates\Components\Abstract_Controller;
+use Tribe\Project\Templates\Components\link\Link_Controller;
+
+class Buttons_Block_Controller extends Abstract_Controller {
+	public const CLASSES = 'classes';
+	public const ATTRS   = 'attrs';
+	public const BUTTONS = 'links';
+
+	private array $classes;
+	private array $attrs;
+	private array $buttons;
+
+	/**
+	 * @param array $args
+	 */
+	public function __construct( array $args = [] ) {
+		$args = $this->parse_args( $args );
+
+		$this->classes = (array) $args[ self::CLASSES ];
+		$this->attrs   = (array) $args[ self::ATTRS ];
+		$this->buttons = (array) $args[ self::BUTTONS ];
+	}
+
+	/**
+	 * @return array
+	 */
+	protected function defaults(): array {
+		return [
+			self::CLASSES => [],
+			self::ATTRS   => [],
+			self::BUTTONS => [],
+		];
+	}
+
+	/**
+	 * @return array
+	 */
+	protected function required(): array {
+		return [
+			self::CLASSES  => [ 'c-block', 'b-buttons' ],
+		];
+	}
+
+	/**
+	 * @return string
+	 */
+	public function get_classes(): string {
+		return Markup_Utils::class_attribute( $this->classes );
+	}
+
+	/**
+	 * @return string
+	 */
+	public function get_attrs(): string {
+		return Markup_Utils::class_attribute( $this->attrs );
+	}
+
+	/**
+	 * @return array
+	 */
+	public function get_buttons(): array {
+		if ( empty( $this->buttons ) ) {
+			return [];
+		}
+
+		return array_map( function ( $button ) {
+			$link = wp_parse_args( $button[ Buttons::BUTTON_LINK ], [
+				'title'  => '',
+				'url'    => '',
+				'target' => '',
+			] );
+
+			return [
+				Link_Controller::URL        => $link['url'],
+				Link_Controller::CONTENT    => $link['title'] ?? $link['url'],
+				Link_Controller::TARGET     => $link['target'],
+				Link_Controller::ARIA_LABEL => [ Buttons::BUTTON_ARIA_LABEL ] ?? '',
+				Link_Controller::CLASSES    => $this->get_button_classes( $button ),
+			];
+		}, $this->buttons );
+	}
+
+	private function get_button_classes( $button ): array {
+		$classes = [ 'a-btn' ];
+
+		if ( $button[ Buttons::BUTTON_STYLE ] !== Buttons::STYLE_PRIMARY ) {
+			$classes = [ sprintf( 'a-btn-%s', (string) $button[ Buttons::BUTTON_STYLE ] ) ];
+		}
+
+		return $classes;
+	}
+
+}
