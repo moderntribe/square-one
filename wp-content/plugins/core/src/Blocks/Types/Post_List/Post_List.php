4e38d45eaa42a974c87b5d5d028d1e622c266523
David Ellenwood <davee@tri.be>
2020-09-04 13:34:45 -0500

    Post List.

--- wp-content/plugins/core/src/Blocks/Types/Post_List/Post_List.php
+++ wp-content/plugins/core/src/Blocks/Types/Post_List/Post_List.php
@@ -47,6 +47,20 @@ class Post_List extends Block_Config {
 			'keywords'    => [ __( 'posts', 'tribe' ), __( 'display', 'tribe' ), __( 'text', 'tribe' ) ],
 			'category'    => 'layout',
 			'supports'    => [ 'align' => false ],
+			'example'     => [
+				'attributes' => [
+					'mode' => 'preview',
+					'data' => [
+						self::TITLE       => __( 'A Selection of Posts', 'tribe' ),
+						self::LEAD_IN     => __( 'Suspendisse potenti', 'tribe' ),
+						self::DESCRIPTION => __( 'Pellentesque diam diam, aliquet non mauris eu, posuere mollis urna. Nulla eget congue ligula, a aliquam lectus. Duis non diam maximus justo dictum porttitor in in risus.',
+							'tribe' ),
+						self::CTA         => [ 'title' => __( 'Call to Action', 'tribe' ), 'url' => '#' ],
+						self::QUERY_TYPE  => self::QUERY_TYPE_AUTO,
+						self::LIMIT       => 3,
+					],
+				],
+			],
 		] ) );
 	}
 
