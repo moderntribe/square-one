0414e835fdecb023de500126103e52ac318ca0ad
Jonathan Brinley <jbrinley@flightless.us>
2020-09-09 16:58:55 -0400

    do not try to translate login logo dimensions

--- wp-content/plugins/core/src/Theme/Branding/Login_Screen.php
+++ wp-content/plugins/core/src/Theme/Branding/Login_Screen.php
@@ -30,9 +30,9 @@ class Login_Screen {
 		<style type="text/css">
 			.login h1 a {
 				background: transparent url(<?php echo esc_url( $logo_data[0] ); ?>) 50% 50% no-repeat !important;
-				width: <?php esc_attr_e( $logo_width ); ?> !important;
-				height: <?php esc_attr_e( $logo_height ); ?> !important;
-				background-size: <?php esc_attr_e( $logo_width . ' ' . $logo_height ); ?> !important;
+				width: <?php printf( '%dpx', $logo_width ); ?> !important;
+				height: <?php printf( '%dpx', $logo_height ); ?> !important;
+				background-size: <?php printf( '%dpx %dpx', $logo_width, $logo_height ); ?> !important;
 			}
 		</style>
 		<?php
@@ -40,23 +40,25 @@ class Login_Screen {
 
 	/**
 	 * Login Header Url
+	 *
 	 * @param string $url
 	 *
 	 * @return string
 	 * @filter login_headerurl
 	 */
-	public function customize_login_header_url( $url ) {
+	public function customize_login_header_url( $url ): string {
 		return get_home_url();
 	}
 
 	/**
 	 * Login Header Title
+	 *
 	 * @param string $name
 	 *
 	 * @return string
 	 * @filter login_headertext
 	 */
-	public function customize_login_header_title( $name ) {
+	public function customize_login_header_title( $name ): string {
 		return get_bloginfo( 'name' );
 	}
 }
