7a3cbf9875fbb35d95c1374b8d7b617051c3d782
Mike Sawicki <mikes@tri.be>
2020-09-25 11:02:43 -0400

    Adds return types back

--- wp-content/plugins/core/src/Templates/Models/Post_List_Object.php
+++ wp-content/plugins/core/src/Templates/Models/Post_List_Object.php
@@ -22,53 +22,58 @@ class Post_List_Object {
 	/**
 	 * @return int
 	 */
-	public function get_post_id() {
+	public function get_post_id(): int {
 		return $this->post_id;
 	}
 
 	/**
 	 * @return string
 	 */
-	public function get_title() {
+	public function get_title(): string {
 		return $this->title;
 	}
 
 	/**
 	 * @return string
 	 */
-	public function get_content() {
+	public function get_content(): string {
 		return $this->content;
 	}
 
 	/**
 	 * @return string
 	 */
-	public function get_excerpt() {
+	public function get_excerpt(): string {
 		return $this->excerpt;
 	}
 
 	/**
 	 * @return int
 	 */
-	public function get_image_id() {
+	public function get_image_id(): int {
 		return $this->image_id;
 	}
 
 	/**
 	 * @return array
 	 */
-	public function get_link() {
+	public function get_link(): array {
 		return $this->link;
 	}
 
 	/**
 	 * @return string
 	 */
-	public function get_post_type() {
+	public function get_post_type(): string {
 		return $this->post_type;
 	}
 
 
+	/**
+	 * @param int $post_id
+	 *
+	 * @return Post_List_Object
+	 */
 	public function set_post_id( int $post_id ) {
 		$this->post_id = $post_id;
 
@@ -76,39 +81,66 @@ class Post_List_Object {
 	}
 
 
+	/**
+	 * @param string $title
+	 *
+	 * @return Post_List_Object
+	 */
 	public function set_title( string $title ) {
 		$this->title = $title;
 
 		return $this;
 	}
 
-
+	/**
+	 * @param string $content
+	 *
+	 * @return Post_List_Object
+	 */
 	public function set_content( string $content ) {
 		$this->content = $content;
 
 		return $this;
 	}
 
-
+	/**
+	 * @param string $excerpt
+	 *
+	 * @return Post_List_Object
+	 */
 	public function set_excerpt( string $excerpt ) {
 		$this->excerpt = $excerpt;
 
 		return $this;
 	}
 
-
+	/**
+	 * @param int $id
+	 *
+	 * @return Post_List_Object
+	 */
 	public function set_image_id( int $id ) {
 		$this->image_id = $id;
 
 		return $this;
 	}
 
+	/**
+	 * @param array $link
+	 *
+	 * @return Post_List_Object
+	 */
 	public function set_link( array $link ) {
 		$this->link = $link;
 
 		return $this;
 	}
 
+	/**
+	 * @param string $post_type
+	 *
+	 * @return Post_List_Object
+	 */
 	public function set_post_type( string $post_type ) {
 		$this->post_type = $post_type;
 
