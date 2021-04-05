d4c44aea103bc16ea5e13aa91cca0afb43a2288f
Matt McAchran <mattmcachran@gmail.com>
2021-04-05 12:25:54 -0400

    PHPCS cleanup.

--- wp-content/plugins/core/src/Routes/Routes_Definer.php
+++ wp-content/plugins/core/src/Routes/Routes_Definer.php
@@ -21,12 +21,12 @@ class Routes_Definer implements Definer_Interface {
 	 */
 	public function define(): array {
 		return [
-			\Tribe\Libs\Routes\Route_Definer::ROUTES      => DI\add(
+			Route_Definer::ROUTES      => DI\add(
 				[
 					DI\get( Sample_Route::class ),
 				]
 			),
-			\Tribe\Libs\Routes\Route_Definer::REST_ROUTES => DI\add(
+			Route_Definer::REST_ROUTES => DI\add(
 				[
 					DI\get( Sample_REST_Route::class ),
 				]
