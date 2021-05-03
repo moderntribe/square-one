584c4c8c8137884686a47e6c3dc7bbd62d6e5491
Ryan Urban <ryan@fringewebdevelopment.com>
2021-05-03 15:03:54 -0500

    Update based on CR

--- local-config-sample.php
+++ local-config-sample.php
@@ -17,6 +17,8 @@
  * - development
  * - local
  *
+ * Note, if you are using Local, you will need to define these here.
+ *
  * @link https://make.wordpress.org/core/2020/07/24/new-wp_get_environment_type-function-in-wordpress-5-5/
  */
 define( 'WP_ENVIRONMENT_TYPE', 'development' );
@@ -30,6 +32,13 @@ define( 'ENVIRONMENT', 'DEV' );
 define( 'HMR_DEV', false );
 
 /*
+ * Panels
+ *
+ * If your project uses panels this will disable/enable panels caching.
+ */
+//define( 'TRIBE_DISABLE_PANELS_CACHE', true );
+
+/*
  * Enable ASSET_VERSION_TIMESTAMP if you are doing front end dev on css/js to force cache invalidation without running a full build
  */
 define( 'ASSET_VERSION_TIMESTAMP', true );
@@ -40,3 +49,11 @@ define( 'ASSET_VERSION_TIMESTAMP', true );
  * If you enable Whoops, the Whoops error library will be used to provide better/prettier error logging.
  */
 define( 'WHOOPS_ENABLE', true );
+
+/*
+ * Glomar
+ *
+ * GLOMAR is a plugin that blocks the frontend of the site from public access.
+ * If you would like to disable the plugin locally, add the following to your local-config.php.
+ */
+define( 'TRIBE_GLOMAR', false );
