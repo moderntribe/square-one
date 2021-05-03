43c9b98dfbe65f7a80830ec6527be266effd41d6
ScottG <garrisonscott@gmail.com>
2021-05-03 12:32:49 -0500

    making sure custom classes and attributes works

--- wp-content/themes/core/components/blocks/spacer/Spacer_Block_Controller.php
+++ wp-content/themes/core/components/blocks/spacer/Spacer_Block_Controller.php
@@ -60,7 +60,7 @@ class Spacer_Block_Controller extends Abstract_Controller {
 	 * @return string
 	 */
 	public function get_attrs(): string {
-		return Markup_Utils::class_attribute( $this->attrs );
+		return Markup_Utils::concat_attrs( $this->attrs );
 	}
 
 	/**
