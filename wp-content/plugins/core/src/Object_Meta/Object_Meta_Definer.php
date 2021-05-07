c490dc6c89df2fff3bd1a9123fd2f35bb1554781
Tanner Record <tanner.record@gmail.com>
2021-05-07 13:57:12 -0400

    Add hero image field for cat archive template

--- wp-content/plugins/core/src/Object_Meta/Object_Meta_Definer.php
+++ wp-content/plugins/core/src/Object_Meta/Object_Meta_Definer.php
@@ -7,6 +7,7 @@ use DI;
 use Psr\Container\ContainerInterface;
 use Tribe\Libs\Container\Definer_Interface;
 use Tribe\Project\Settings;
+use Tribe\Project\Taxonomies\Category\Category;
 
 class Object_Meta_Definer implements Definer_Interface {
 	public function define(): array {
@@ -16,6 +17,7 @@ class Object_Meta_Definer implements Definer_Interface {
 				DI\get( Analytics_Settings::class ),
 				DI\get( Social_Settings::class ),
 				DI\get( Post_Archive_Settings::class ),
+				DI\get( Taxonomy_Archive_Settings::class ),
 			] ),
 
 			// add analytics settings to the general settings screen
@@ -37,6 +39,12 @@ class Object_Meta_Definer implements Definer_Interface {
 					'settings_pages' => [ $container->get( Settings\General::class )->get_slug() ],
 				] );
 			},
+
+			Taxonomy_Archive_Settings::class => static function (): Taxonomy_Archive_Settings {
+				return new Taxonomy_Archive_Settings( [
+					'taxonomies' => [ Category::NAME ],
+				] );
+			}
 		];
 	}
 }
