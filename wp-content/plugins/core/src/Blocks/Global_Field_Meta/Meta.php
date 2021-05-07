01a3eaf9fc00055b624f2cdee0a83065022b907a
Justin Frydman <justin.frydman@gmail.com>
2021-05-07 17:19:10 -0600

    Add meta interface

---
+++ wp-content/plugins/core/src/Blocks/Global_Field_Meta/Meta.php
@@ -0,0 +1,18 @@
+<?php declare(strict_types=1);
+
+namespace Tribe\Project\Blocks\Global_Field_Meta;
+
+use Tribe\Libs\ACF\Field;
+
+/**
+ * Meta interface that returns an array of Field objects.
+ *
+ * @package Tribe\Project\Blocks\Global_Field_Meta
+ */
+interface Meta {
+
+	/**
+	 * @return Field[]
+	 */
+	public function get_fields(): array;
+}
