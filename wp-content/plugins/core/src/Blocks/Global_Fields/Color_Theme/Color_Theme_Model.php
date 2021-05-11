3aa5101c27f52dc790ef111107a3d0915b5d134c
Justin Frydman <justin.frydman@gmail.com>
2021-05-10 22:45:20 -0600

    WIP: Color_Theme_Model.php

---
+++ wp-content/plugins/core/src/Blocks/Global_Fields/Color_Theme/Color_Theme_Model.php
@@ -0,0 +1,12 @@
+<?php declare(strict_types=1);
+
+namespace Tribe\Project\Blocks\Global_Fields\Color_Theme;
+
+use Tribe\Project\Blocks\Global_Fields\Block_Model;
+
+class Color_Theme_Model extends Block_Model {
+
+	protected function set_data(): array {
+		// TODO: Implement set_data() method.
+	}
+}
