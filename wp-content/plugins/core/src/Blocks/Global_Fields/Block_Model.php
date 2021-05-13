193e58afeaae16ce109aee535bc00b7cddf3b9a1
Justin Frydman <justin.frydman@gmail.com>
2021-05-13 14:16:42 -0600

    Allow assignment of an ACF block ID so models will query the specific block

--- wp-content/plugins/core/src/Blocks/Global_Fields/Block_Model.php
+++ wp-content/plugins/core/src/Blocks/Global_Fields/Block_Model.php
@@ -5,13 +5,52 @@ namespace Tribe\Project\Blocks\Global_Fields;
 use Tribe\Libs\ACF\Traits\With_Get_Field;
 use Tribe\Project\Blocks\Types\Model;
 
+/**
+ * Extend to override data being passed to existing models.
+ *
+ * @package Tribe\Project\Blocks\Global_Fields
+ */
 abstract class Block_Model implements Model {
 
 	use With_Get_Field;
 
+	/**
+	 * The ACF Block ID.
+	 *
+	 * @var string
+	 */
+	protected string $block_id;
+
+	/**
+	 * A multidimensional array of model data.
+	 *
+	 * @return mixed[]
+	 */
 	abstract protected function set_data(): array;
 
+	/**
+	 * Assign an ACF block ID.
+	 *
+	 * @param string $block_id
+	 *
+	 * @return $this
+	 */
+	public function set_block_id( string $block_id ): self {
+		$this->block_id = $block_id;
+
+		return $this;
+	}
+
+	/**
+	 * Retrieve model data.
+	 *
+	 * @return array|mixed[]
+	 */
 	public function get_data(): array {
+		if ( empty( $this->block_id ) ) {
+			return [];
+		}
+
 		return $this->set_data();
 	}
 }
