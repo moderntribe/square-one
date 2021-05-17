bed055ae34f1f42a37a97fced844a3844ee40316
Tanner Record <tanner.record@gmail.com>
2021-05-17 09:13:35 -0400

    Update color theme global field specifics

--- wp-content/plugins/core/src/Blocks/Global_Fields/Color_Theme/Color_Theme_Meta.php
+++ wp-content/plugins/core/src/Blocks/Global_Fields/Color_Theme/Color_Theme_Meta.php
@@ -17,17 +17,21 @@ class Color_Theme_Meta extends Block_Meta implements Appearance {
 	public const SECTION = 's-colors';
 
 	protected function add_fields(): void {
-		$this->add_field( new Field_Section( self::SECTION, __( 'Color Settings', 'tribe' ), 'accordion' ) )
+		$this->add_field( new Field_Section( self::SECTION, __( 'Appearance', 'tribe' ), 'accordion' ) )
 			 ->add_field( $this->get_page_theme_override_field() )
 			 ->add_field( $this->get_color_theme_field() );
 	}
 
 	protected function get_page_theme_override_field(): Field {
 		return new Field( self::NAME . '_' . self::PAGE_THEME_OVERRIDE, [
-			'type'  => 'true_false',
-			'name'  => self::PAGE_THEME_OVERRIDE,
-			'label' => __( 'Override Page Theme', 'tribe' ),
-			'ui'    => true,
+			'type'   => 'radio',
+			'name'   => self::PAGE_THEME_OVERRIDE,
+			'label'  => __( 'Color Theme', 'tribe' ),
+			'layout' => 'horizontal',
+			'choices' => [
+				'page'   => 'Page',
+				'custom' => 'Custom',
+			],
 		] );
 	}
 
@@ -35,7 +39,7 @@ class Color_Theme_Meta extends Block_Meta implements Appearance {
 		return new Field( self::NAME . '_' . self::COLOR_THEME, [
 			'type'              => 'swatch',
 			'name'              => self::COLOR_THEME,
-			'label'             => __( 'Color Theme', 'tribe' ),
+			'label'             => __( 'Custom Color Theme', 'tribe' ),
 			'choices'           => [
 				self::OPTION_LIGHT => __( 'Light', 'tribe' ),
 				self::OPTION_DARK  => __( 'Dark', 'tribe' ),
@@ -47,7 +51,7 @@ class Color_Theme_Meta extends Block_Meta implements Appearance {
 					[
 						'field'    => $this->get_key_with_prefix( self::PAGE_THEME_OVERRIDE ),
 						'operator' => '==',
-						'value'    => '1',
+						'value'    => 'custom',
 					],
 				],
 			],
