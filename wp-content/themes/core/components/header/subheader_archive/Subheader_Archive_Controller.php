abeb947bfbc028ccb0686bd03ac6a30507b0ceda
Lucia developer <luciasanchezfraile@egmail.com>
2021-05-20 12:27:04 +0200

    subheader for archive pages

---
+++ wp-content/themes/core/components/header/subheader_archive/Subheader_Archive_Controller.php
@@ -0,0 +1,172 @@
+<?php
+declare( strict_types=1 );
+
+namespace Tribe\Project\Templates\Components\header\subheader_archive;
+
+use Tribe\Libs\Utils\Markup_Utils;
+use Tribe\Project\Templates\Components\Abstract_Controller;
+use Tribe\Project\Templates\Components\text\Text_Controller;
+use Tribe\Project\Templates\Components\Traits\Page_Title;
+use Tribe\Project\Templates\Components\image\Image_Controller;
+use Tribe\Project\Theme\Config\Image_Sizes;
+
+class Subheader_Archive_Controller extends Abstract_Controller {
+	use Page_Title;
+
+	public const CLASSES = 'classes';
+	public const ATTRS   = 'attrs';
+	public const CONTAINER_CLASSES = 'container_classes';
+	public const MEDIA_CLASSES     = 'media_classes';
+	public const CONTENT_CLASSES   = 'content_classes';
+
+	private array  $classes;
+	private array  $attrs;
+	private array  $container_classes;
+	private array  $media_classes;
+	private array  $content_classes;
+	private string $title;
+	private string $description;
+	private int    $hero_image_id;
+
+
+	public function __construct( array $args = [] ) {
+		$args = $this->parse_args( $args );
+
+		$this->classes = (array) $args[ self::CLASSES ];
+		$this->attrs   = (array) $args[ self::ATTRS ];
+		$this->container_classes = (array) $args[ self::CONTAINER_CLASSES ];
+		$this->media_classes     = (array) $args[ self::MEDIA_CLASSES ];
+		$this->content_classes   = (array) $args[ self::CONTENT_CLASSES ];
+
+		$this->map_acf_fields();
+
+	}
+
+	protected function defaults(): array {
+		return [
+			self::CLASSES => [ 'c-subheader' ],
+			self::CONTAINER_CLASSES => [ 'l-container' ], 
+			self::ATTRS   => [],
+			self::MEDIA_CLASSES     => [],
+			self::CONTENT_CLASSES   => [],
+		];
+	}
+
+	protected function required(): array {
+		return [];
+	}
+
+	public function get_classes(): string {
+		return Markup_Utils::class_attribute( $this->classes );
+	}
+
+	/**
+	 * @return string
+	 */
+	public function get_content_classes(): string {
+		return Markup_Utils::class_attribute( $this->content_classes );
+	}
+
+	/**
+	 * @return string
+	 */
+	public function get_media_classes(): string {
+		return Markup_Utils::class_attribute( $this->media_classes );
+	}
+
+
+	/**
+	 * @return string
+	 */
+	public function get_container_classes(): string {
+		return Markup_Utils::class_attribute( $this->container_classes );
+	}
+
+	public function get_attrs(): string {
+		return Markup_Utils::concat_attrs( $this->attrs );
+	}
+
+	
+	/**
+	 * @return array
+	 */
+	public function get_image_args(): array {
+		if (  empty( $this->hero_image_id ) ) {
+			return [];
+		}
+	
+		return [
+			Image_Controller::IMG_ID       => (int) $this->hero_image_id,
+			Image_Controller::AUTO_SHIM    => false,
+			Image_Controller::USE_LAZYLOAD => true,
+			Image_Controller::CLASSES      => [ 'c-image--overlay', 'c-image--object-fit' ],
+			Image_Controller::IMG_CLASSES  => [ 'c-subheader__media__image' ],
+			Image_Controller::SRC_SIZE     => Image_Sizes::SIXTEEN_NINE,
+			Image_Controller::SRCSET_SIZES => [
+				Image_Sizes::SIXTEEN_NINE,
+				Image_Sizes::SIXTEEN_NINE_SMALL,
+			],
+		];
+	}
+
+	/**
+	 * @return array
+	 */
+	public function get_description_args(): array {
+	
+		return [
+			Text_Controller::TAG     => 'p',
+			Text_Controller::CLASSES => [ '' ],
+			Text_Controller::CONTENT => $this->description,
+		];
+	}
+
+	/**
+	 * @return array
+	 */
+	public function get_title_args(): array {
+		return [
+			Text_Controller::TAG     => 'h1',
+			Text_Controller::CLASSES => [ '' ],
+			Text_Controller::CONTENT => $this->title,
+		];
+	}
+
+
+	/**
+	 * Get ACF fields 
+	 * 
+	 * Gets the content added in the meta fields 
+	 */
+	protected function map_acf_fields() {
+	
+
+		if ( is_category() ) {
+			
+			$term = get_queried_object();
+			
+			if ( ! empty( $term ) ) {
+				$this->title = $term->name;
+				$this->description = $term->category_description;
+			}
+	
+			if ( ! empty( get_field( 'hero_image', $term ) ) ) {
+				$this->hero_image_id = get_field( 'hero_image', $term )['ID'];
+			}
+			
+		} else {
+
+			if ( ! empty( get_field( 'title', 'option' ) ) ) {
+				$this->title = get_field( 'title', 'option' );
+			}
+	
+			if ( ! empty( get_field( 'description', 'option' )) ) {
+				$this->description = get_field( 'description', 'option' );
+			}
+	
+			if ( ! empty( get_field( 'hero_image', 'option' ) ) ) {
+				$this->hero_image_id = get_field( 'hero_image', 'option' )['ID'];
+			}
+		}	
+	}
+}
