4a0c57c139c440b903521a9854ebd20827391c45
Justin Frydman <justin.frydman@gmail.com>
2021-05-23 14:30:50 -0600

    [SQONE-429] roll back

--- wp-content/object-cache.php
+++ wp-content/object-cache.php
@@ -1,6 +1,4 @@
-<?php
-
-// test
+<?php declare(strict_types=1);
 
 if ( ! filter_var( getenv( 'DISABLE_OBJECT_CACHE', true ), FILTER_VALIDATE_BOOLEAN ) ) {
 	require_once  __DIR__ . '/memcached-object-cache.php';
