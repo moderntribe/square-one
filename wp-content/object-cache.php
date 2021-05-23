fc08cdeebdbd770101b322571db81ae946dd4f9d
Justin Frydman <justin.frydman@gmail.com>
2021-05-23 14:30:37 -0600

    [SQONE-429] test lefthook/phpcs should ignore this

--- wp-content/object-cache.php
+++ wp-content/object-cache.php
@@ -1,4 +1,6 @@
-<?php declare(strict_types=1);
+<?php
+
+// test
 
 if ( ! filter_var( getenv( 'DISABLE_OBJECT_CACHE', true ), FILTER_VALIDATE_BOOLEAN ) ) {
 	require_once  __DIR__ . '/memcached-object-cache.php';
