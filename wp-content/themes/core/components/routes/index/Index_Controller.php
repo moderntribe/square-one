071c0c676dfa9a47a8c9a401b0525effafde6297
Lucia developer <luciasanchezfraile@egmail.com>
2021-05-26 09:51:19 +0200

    functions to retrieve posts and featured posts added

--- wp-content/themes/core/components/routes/index/Index_Controller.php
+++ wp-content/themes/core/components/routes/index/Index_Controller.php
@@ -3,12 +3,35 @@ declare( strict_types=1 );
 
 namespace Tribe\Project\Templates\Components\routes\index;
 
+use Tribe\Project\Taxonomies\Featured\Featured;
 use Tribe\Project\Templates\Components\Abstract_Controller;
 use Tribe\Project\Templates\Components\sidebar\Sidebar_Controller;
 use Tribe\Project\Templates\Components\breadcrumbs\Breadcrumbs_Controller;
 use Tribe\Project\Templates\Models\Breadcrumb;
+use Tribe\Project\Blocks\Types\Content_Loop\Content_Loop;
+use Tribe\Project\Templates\Components\blocks\content_loop\Content_Loop_Controller;
+use Tribe\Project\Templates\Components\Traits\Post_List_Field_Formatter;
+use WP_Query;
 
 class Index_Controller extends Abstract_Controller {
+	
+	use Post_List_Field_Formatter;
+	
+	public const IS_TERM = 'is_term';
+
+	private bool $is_term;
+
+	public function __construct( array $args = [] ) {
+		$args = $this->parse_args( $args );
+
+		$this->is_term = (bool) $args[ self::IS_TERM ];
+	}
+
+	protected function defaults(): array {
+		return [
+			self::IS_TERM => false,
+		];
+	}
 
 	/**
 	 * @var int|string
@@ -87,6 +110,66 @@ class Index_Controller extends Abstract_Controller {
 
 		return [
 			new Breadcrumb( $url, __( 'News', 'tribe' ) ),
+		];
+	}
+
+	public function get_current_page(): int {
+		return (int) get_query_var('paged') ?: 1;
+	}
+
+	/**
+	* Get posts in the featured taxonomy.
+	*
+	* @return array
+	*/
+	public function get_featured_posts_args(): array {
+		if ( $this->is_term ) {
+			return [];
+		}
+
+		$posts = [];
+
+		$query = new WP_Query( [
+			'tax_query' => [
+				[
+					'taxonomy' => Featured::NAME,
+					'operator' => 'EXISTS',
+				]
+			]
+		] );
+
+		if ( ! empty( $query->posts ) ) {
+			foreach ( $query->posts as $post ) {
+				$posts[] = $this->formatted_post( $post, 45 );
+			}
+		}
+
+		return [
+			Content_Loop_Controller::LAYOUT => Content_Loop::LAYOUT_FEATURE,
+			Content_Loop_Controller::POSTS  => array_slice( $posts, 0, 7 ),
+		];
+	}
+
+	/**
+	*
+	*
+	* @return array
+	*/
+	public function get_loop_args(): array {
+		$posts = [];
+
+		while ( have_posts() ) {
+			the_post();
+			$posts[] = $this->formatted_post( get_post(), 22 );
+		}
+
+		
+		return [
+			Content_Loop_Controller::CLASSES => [ 'item-index__loop' ],
+			//Content_Loop_Controller::LAYOUT  => Content_Loop::LAYOUT_ROW,
+		//	Content_Loop_Controller::LAYOUT  => Content_Loop::LAYOUT_COLUMNS,
+			Content_Loop_Controller::POSTS   => $posts,
+		//	Content_Loop_Controller::VARIANT => Content_Loop_Controller::VARIANT_SMALL,
 		];
 	}
 }
