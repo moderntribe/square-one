6ae17da9de0fc3f64e37e370b651d3efc2eadbc3
Justin Frydman <justin.frydman@gmail.com>
2021-06-01 11:23:39 -0600

    Type wp_nav_menu() arguments with the proper stdClass

--- wp-content/plugins/core/src/Nav_Menus/Nav_Attribute_Filters.php
+++ wp-content/plugins/core/src/Nav_Menus/Nav_Attribute_Filters.php
@@ -2,6 +2,8 @@
 
 namespace Tribe\Project\Nav_Menus;
 
+use stdClass;
+
 /**
  * Class Nav_Attribute_Filters
  *
@@ -12,57 +14,61 @@ class Nav_Attribute_Filters {
 	/**
 	 * Remove the ID attributed from the nav item
 	 *
-	 * @param string $menu_id The ID that is applied to the menu item's `<li>` element.
-	 * @param object $item    The current menu item.
-	 * @param array  $args    An array of wp_nav_menu() arguments.
-	 * @param int    $depth   Depth of menu item. Used for padding.
+	 * @param string    $menu_id The ID that is applied to the menu item's `<li>` element.
+	 * @param object    $item    The current menu item.
+	 * @param \stdClass $args    An object of {@see wp_nav_menu()} arguments.
+	 * @param int       $depth   Depth of menu item. Used for padding.
 	 *
 	 * @return string
 	 *
 	 * @filter nav_menu_item_id
 	 */
-	public function customize_nav_item_id( $menu_id, $item, $args, $depth ): string {
+	public function customize_nav_item_id( string $menu_id, object $item, stdClass $args, int $depth ): string {
 		return '';
 	}
 
 	/**
 	 * Customize the CSS classes applied to an <li> in the nav menu.
 	 *
-	 * @param array  $classes The CSS classes that are applied to the menu item's `<li>` element.
-	 * @param object $item    The current menu item.
-	 * @param array  $args    An array of {@see wp_nav_menu()} arguments
-	 * @param int    $depth   Depth of menu item. Used for padding.
+	 * @param array     $classes The CSS classes that are applied to the menu item's `<li>` element.
+	 * @param object    $item    The current menu item.
+	 * @param \stdClass $args    An object of {@see wp_nav_menu()} arguments.
+	 * @param int       $depth   Depth of menu item. Used for padding.
+	 *
+	 * @note   WP Core docs claim that $args is an array, but it comes in as an object thanks to casting in wp_nav_menu().
 	 *
 	 * @return array
 	 *
 	 * @filter nav_menu_css_class
 	 */
-	public function customize_nav_item_classes( $classes, $item, $args, $depth ): array {
-		$classes[] = $args['theme_location'] . '__list-item';
+	public function customize_nav_item_classes( array $classes, object $item, stdClass $args, int $depth ): array {
+		$theme_location = $args->theme_location;
+
+		$classes[] = $theme_location . '__list-item';
 
 		// Depth
-		$classes[] = $args['theme_location'] . '__list-item--depth-' . $depth;
+		$classes[] = $theme_location . '__list-item--depth-' . $depth;
 
 		// Has children items
 		if ( in_array( 'menu-item-has-children', $item->classes ) ) {
-			$classes[] = $args['theme_location'] . '__list-item--has-children';
+			$classes[] = $theme_location . '__list-item--has-children';
 		}
 
 		// Is Parent Item
 		if ( in_array( 'current-menu-parent', $item->classes ) ) {
-			$classes[] = $args['theme_location'] . '__list-item--is-current-parent';
+			$classes[] = $theme_location . '__list-item--is-current-parent';
 		}
 
 		// Is Current Item
 		if ( in_array( 'current-menu-item', $item->classes ) ) {
-			$classes[] = $args['theme_location'] . '__list-item--is-current';
+			$classes[] = $theme_location . '__list-item--is-current';
 		}
 
 		/**
 		 * Filter the array of classes to remove the classes added by WP Core.
 		 * Regex is designed to filter all classes defined in `_wp_menu_item_classes_by_context()`;
 		 */
-		$classes = array_filter( $classes, static function ( $class ) {
+		return array_filter( $classes, static function ( $class ) {
 
 			/**
 			 * Patterns used for matching:
@@ -76,38 +82,39 @@ class Nav_Attribute_Filters {
 
 			return ! preg_match( $pattern, $class );
 		} );
-
-		return $classes;
 	}
 
 	/**
 	 * Customize WP menu item anchor attributes
 	 *
-	 * @param array $atts {
-	 *     The HTML attributes applied to the menu item's `<a>` element, empty strings are ignored.
+	 * @param array     $atts   {
+	 *                          The HTML attributes applied to the menu item's `<a>` element, empty strings are ignored.
 	 *
-	 *     @type string $title  Title attribute.
-	 *     @type string $target Target attribute.
-	 *     @type string $rel    The rel attribute.
-	 *     @type string $href   The href attribute.
+	 * @type string     $title  Title attribute.
+	 * @type string     $target Target attribute.
+	 * @type string     $rel    The rel attribute.
+	 * @type string     $href   The href attribute.
 	 * }
-	 * @param object $item  The current menu item.
-	 * @param array  $args  An array of {@see wp_nav_menu()} arguments.
-	 * @param int    $depth Depth of menu item. Used for padding.
+	 *
+	 * @param object    $item   The current menu item.
+	 * @param \stdClass $args   An object of {@see wp_nav_menu()} arguments.
+	 * @param int       $depth  Depth of menu item. Used for padding.
 	 *
 	 * @return array
 	 *
 	 * @filter nav_menu_link_attributes
 	 */
-	public function customize_nav_item_anchor_atts( $atts, $item, $args, $depth ): array {
+	public function customize_nav_item_anchor_atts( array $atts, object $item, stdClass $args, int $depth ): array {
+		$theme_location = $args->theme_location;
+
 		$classes = [
-			$args['theme_location'] . '__action',
-			$args['theme_location'] . '__action--depth-' . $depth,
+			$theme_location . '__action',
+			$theme_location . '__action--depth-' . $depth,
 		];
 
 		// Has children items
 		if ( in_array( 'menu-item-has-children', $item->classes ) ) {
-			$classes[] = $args['theme_location'] . '__action--has-children';
+			$classes[] = $theme_location . '__action--has-children';
 		}
 
 		$atts['class'] = implode( ' ', array_unique( $classes ) );
