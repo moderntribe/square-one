b56b3b3850b540066555129781aaf255e1d6cbcb
tjdunklee <tjdunklee@gmail.com>
2021-06-01 14:30:59 -0500

    SQONE-281 Making new typgraphy style

--- wp-content/plugins/core/src/Query/Query_Subscriber.php
+++ wp-content/plugins/core/src/Query/Query_Subscriber.php
@@ -6,7 +6,7 @@ use Tribe\Libs\Container\Abstract_Subscriber;
 
 class Query_Subscriber extends Abstract_Subscriber {
 	public function register(): void {
-		add_action( 'posts_request', function ( $request, $wp_query ) {
+		add_filter( 'posts_request', function ( $request, $wp_query ) {
 			return $this->container->get( Search::class )->dont_do_search( $request, $wp_query );
 		}, 10, 2 );
 	}
