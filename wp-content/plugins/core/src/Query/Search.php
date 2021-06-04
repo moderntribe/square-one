3a3e8d700626e3603dcfe1707064286d5104db87
Justin Frydman <justin.frydman@gmail.com>
2021-06-04 10:45:12 -0600

    [SQONE-281] Code cleanup on search

--- wp-content/plugins/core/src/Query/Search.php
+++ wp-content/plugins/core/src/Query/Search.php
@@ -2,6 +2,8 @@
 
 namespace Tribe\Project\Query;
 
+use WP_Query;
+
 class Search {
 
 	/**
@@ -13,14 +15,16 @@ class Search {
 	 *
 	 * @hook https://developer.wordpress.org/reference/hooks/posts_request/
 	 */
-	public function force_load_search_template( string $request, \WP_Query $query ): string {
-		//This basically prevents the query for search but still provides the benefit of not
-		//sending back a 404 and still provides the page title, etc.
+	public function force_load_search_template( string $request, WP_Query $query ): string {
+		/*
+			This basically prevents the query for search but still provides the benefit of not
+			sending back a 404 and still provides the page title, etc.
+		*/
 		if ( $query->is_search() && ! is_admin() && empty( $query->get( 's' ) ) ) {
-			/* prevent SELECT FOUND_ROWS() query */
+			// prevent SELECT FOUND_ROWS() query
 			$query->query_vars['no_found_rows'] = true;
 
-			/* prevent post term and meta cache update queries */
+			// prevent post term and meta cache update queries
 			$query->query_vars['cache_results'] = false;
 
 			return '';
