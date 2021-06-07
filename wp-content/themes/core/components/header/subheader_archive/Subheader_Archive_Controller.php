a21679ae94f8ba58e3b05a4cac8257003042ec57
Lucia developer <luciasanchezfraile@egmail.com>
2021-06-08 00:21:56 +0200

    code standards check

--- wp-content/themes/core/components/header/subheader_archive/Subheader_Archive_Controller.php
+++ wp-content/themes/core/components/header/subheader_archive/Subheader_Archive_Controller.php
@@ -1,37 +1,35 @@
-<?php
-declare( strict_types=1 );
+<?php declare(strict_types=1);
 
 namespace Tribe\Project\Templates\Components\header\subheader_archive;
 
 use Tribe\Libs\Utils\Markup_Utils;
 use Tribe\Project\Templates\Components\Abstract_Controller;
-use Tribe\Project\Templates\Components\text\Text_Controller;
 use Tribe\Project\Templates\Components\image\Image_Controller;
+use Tribe\Project\Templates\Components\text\Text_Controller;
 use Tribe\Project\Theme\Config\Image_Sizes;
 
 class Subheader_Archive_Controller extends Abstract_Controller {
 
-	public const CLASSES = 'classes';
-	public const ATTRS   = 'attrs';
+	public const CLASSES           = 'classes';
+	public const ATTRS             = 'attrs';
 	public const CONTAINER_CLASSES = 'container_classes';
 	public const MEDIA_CLASSES     = 'media_classes';
 	public const CONTENT_CLASSES   = 'content_classes';
 
-	private array  $classes;
-	private array  $attrs;
-	private array  $container_classes;
-	private array  $media_classes;
-	private array  $content_classes;
+	private array $classes;
+	private array $attrs;
+	private array $container_classes;
+	private array $media_classes;
+	private array $content_classes;
 	private string $title;
 	private string $description;
-	private int    $hero_image_id;
-
+	private int $hero_image_id;
 
 	public function __construct( array $args = [] ) {
 		$args = $this->parse_args( $args );
 
-		$this->classes = (array) $args[ self::CLASSES ];
-		$this->attrs   = (array) $args[ self::ATTRS ];
+		$this->classes           = (array) $args[ self::CLASSES ];
+		$this->attrs             = (array) $args[ self::ATTRS ];
 		$this->container_classes = (array) $args[ self::CONTAINER_CLASSES ];
 		$this->media_classes     = (array) $args[ self::MEDIA_CLASSES ];
 		$this->content_classes   = (array) $args[ self::CONTENT_CLASSES ];
@@ -119,7 +117,6 @@ class Subheader_Archive_Controller extends Abstract_Controller {
 	 * @return array
 	 */
 	public function get_description_args(): array {
-	
 		return [
 			Text_Controller::TAG     => 'p',
 			Text_Controller::CLASSES => [ 'c-subheader__description' ],
@@ -146,25 +143,22 @@ class Subheader_Archive_Controller extends Abstract_Controller {
 	 */
 	protected function map_acf_fields() {
 		if ( is_category() ) {
-			
 			$term = get_queried_object();
 			
 			if ( ! empty( $term ) ) {
-				$this->title = $term->name;
+				$this->title       = $term->name;
 				$this->description = $term->category_description;
 			}
 	
 			if ( ! empty( get_field( 'hero_image', $term ) ) ) {
 				$this->hero_image_id = get_field( 'hero_image', $term )['ID'];
 			}
-			
 		} else {
-
 			if ( ! empty( get_field( 'title', 'option' ) ) ) {
 				$this->title = get_field( 'title', 'option' );
 			}
 	
-			if ( ! empty( get_field( 'description', 'option' )) ) {
+			if ( ! empty( get_field( 'description', 'option' ) ) ) {
 				$this->description = get_field( 'description', 'option' );
 			}
 	
@@ -173,4 +167,5 @@ class Subheader_Archive_Controller extends Abstract_Controller {
 			}
 		}	
 	}
+
 }
