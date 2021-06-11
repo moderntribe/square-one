9f0746cb0b1d8ded598914d31708dd7b95cdffbb
Lucia developer <luciasanchezfraile@egmail.com>
2021-06-11 10:42:15 +0200

    index route displaying new posts layout

--- wp-content/themes/core/components/routes/index/Index_Controller.php
+++ wp-content/themes/core/components/routes/index/Index_Controller.php
@@ -4,152 +4,17 @@ namespace Tribe\Project\Templates\Components\routes\index;
 
 use Tribe\Project\Blocks\Types\Content_Loop\Content_Loop;
 use Tribe\Project\Taxonomies\Featured\Featured;
-use Tribe\Project\Templates\Components\Abstract_Controller;
 use Tribe\Project\Templates\Components\blocks\content_loop\Content_Loop_Controller;
-use Tribe\Project\Templates\Components\breadcrumbs\Breadcrumbs_Controller;
-use Tribe\Project\Templates\Components\sidebar\Sidebar_Controller;
-use Tribe\Project\Templates\Components\text\Text_Controller;
-use Tribe\Project\Templates\Components\Traits\Post_List_Field_Formatter;
-use Tribe\Project\Templates\Models\Breadcrumb;
+use Tribe\Project\Templates\Components\routes\archive\Archive_Controller;
 use WP_Query;
 
-class Index_Controller extends Abstract_Controller {
-	
-	use Post_List_Field_Formatter;
-	
-	public const IS_TERM = 'is_term';
-
-	private bool $is_term;
-	private array $featured_posts_id = [ '0' ];
-	private $sidebar_id              = '';
-
-	public function __construct( array $args = [] ) {
-		$args          = $this->parse_args( $args );
-		$this->is_term = (bool) $this->is_term();
-	}
-
-	protected function defaults(): array {
-		return [
-			self::IS_TERM => false,
-		];
-	}
-
-
-	/**
-	 * Render the header component
-	 *
-	 * Bypasses the get_header() function to
-	 * load our component instead of header.php
-	 *
-	 * @return void
-	 */
-	public function render_header(): void {
-		do_action( 'get_header', null );
-		get_template_part( 'components/document/header/header', 'index' );
-	}
-
-
-	/**
-	 * Render the subheader component
-	 *
-	 * @return void
-	 */
-	public function render_subheader(): void {
-		get_template_part( 'components/header/subheader_archive/subheader_archive', 'index' );
-	}
-
-
-	/**
-	 * Render the sidebar component
-	 *
-	 * Bypasses the get_sidebar() function to
-	 * load our component instead of sidebar.php
-	 *
-	 * @return void
-	 */
-	public function render_sidebar(): void {
-		do_action( 'get_sidebar', null );
-		get_template_part(
-			'components/sidebar/sidebar',
-			'index',
-			[ Sidebar_Controller::SIDEBAR_ID => $this->sidebar_id ]
-		);
-	}
-
-	/**
-	 * Render the footer component
-	 *
-	 * Bypasses the get_footer() function to
-	 * load our component instead of footer.php
-	 *
-	 * @return void
-	 */
-	public function render_footer(): void {
-		do_action( 'get_footer', null );
-		get_template_part( 'components/document/footer/footer', 'index' );
-	}
-
-	/**
-	 * Render the breadcrums component
-	 *
-	 * @return void
-	 */
-	public function render_breadcrumbs(): void {
-		get_template_part(
-			'components/breadcrumbs/breadcrumbs',
-			'index',
-			[ Breadcrumbs_Controller::BREADCRUMBS => $this->get_breadcrumbs() ]
-		);
-	}
-
-	/**
-	 * @return \Tribe\Project\Templates\Models\Breadcrumb[]
-	 */
-	protected function get_breadcrumbs(): array {
-		$page = get_option( 'page_for_posts' );
-		$url  = $page ? get_permalink( $page ) : home_url();
-
-		return [
-			new Breadcrumb( $url, __( 'News', 'tribe' ) ),
-		];
-	}
-
-
-	public function is_term(): int {
-		if ( is_home() ) {
-			return 0;
-		}
-
-		return (int) get_queried_object()->term_id ?: 0;
-	}
-
-
-	public function get_current_page(): int {
-		return (int) get_query_var( 'paged' ) ?: 1;
-	}
-
-
-	/**
-	 * @return void
-	 */
-	public function get_number_of_posts() {
-		return 
-		[
-			Text_Controller::TAG     => 'p',
-			Text_Controller::CLASSES => [ '' ],
-			Text_Controller::CONTENT => wp_count_posts()->publish.' '.__( 'posts in', 'tribe' ).' "'.get_the_archive_title().'" ',
-			
-		];
-	}
+class Index_Controller extends Archive_Controller {
 
 	/**
 	* Get posts in the featured taxonomy.
 	 */
 	public function get_featured_posts_args(): array {
-		if ( $this->is_term ) {
-			return [];
-		}
-
+		
 		$featured_posts = [];
 		
 		$query = new WP_Query( [
@@ -177,13 +42,12 @@ class Index_Controller extends Abstract_Controller {
 		];
 	}
 
-	
 	public function get_loop_args(): array {
 		$posts = [];
 
 		/* to exclude featured if we are displaying them */
 		$query = new WP_Query( [
-			'post__not_in'   => array_slice( $this->featured_posts_id, 0, 7 ),
+			'post__not_in'   => $this->featured_posts_id,
 			'posts_per_page' => '9',
 		] );
 
@@ -200,6 +64,10 @@ class Index_Controller extends Abstract_Controller {
 			Content_Loop_Controller::POSTS   => $posts,
 		
 		];
+	}
+
+	public function get_current_page(): int {
+		return (int) get_query_var( 'paged' ) ?: 1;
 	}
 
 }
--- wp-content/themes/core/components/routes/index/index.php
+++ wp-content/themes/core/components/routes/index/index.php
@@ -2,40 +2,36 @@
 
 use \Tribe\Project\Templates\Components\routes\index\Index_Controller;
 
-$c = Index_Controller::factory( $args );
+$c = Index_Controller::factory();
 
 $c->render_header();
 ?>
-	<main id="main-content">
+<main id="main-content">
 		<?php $c->render_subheader(); ?>
 		
-		<?php if ( is_front_page() ) : ?>
-			<?php the_content(); ?>
+		<?php if ( have_posts() ) : ?>
+			<div class="l-container"> 
+				<?php get_template_part( 'components/text/text', null, $c->get_number_of_posts() );?>
+			</div>
+			
+			<?php
 
+			if ( $c->get_current_page() === 1 && ! empty( $c->get_featured_posts_args() ) ) :
+				get_template_part( 'components/blocks/content_loop/content_loop', null, $c->get_featured_posts_args() );
+			endif;
+
+			get_template_part( 'components/blocks/content_loop/content_loop', null, $c->get_loop_args() );
+			
+			?>
+			
+			<div class="l-container"> 
+				<?php // to do
+				// get_template_part( 'components/pagination/loop/loop', 'search' ); ?>
+				
+			</div>
+				
 		<?php else :
-			if ( have_posts() ) : ?>
-				<div class="l-container"> 
-					<?php get_template_part( 'components/text/text', null, $c->get_number_of_posts() );?>
-				</div>
-				
-				<?php
-
-				if ( $c->get_current_page() === 1 && ! empty( $c->get_featured_posts_args() ) ) :
-					get_template_part( 'components/blocks/content_loop/content_loop', null, $c->get_featured_posts_args() );
-				endif;
-
-				get_template_part( 'components/blocks/content_loop/content_loop', null, $c->get_loop_args() );
-				
-				?>
-				
-				<div class="l-container"> 
-					<?php //get_template_part( 'components/pagination/loop/loop', 'search' ); ?>
-					
-				</div>
-				
-			<?php else :
-				get_template_part( 'components/no_results/no_results', 'index' );
-			endif; 
+			get_template_part( 'components/no_results/no_results', 'index' );
 		endif; ?>
 
 	</main>
