48761b8635c6194fff38e2166f5ef105b5cc199d
Nick Pelton <nickpelton@gmail.com>
2021-06-29 17:11:50 -0500

    Apply suggestions from code review
    PHPCS additions.
    
    Co-authored-by: Justin <defunctl@users.noreply.github.com>

--- wp-content/plugins/core/src/Settings/Post_Settings.php
+++ wp-content/plugins/core/src/Settings/Post_Settings.php
@@ -6,15 +6,15 @@ use Tribe\Libs\ACF\ACF_Settings;
 
 class Post_Settings extends ACF_Settings {
 
-	public function get_title() {
+	public function get_title(): string {
 		return __( 'Settings', 'tribe' );
 	}
 
-	public function get_capability() {
+	public function get_capability(): string {
 		return 'activate_plugins';
 	}
 
-	public function get_parent_slug() {
+	public function get_parent_slug(): string {
 		return 'edit.php';
 	}
 
