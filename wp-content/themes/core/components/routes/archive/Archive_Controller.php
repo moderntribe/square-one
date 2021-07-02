afb8d5aea274a9a8e5de3bb572bae4456aa71719
Nick Pelton <nickpelton@gmail.com>
2021-07-02 13:39:59 -0500

    [SQONE-279] phpcs: removed unused classes

--- wp-content/themes/core/components/routes/archive/Archive_Controller.php
+++ wp-content/themes/core/components/routes/archive/Archive_Controller.php
@@ -2,12 +2,8 @@
 
 namespace Tribe\Project\Templates\Components\routes\archive;
 
-use Tribe\Project\Object_Meta\Post_Archive_Settings;
-use Tribe\Project\Templates\Components\header\subheader_archive\Subheader_Archive_Controller;
 use Tribe\Project\Templates\Components\routes\index\Index_Controller;
-use Tribe\Project\Templates\Components\Traits\Page_Title;
 
 class Archive_Controller extends Index_Controller {
-
 
 }
