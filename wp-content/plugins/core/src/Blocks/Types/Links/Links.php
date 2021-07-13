28a48edfcee59541fc806538055a2fcde6e0253f
Matt McAchran <mattmcachran@gmail.com>
2021-07-13 12:51:03 -0400

    Updates Links block.

--- wp-content/plugins/core/src/Blocks/Types/Links/Links.php
+++ wp-content/plugins/core/src/Blocks/Types/Links/Links.php
@@ -22,20 +22,23 @@ class Links extends Block_Config implements Cta_Field {
 	public const TITLE           = 'title';
 	public const DESCRIPTION     = 'description';
 
-	public const LINKS_TITLE = 'links_title';
-	public const LINKS       = 'links';
+	public const SECTION_LINKS = 's-links';
+	public const LINKS_TITLE   = 'links_title';
+	public const LINKS         = 'links';
+	public const LINK_HEADER   = 'link_header';
+	public const LINK_CONTENT  = 'link_content';
 
-	public const SECTION_SETTINGS = 's-settings';
-	public const LAYOUT           = 'layout';
-	public const LAYOUT_INLINE    = 'inline';
-	public const LAYOUT_STACKED   = 'stacked';
+	public const SECTION_APPEARANCE = 's-appearance';
+	public const LAYOUT             = 'layout';
+	public const LAYOUT_INLINE      = 'inline';
+	public const LAYOUT_STACKED     = 'stacked';
 
 	public function add_block() {
 		$this->set_block( new Block( self::NAME, [
-			'title'       => __( 'Links', 'tribe' ),
-			'description' => __( 'A list of links', 'tribe' ),
+			'title'       => esc_html__( 'Links', 'tribe' ),
+			'description' => esc_html__( 'A list of links', 'tribe' ),
 			'icon'        => 'list-view',
-			'keywords'    => [ __( 'list', 'tribe' ) ],
+			'keywords'    => [ esc_html__( 'list', 'tribe' ) ],
 			'category'    => 'layout',
 			'supports'    => [
 				'align'  => false,
@@ -95,22 +98,22 @@ class Links extends Block_Config implements Cta_Field {
 	}
 
 	public function add_fields() {
-		//==========================================
-		// Content Fields
-		//==========================================
-		$this->add_section( new Field_Section( self::SECTION_CONTENT, __( 'Content', 'tribe' ), 'accordion' ) )
-			 ->add_field( new Field( self::NAME . '_' . self::LEAD_IN, [
-					 'label' => __( 'Lead in', 'tribe' ),
-					 'name'  => self::LEAD_IN,
-					 'type'  => 'text',
+		$this->add_field( new Field( self::NAME . '_' . self::LEAD_IN, [
+					 'label'       => esc_html__( 'Lead in', 'tribe' ),
+					 'name'        => self::LEAD_IN,
+					 'type'        => 'text',
+					 'placeholder' => esc_html__( 'Leadin (optional)', 'tribe' ),
+					 'wrapper'     => [
+						'class' => 'tribe-acf-hide-label',
+					],
 				 ] )
 			 )->add_field( new Field( self::NAME . '_' . self::TITLE, [
-					'label' => __( 'Title', 'tribe' ),
+					'label' => esc_html__( 'Title', 'tribe' ),
 					'name'  => self::TITLE,
 					'type'  => 'text',
 				] )
 			)->add_field( new Field( self::NAME . '_' . self::DESCRIPTION, [
-					'label'        => __( 'Description', 'tribe' ),
+					'label'        => esc_html__( 'Description', 'tribe' ),
 					'name'         => self::DESCRIPTION,
 					'type'         => 'wysiwyg',
 					'toolbar'      => Classic_Editor_Formats::MINIMAL,
@@ -119,26 +122,33 @@ class Links extends Block_Config implements Cta_Field {
 				] )
 			)->add_field(
 				$this->get_cta_field( self::NAME )
-			)->add_field(
+			);
+
+		$this->add_section( new Field_Section( self::SECTION_LINKS, esc_html__( 'Links', 'tribe' ), 'accordion' ) )
+			 ->add_field(
 				new Field( self::NAME . '_' . self::LINKS_TITLE, [
-					'label' => __( 'Link List Title', 'tribe' ),
-					'name'  => self::LINKS_TITLE,
-					'type'  => 'text',
+					'label'       => esc_html__( 'Link List Title', 'tribe' ),
+					'name'        => self::LINKS_TITLE,
+					'type'        => 'text',
+					'placeholder' => esc_html( 'Link List Title (optional)', 'tribe' ),
+					'wrapper'     => [
+						'class' => 'tribe-acf-hide-label',
+					],
 				] )
 			)->add_field(
 				$this->get_links_section()
 			);
 
 		//==========================================
-		// Setting Fields
+		// Appearance Fields
 		//==========================================
-		$this->add_section( new Field_Section( self::SECTION_SETTINGS, __( 'Settings', 'tribe' ), 'accordion' ) )
+		$this->add_section( new Field_Section( self::SECTION_APPEARANCE, esc_html__( 'Appearance', 'tribe' ), 'accordion' ) )
 			 ->add_field( new Field( self::NAME . '_' . self::LAYOUT, [
 				 'type'            => 'image_select',
 				 'name'            => self::LAYOUT,
 				 'choices'         => [
-					 self::LAYOUT_INLINE  => __( 'Inline', 'tribe' ),
-					 self::LAYOUT_STACKED => __( 'Stacked', 'tribe' ),
+					 self::LAYOUT_INLINE  => esc_html__( 'Inline', 'tribe' ),
+					 self::LAYOUT_STACKED => esc_html__( 'Stacked', 'tribe' ),
 				 ],
 				 'default_value'   => self::LAYOUT_STACKED,
 				 'multiple'        => 0,
@@ -157,7 +167,7 @@ class Links extends Block_Config implements Cta_Field {
 	protected function get_links_section(): Repeater {
 		$group = new Repeater( self::NAME . '_' . self::LINKS );
 		$group->set_attributes( [
-			'label'  => __( 'Links List', 'tribe' ),
+			'label'  => esc_html__( 'Links List', 'tribe' ),
 			'name'   => self::LINKS,
 			'layout' => 'block',
 			'min'    => 0,
@@ -166,6 +176,20 @@ class Links extends Block_Config implements Cta_Field {
 		$link = $this->get_cta_field( self::NAME );
 
 		$group->add_field( $link );
+
+		$group->add_field( new Field( self::NAME . '_' . self::LINK_HEADER, [
+			'label'        => esc_html__( 'Link Header', 'tribe' ),
+			'name'         => self::LINK_HEADER,
+			'type'         => 'text',
+		] )
+		)->add_field( new Field( self::NAME . '_' . self::LINK_CONTENT, [
+			'label'        => esc_html__( 'Link Content', 'tribe' ),
+			'name'         => self::LINK_CONTENT,
+			'type'         => 'wysiwyg',
+			'toolbar'      => Classic_Editor_Formats::MINIMAL,
+			'tabs'         => 'visual',
+			'media_upload' => 0,
+		] ) );
 
 		return $group;
 	}
