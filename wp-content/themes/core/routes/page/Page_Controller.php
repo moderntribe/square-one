f02166144fbcf254a7f18b8ee742c3fb2389161e
Nick Pelton <nickpelton@gmail.com>
2021-08-31 22:20:56 -0500

    [2] fix page container class

--- wp-content/themes/core/routes/page/Page_Controller.php
+++ wp-content/themes/core/routes/page/Page_Controller.php
@@ -2,6 +2,7 @@
 
 namespace Tribe\Project\Templates\Routes\page;
 
+use Tribe\Project\Templates\Components\header\subheader\Subheader_Single_Controller;
 use Tribe\Project\Templates\Routes\single\Single_Controller;
 
 class Page_Controller extends Single_Controller {
@@ -10,5 +11,12 @@ class Page_Controller extends Single_Controller {
 	 * @var int|string
 	 */
 	public $sidebar_id = '';
+
+	public function get_subheader_args(): array {
+
+		$args[ Subheader_Single_Controller::TITLE ] = $this->get_page_title();
+
+		return $args;
+	}
 
 }
