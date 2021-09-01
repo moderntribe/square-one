531386d331f94f233da93998ebf40cf67808d6ac
Nick Pelton <nickpelton@gmail.com>
2021-09-01 08:58:35 -0500

    [2] Fix some inconsistencies noticed in the subheader across CPTs

--- wp-content/themes/core/components/header/subheader/Subheader_Controller.php
+++ wp-content/themes/core/components/header/subheader/Subheader_Controller.php
@@ -64,13 +64,13 @@ class Subheader_Controller extends Abstract_Controller {
 			self::CONTAINER_CLASSES => [ 'l-container' ],
 			self::MEDIA_CLASSES     => [ 'c-subheader__media' ],
 			self::CONTENT_CLASSES   => [ 'c-subheader__content' ],
-			self::CLASSES           => [ 'c-subheader'],
+			self::CLASSES           => [ 'c-subheader', 'c-subheader--has-background'],
 		];
 	}
 
 	public function get_classes(): string {
 		if ( ! empty( $this->hero_image_id ) ) {
-			$this->classes[] = 'c-subheader--has-background';
+			$this->classes[] = 'c-subheader--has-image';
 		}
 
 		return Markup_Utils::class_attribute( $this->classes );
