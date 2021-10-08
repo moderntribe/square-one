add42cf95a56a8cf2d617da853d46d35720cb5d3
David Ellenwood <davee@tri.be>
2021-10-08 12:22:27 -0500

    SQONE-523 Update navigation component list element.

--- wp-content/themes/core/components/navigation/navigation.php
+++ wp-content/themes/core/components/navigation/navigation.php
@@ -14,7 +14,7 @@ if ( ! $c->has_menu() ) {
 
 ?>
 <nav <?php echo $c->get_classes(); ?> <?php echo $c->get_attrs(); ?>>
-	<ol <?php echo $c->get_nav_list_classes(); ?>>
+	<ul <?php echo $c->get_nav_list_classes(); ?>>
 		<?php echo $c->get_menu(); ?>
-	</ol>
+	</ul>
 </nav>
