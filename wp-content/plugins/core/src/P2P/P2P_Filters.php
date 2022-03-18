a42e04cb3d989998fccba2923b22b931b3d84c7f
Chris Clontz <clontz.chris@gmail.com>
2022-03-18 15:00:46 -0500

    [SQONE-692] P2P Integration
    - Corrects missing post titles in the P2P metabox
    - Corrects the P2P ID argument type when caching P2P connections

---
+++ wp-content/plugins/core/src/P2P/P2P_Filters.php
@@ -0,0 +1,29 @@
+<?php declare(strict_types=1);
+
+/**
+ * Holds various filters for P2P.
+ *
+ * @package DATAORG
+ */
+
+namespace Tribe\Project\P2P;
+
+/**
+ * Class to hold p2p filters.
+ */
+class P2P_Filters {
+
+	/**
+	 * Fix title for P2P connection and candidate.
+	 *
+	 * @param string|null                   $title Current title for the item.
+	 * @param \WP_Post                      $post  Post object for the item.
+	 * @param \P2P_Directed_Connection_Type $ctype Type object.
+	 *
+	 * @return string                              Modified title for the item.
+	 */
+	public function fix_title( ?string $title, \WP_Post $post, \P2P_Directed_Connection_Type $ctype ): string {
+		return ! empty( $title ) ? $title : $post->post_title;
+	}
+
+}
