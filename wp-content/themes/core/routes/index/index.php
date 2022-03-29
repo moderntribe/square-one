4d801e9d02971390f916901daba3e2635a62d67b
Justin Frydman <justin.frydman@gmail.com>
2022-03-29 15:36:39 -0600

    Refactor the index controller

--- wp-content/themes/core/routes/index/index.php
+++ wp-content/themes/core/routes/index/index.php
@@ -11,7 +11,7 @@ get_header();
 		<?php get_template_part( 'components/header/subheader/subheader', 'index', $c->get_subheader_args() ); ?>
 
 		<?php if ( have_posts() ) :
-			if ( $c->get_current_page() === 1 && ! empty( $c->get_content_loop_featured_args() ) ) :
+			if ( $c->is_page_one() && ! empty( $c->get_content_loop_featured_args() ) ) :
 				get_template_part( 'components/blocks/content_loop/content_loop', null, $c->get_content_loop_featured_args() );
 			endif;
 			get_template_part( 'components/blocks/content_loop/content_loop', null, $c->get_content_loop_args() ); ?>
@@ -24,7 +24,7 @@ do_action( 'get_sidebar', null );
 get_template_part(
 	'components/sidebar/sidebar',
 	'index',
-	[ Sidebar_Controller::SIDEBAR_ID => $c->sidebar_id ]
+	[ Sidebar_Controller::SIDEBAR_ID => $c->get_sidebar_id() ]
 );
 
 get_footer();
