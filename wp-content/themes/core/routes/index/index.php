e27b1556be1f6fa87390e5bb4d5ff4f9c3c74b7f
Justin Frydman <justin.frydman@gmail.com>
2022-03-29 15:45:15 -0600

    no html, use bracket style conditionals

--- wp-content/themes/core/routes/index/index.php
+++ wp-content/themes/core/routes/index/index.php
@@ -10,14 +10,16 @@ get_header();
 	<main id="main-content">
 		<?php get_template_part( 'components/header/subheader/subheader', 'index', $c->get_subheader_args() ); ?>
 
-		<?php if ( have_posts() ) :
-			if ( $c->is_page_one() && ! empty( $c->get_content_loop_featured_args() ) ) :
+		<?php if ( have_posts() ) {
+			// Featured posts
+			if ( $c->is_page_one() && ! empty( $c->get_content_loop_featured_args() ) ) {
 				get_template_part( 'components/blocks/content_loop/content_loop', null, $c->get_content_loop_featured_args() );
-			endif;
-			get_template_part( 'components/blocks/content_loop/content_loop', null, $c->get_content_loop_args() ); ?>
-		<?php else :
+			}
+
+			get_template_part( 'components/blocks/content_loop/content_loop', null, $c->get_content_loop_args() );
+		} else {
 			get_template_part( 'components/no_results/no_results', 'index' );
-		endif; ?>
+		} ?>
 	</main>
 <?php
 do_action( 'get_sidebar', null );
