9e566deac203ae5f4d2d7d7f91671ad44b333ccc
Justin Frydman <justin.frydman@gmail.com>
2022-04-01 08:11:29 -0600

    standardize button model

--- wp-content/themes/core/blocks/buttons/buttons.php
+++ wp-content/themes/core/blocks/buttons/buttons.php
@@ -6,4 +6,9 @@ use Tribe\Project\Blocks\Types\Buttons\Buttons_Model;
  * @var array $args ACF block data.
  */
 $model = tribe_project()->container()->make( Buttons_Model::class, $args );
-get_template_part( 'components/blocks/buttons/buttons', '', $model->get_data() );
+
+get_template_part(
+	'components/blocks/buttons/buttons',
+	'',
+	$model->get_data()
+);
