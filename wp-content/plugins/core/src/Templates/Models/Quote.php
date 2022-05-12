fe09e00da09a19a7e7d380e59533e20366c57634
Justin Frydman <justin.frydman@gmail.com>
2022-05-12 15:12:26 -0600

    Refactor quote into an ACF group

--- wp-content/plugins/core/src/Templates/Models/Quote.php
+++ wp-content/plugins/core/src/Templates/Models/Quote.php
@@ -3,10 +3,13 @@
 namespace Tribe\Project\Templates\Models;
 
 use Tribe\Libs\Field_Models\Field_Model;
+use Tribe\Libs\Field_Models\Models\Image;
 
 class Quote extends Field_Model {
 
 	public string $quote_text = '';
-	public Citation $citation;
+	public string $cite_name  = '';
+	public string $cite_title = '';
+	public Image $cite_image;
 
 }
--- wp-content/themes/core/components/quote/quote.php
+++ wp-content/themes/core/components/quote/quote.php
@@ -10,37 +10,36 @@ $c = Quote_Controller::factory( $args );
 ?>
 
 <blockquote <?php echo $c->get_classes(); ?> <?php echo $c->get_attrs(); ?>>
-	<?php if ( ! empty( $c->get_quote()->quote_text ) ) {
-		get_template_part( 'components/text/text', '', [
-			Text_Controller::TAG     => 'h2',
-			Text_Controller::CLASSES => [
-				'c-quote__text',
-				'h4',
-			],
-			Text_Controller::CONTENT => esc_html( $c->get_quote()->quote_text ),
-		] );
-	} ?>
+	<?php if ( $c->has_quote() ) : ?>
+		<?php if ( ! empty( $c->get_quote()->quote_text ) ) {
+			get_template_part( 'components/text/text', '', [
+				Text_Controller::TAG     => 'h2',
+				Text_Controller::CLASSES => [
+					'c-quote__text',
+					'h4',
+				],
+				Text_Controller::CONTENT => esc_html( $c->get_quote()->quote_text ),
+			] );
+		} ?>
 
-	<?php if ( $c->has_citation() ) : ?>
 		<cite class="c-quote__cite">
 			<?php if ( ! empty( ( $c->get_image_args() ) ) ) {
 				get_template_part( 'components/image/image', null, $c->get_image_args() );
 			} ?>
 
 			<span class="c-quote__cite-text">
-				<?php if ( ! empty( $c->get_quote()->citation->cite_name ) ) : ?>
+				<?php if ( ! empty( $c->get_quote()->cite_name ) ) : ?>
 					<span class="c-quote__cite-name">
-						<?php echo esc_html( $c->get_quote()->citation->cite_name ); ?>
+						<?php echo esc_html( $c->get_quote()->cite_name ); ?>
 					</span>
 				<?php endif; ?>
-				<?php if ( ! empty( $c->get_quote()->citation->cite_title ) ) : ?>
+				<?php if ( ! empty( $c->get_quote()->cite_title ) ) : ?>
 					<span class="c-quote__cite-title">
-						<?php echo esc_html( $c->get_quote()->citation->cite_title ); ?>
+						<?php echo esc_html( $c->get_quote()->cite_title ); ?>
 					</span>
 				<?php endif; ?>
 			</span>
 
 		</cite>
 	<?php endif; ?>
-
 </blockquote>
