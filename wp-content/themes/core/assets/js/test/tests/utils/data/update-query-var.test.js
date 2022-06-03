cb83d4f635d78bd07d651cc4414d0ef3ab2416fa
GeoffDusome <geoffdusome@gmail.com>
2022-06-02 20:22:27 -0400

    fix update query var utility; add tests

--- wp-content/themes/core/assets/js/test/tests/utils/data/update-query-var.test.js
+++ wp-content/themes/core/assets/js/test/tests/utils/data/update-query-var.test.js
@@ -1,3 +1,4 @@
+import expect from 'expect';
 import updateQueryVar from 'utils/data/update-query-var';
 
 describe( 'updateQueryVar', () => {
@@ -27,6 +28,24 @@ describe( 'updateQueryVar', () => {
 		const key = 'param1';
 		const value = 'false';
 		const expectedUrl = 'http://localhost/?param1=false#hash';
+
+		expect( updateQueryVar( key, value, url ) ).toBe( expectedUrl );
+	} );
+
+	it( 'remove a key that has been set to undefined', () => {
+		const url = 'http://localhost/?param1=true&param2=1024';
+		const key = 'param1';
+		const value = undefined;
+		const expectedUrl = 'http://localhost/?param2=1024';
+
+		expect( updateQueryVar( key, value, url ) ).toBe( expectedUrl );
+	} );
+
+	it ( 'do not add a key that is set to undefined', () => {
+		const url = 'http://localhost/';
+		const key = 'param1';
+		const value = undefined;
+		const expectedUrl = 'http://localhost/';
 
 		expect( updateQueryVar( key, value, url ) ).toBe( expectedUrl );
 	} );
