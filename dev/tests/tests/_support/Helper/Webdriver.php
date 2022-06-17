3b21656dc86a4a2297f3317d2ecc5e541bfdde03
Justin Frydman <justin.frydman@gmail.com>
2022-06-17 15:38:20 -0600

    Add additional helper methods for the webdriver actor to use

--- dev/tests/tests/_support/Helper/Webdriver.php
+++ dev/tests/tests/_support/Helper/Webdriver.php
@@ -1,17 +1,32 @@
-<?php
+<?php declare(strict_types=1);
 
 namespace Helper;
 
 // here you can define custom actions
 // all public methods declared in helper class will be available in $I
 
+use Codeception\Module\WPWebDriver;
+use Codeception\TestInterface;
+
 class Webdriver extends \Codeception\Module {
 
+	private WPWebDriver $webdriver;
+
+	public function _before( TestInterface $test ) {
+		parent::_before( $test );
+
+		$this->webdriver = $this->getModule( 'WPWebDriver' );
+	}
+
 	public function shouldUseXdebug(): bool {
-		$webdriver      = $this->getModule( 'WPWebDriver' );
-		$xdebug_enabled = $webdriver->_getConfig( "xdebug_enabled" );
+		$xdebug_enabled = $this->webdriver->_getConfig( "xdebug_enabled" );
 
 		return (bool) $xdebug_enabled;
+	}
+
+	public function waitForPageLoad( int $timeout = 10 ) {
+		$this->webdriver->waitForJs( 'return document.readyState == "complete"', $timeout );
+		$this->webdriver->waitForJqueryAjax( $timeout );
 	}
 
 }
