9a20aec50314d6ea2bafb974c82d1e1d237c9b29
David Paul Ellenwood <davee@tri.be>
2022-06-20 18:46:14 -0500

    [SQONE-270]: Add Footer section to Customizer.

---
+++ wp-content/plugins/core/src/Theme_Customizer/Customizer_Sections/Footer.php
@@ -0,0 +1,27 @@
+<?php declare(strict_types=1);
+
+namespace Tribe\Project\Theme_Customizer\Customizer_Sections;
+
+/**
+ * Class Footer.
+ */
+class Footer extends Abstract_Setting {
+
+	// Keys.
+	public const FOOTER_SECTION = 'footer_section';
+
+	/**
+	 * Fluent title.
+	 *
+	 * @return $this
+	 */
+	public function section_title(): self {
+		$this->wp_customize->add_section( self::FOOTER_SECTION, [
+			'title'    => __( 'Footer', 'tribe' ),
+			'priority' => 170,
+		] );
+
+		return $this;
+	}
+
+}
