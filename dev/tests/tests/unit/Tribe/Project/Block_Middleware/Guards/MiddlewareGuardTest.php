98ca1586f6ab0a55139114e4e33f2fd56e515333
Justin Frydman <justin.frydman@gmail.com>
2022-07-05 16:10:16 -0600

    Add denial middleware guard tests

--- dev/tests/tests/unit/Tribe/Project/Block_Middleware/Guards/MiddlewareGuardTest.php
+++ dev/tests/tests/unit/Tribe/Project/Block_Middleware/Guards/MiddlewareGuardTest.php
@@ -34,6 +34,53 @@ final class MiddlewareGuardTest extends Unit {
 		$this->assertTrue( $guard->allowed( $blockTwo, $middleware ) );
 	}
 
+	public function test_it_denies_middleware_for_missing_blocks(): void {
+		$blockOne = new class extends Block_Config {
+			public function add_block() {
+			}
+		};
+
+		$blockTwo = new class extends Block_Config {
+			public function add_block() {
+			}
+		};
+
+		$middleware = new class implements Middleware {};
+
+		$allowed_items = [
+			get_class( $blockOne ) => true,
+		];
+
+		$guard = new Middleware_Guard( new Map( $allowed_items ) );
+
+		$this->assertTrue( $guard->allowed( $blockOne, $middleware ) );
+		$this->assertFalse( $guard->allowed( $blockTwo, $middleware ) );
+	}
+
+	public function test_it_explicitly_denies_middleware_for_blocks(): void {
+		$blockOne = new class extends Block_Config {
+			public function add_block() {
+			}
+		};
+
+		$blockTwo = new class extends Block_Config {
+			public function add_block() {
+			}
+		};
+
+		$middleware = new class implements Middleware {};
+
+		$allowed_items = [
+			get_class( $blockOne ) => false,
+			get_class( $blockTwo ) => true,
+		];
+
+		$guard = new Middleware_Guard( new Map( $allowed_items ) );
+
+		$this->assertFalse( $guard->allowed( $blockOne, $middleware ) );
+		$this->assertTrue( $guard->allowed( $blockTwo, $middleware ) );
+	}
+
 	public function test_it_allows_specific_middleware_for_blocks(): void {
 		$blockOne = new class extends Block_Config {
 			public function add_block() {
@@ -142,5 +189,71 @@ final class MiddlewareGuardTest extends Unit {
 		$this->assertFalse( $guard->allowed( $modelTwo, $allowed_middleware_model_one ) );
 	}
 
+	public function test_it_denies_block_model_middleware_for_missing_blocks(): void {
+		$modelOne = new class implements Model {
+			public function set_data( array $data ): Model {
+				return $this;
+			}
+
+			public function get_data(): array {
+				return [];
+			}
+		};
+
+		$modelTwo = new class implements Model {
+			public function set_data( array $data ): Model {
+				return $this;
+			}
+
+			public function get_data(): array {
+				return [];
+			}
+		};
+
+		$middleware = new class implements Middleware {};
+
+		$allowed_items = [
+			get_class( $modelOne ) => true,
+		];
+
+		$guard = new Middleware_Guard( new Map( $allowed_items ) );
+
+		$this->assertTrue( $guard->allowed( $modelOne, $middleware ) );
+		$this->assertFalse( $guard->allowed( $modelTwo, $middleware ) );
+	}
+
+	public function test_it_explicitly_denies_block_model_middleware_for_blocks(): void {
+		$modelOne = new class implements Model {
+			public function set_data( array $data ): Model {
+				return $this;
+			}
+
+			public function get_data(): array {
+				return [];
+			}
+		};
+
+		$modelTwo = new class implements Model {
+			public function set_data( array $data ): Model {
+				return $this;
+			}
+
+			public function get_data(): array {
+				return [];
+			}
+		};
+
+		$middleware = new class implements Middleware {};
+
+		$allowed_items = [
+			get_class( $modelOne ) => false,
+			get_class( $modelTwo ) => true,
+		];
+
+		$guard = new Middleware_Guard( new Map( $allowed_items ) );
+
+		$this->assertFalse( $guard->allowed( $modelOne, $middleware ) );
+		$this->assertTrue( $guard->allowed( $modelTwo, $middleware ) );
+	}
 
 }
