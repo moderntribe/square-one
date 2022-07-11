002e6c65b7384647a3eb4459677c047bab46d62b
Justin Frydman <justin.frydman@gmail.com>
2022-07-11 11:06:55 -0600

    Update color theme middleware to implement the field interface

--- wp-content/plugins/core/src/Blocks/Middleware/Color_Theme/Field_Middleware/Color_Theme_Field_Middleware.php
+++ wp-content/plugins/core/src/Blocks/Middleware/Color_Theme/Field_Middleware/Color_Theme_Field_Middleware.php
@@ -7,9 +7,10 @@ use Tribe\Libs\ACF\Field;
 use Tribe\Libs\ACF\Field_Section;
 use Tribe\Project\Block_Middleware\Contracts\Abstract_Field_Middleware;
 use Tribe\Project\Blocks\Middleware\Color_Theme\Contracts\Appearance;
+use Tribe\Project\Blocks\Middleware\Color_Theme\Contracts\Color_Theme_Field;
 use Tribe\Project\Blocks\Middleware\Color_Theme\Traits\With_Color_Choices;
 
-class Color_Theme_Field_Middleware extends Abstract_Field_Middleware implements Appearance {
+class Color_Theme_Field_Middleware extends Abstract_Field_Middleware implements Appearance, Color_Theme_Field {
 
 	use With_Color_Choices;
 
@@ -27,15 +28,19 @@ class Color_Theme_Field_Middleware extends Abstract_Field_Middleware implements 
 	 */
 	public function set_fields( Block_Config $block, array $params = [] ): Block_Config {
 		return $block->add_field( new Field_Section( self::SECTION, esc_html__( 'Color Theme', 'tribe' ), 'accordion' ) )
-			  ->add_field( new Field( self::NAME . '_' . self::COLOR_THEME, [
-				  'type'          => 'swatch',
-				  'name'          => self::COLOR_THEME,
-				  'label'         => '',
-				  'default_value' => self::THEME_DEFAULT,
-				  'allow_null'    => false,
-				  'allow_other'   => false,
-				  'choices'       => $this->get_color_theme_choices(),
-			  ] ) );
+			  ->add_field( $this->get_color_theme_field() );
+	}
+
+	public function get_color_theme_field( string $label = '' ): Field {
+		return new Field( self::NAME . '_' . self::COLOR_THEME, [
+			'type'          => 'swatch',
+			'name'          => self::COLOR_THEME,
+			'label'         => $label,
+			'default_value' => self::THEME_DEFAULT,
+			'allow_null'    => false,
+			'allow_other'   => false,
+			'choices'       => $this->get_color_theme_choices(),
+		] );
 	}
 
 }
