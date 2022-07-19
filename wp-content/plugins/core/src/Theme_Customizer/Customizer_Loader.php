9871701d89bd0613885d2e103269171af38b1d95
David Paul Ellenwood <davee@tri.be>
2022-07-19 14:14:32 -0500

    [SQONE-270]: Remove old Footer Customizer settings.

--- wp-content/plugins/core/src/Theme_Customizer/Customizer_Loader.php
+++ wp-content/plugins/core/src/Theme_Customizer/Customizer_Loader.php
@@ -2,8 +2,6 @@
 
 namespace Tribe\Project\Theme_Customizer;
 
-use Tribe\Project\Theme_Customizer\Customizer_Sections\Footer_Settings;
-
 /**
  * @package Tribe\Project\Theme_Customizer
  */
@@ -19,14 +17,6 @@ class Customizer_Loader {
 	 * @action customize_register
 	 */
 	public function register_customizer_controls( \WP_Customize_Manager $wp_customize ): void {
-		$this->footer_section( $wp_customize );
-	}
-
-	protected function footer_section( \WP_Customize_Manager $wp_customize ): void {
-		$footer = new Footer_Settings( $wp_customize );
-		$footer->section_title()
-			->field_footer_logo()
-			->field_footer_description();
 	}
 
 }
