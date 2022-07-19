3b0323f30ddd821e312345a5e439e89792eeac7d
David Paul Ellenwood <davee@tri.be>
2022-07-19 12:33:06 -0500

    [SQONE-270]: Remove unused Analytics and Social Media Customizer sections.

--- wp-content/plugins/core/src/Theme_Customizer/Customizer_Loader.php
+++ wp-content/plugins/core/src/Theme_Customizer/Customizer_Loader.php
@@ -2,9 +2,7 @@
 
 namespace Tribe\Project\Theme_Customizer;
 
-use Tribe\Project\Theme_Customizer\Customizer_Sections\Analytics_Settings;
 use Tribe\Project\Theme_Customizer\Customizer_Sections\Footer_Settings;
-use Tribe\Project\Theme_Customizer\Customizer_Sections\Social_Follow_Settings;
 
 /**
  * @package Tribe\Project\Theme_Customizer
@@ -21,15 +19,7 @@ class Customizer_Loader {
 	 * @action customize_register
 	 */
 	public function register_customizer_controls( \WP_Customize_Manager $wp_customize ): void {
-		$this->analytics_section( $wp_customize );
-		$this->social_follow_section( $wp_customize );
 		$this->footer_section( $wp_customize );
-	}
-
-	protected function analytics_section( \WP_Customize_Manager $wp_customize ): void {
-		$analytics = new Analytics_Settings( $wp_customize );
-		$analytics->section_title()
-				  ->field_gtm_id();
 	}
 
 	protected function footer_section( \WP_Customize_Manager $wp_customize ): void {
@@ -37,12 +27,6 @@ class Customizer_Loader {
 		$footer->section_title()
 			->field_footer_logo()
 			->field_footer_description();
-	}
-
-	protected function social_follow_section( \WP_Customize_Manager $wp_customize ): void {
-		$social = new Social_Follow_Settings( $wp_customize );
-		$social->section_title()
-			   ->field_all_links();
 	}
 
 }
