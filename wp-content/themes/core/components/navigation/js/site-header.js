a37ac7c1a1429ae37dd562bbe1125b05e6257044
David Paul Ellenwood <davee@tri.be>
2022-08-09 22:48:37 -0500

    [SQONE-519]: adding function comments.

--- wp-content/themes/core/components/navigation/js/site-header.js
+++ wp-content/themes/core/components/navigation/js/site-header.js
@@ -23,6 +23,11 @@ const options = {
 	submenuRevealSpeed: 250,
 };
 
+/**
+ * Close a submenu immediately, without animating the close interaction.
+ *
+ * @param trigger	The button which triggered the click event
+ */
 const closeSubmenuFast = ( trigger ) => {
 	const listItem = trigger.closest( '.c-nav-primary__list-item--has-children' );
 	const submenu = listItem.querySelector( '[data-js="c-nav-child-menu"]' );
@@ -33,6 +38,9 @@ const closeSubmenuFast = ( trigger ) => {
 	state.submenuOpen = false;
 };
 
+/**
+ * Close all submenus
+ */
 const closeAllSubmenus = () => {
 	el.subMenuToggles.forEach( ( trigger ) => {
 		if ( trigger.getAttribute( 'aria-expanded' ) === 'true' ) {
@@ -41,6 +49,13 @@ const closeAllSubmenus = () => {
 	} );
 };
 
+/**
+ * Close a submenu
+ *
+ * @param trigger	The button which triggered the click event
+ * @param listItem	The parent list that contains the trigger and submenu list
+ * @param submenu	The child list of menu items
+ */
 const closeSubmenu = ( trigger, listItem, submenu ) => {
 	slide.up( submenu, submenu.id );
 	listItem.classList.remove( 'c-nav-primary__list-child--active' );
@@ -52,6 +67,13 @@ const closeSubmenu = ( trigger, listItem, submenu ) => {
 	}, options.submenuRevealSpeed );
 };
 
+/**
+ * Open a submenu
+ *
+ * @param trigger	The button which triggered the click event
+ * @param listItem	The parent list that contains the trigger and submenu list
+ * @param submenu	The child list of menu items
+ */
 const openSubmenu = ( trigger, listItem, submenu ) => {
 	if ( globalState.v_width >= globalOptions.FULL_BREAKPOINT ) {
 		closeAllSubmenus();
@@ -67,6 +89,11 @@ const openSubmenu = ( trigger, listItem, submenu ) => {
 	}, 25 );
 };
 
+/**
+ * Toggle a submenu on click
+ *
+ * @param e
+ */
 const toggleSubmenu = ( e ) => {
 	const trigger = e.target;
 	const listItem = trigger.closest( '.c-nav-primary__list-item--has-children' );
@@ -76,16 +103,22 @@ const toggleSubmenu = ( e ) => {
 		: openSubmenu( trigger, listItem, submenu );
 };
 
+/**
+ * Handle keyboard events for the submenus
+ *
+ * @param e
+ */
 const handleSubmenuKeyEvents = ( e ) => {
 	if ( e.key !== 'Escape' && e.key !== 'Tab' ) {
 		return;
 	}
 
-	if ( ! state.submenuOpen ) {
+	// Only applies for desktop viewports
+	if ( globalState.v_width < globalOptions.FULL_BREAKPOINT ) {
 		return;
 	}
 
-	if ( globalState.v_width < globalOptions.FULL_BREAKPOINT ) {
+	if ( ! state.submenuOpen ) {
 		return;
 	}
 
@@ -93,23 +126,35 @@ const handleSubmenuKeyEvents = ( e ) => {
 	const trigger = listItem.querySelector( '[data-js="c-nav-child-menu-trigger"]' );
 	const submenu = listItem.querySelector( '[data-js="c-nav-child-menu"]' );
 
+	// Handle Escape
 	if ( e.key === 'Escape' ) {
 		closeSubmenuFast( trigger );
 		trigger.focus();
 	}
 
+	// Handle Tab
 	if ( e.key === 'Tab' && ! listItem.contains( document.activeElement ) ) {
 		closeSubmenu( trigger, listItem, submenu );
 	}
 };
 
+/**
+ * Close any open submenu when clicking outside it.
+ * @param e
+ */
 const handleSubmenuClickOut = ( e ) => {
+	// Only applies for desktop viewports
+	if ( globalState.v_width < globalOptions.FULL_BREAKPOINT ) {
+		return;
+	}
+
 	if ( ! state.submenuOpen ) {
 		return;
 	}
 
 	const listItem = el.navContainer.querySelector( '.c-nav-primary__list-child--active' );
 
+	// if the click is already closing the submenu, there is no active submenu so bail.
 	if ( ! listItem ) {
 		return;
 	}
@@ -121,6 +166,10 @@ const handleSubmenuClickOut = ( e ) => {
 	}
 };
 
+/**
+ * Handle keyboard events for the flyout menu
+ * @param e
+ */
 const handleNavFlyoutKeyboardEvents = ( e ) => {
 	if ( globalState.v_width >= globalOptions.FULL_BREAKPOINT ) {
 		return;
@@ -130,6 +179,9 @@ const handleNavFlyoutKeyboardEvents = ( e ) => {
 	a11y.focusLoop( e, el.navToggle, el.container, closeNavFlyout );
 };
 
+/**
+ * Close the navigation flyout menu
+ */
 const closeNavFlyout = () => {
 	closeAllSubmenus();
 	el.container.classList.remove( 'c-site-header--nav-active' );
@@ -146,6 +198,9 @@ const closeNavFlyout = () => {
 	}, options.navRevealSpeed );
 };
 
+/**
+ * Open the navigation flyout menu
+ */
 const openNavFlyout = () => {
 	closeAllSubmenus();
 	window.scrollTo( 0, 0 );
@@ -165,6 +220,9 @@ const openNavFlyout = () => {
 	}, options.navRevealSpeed );
 };
 
+/**
+ * Toggle the navigation flyout menu
+ */
 const toggleNavFlyout = () => {
 	if ( state.navAnimating ) {
 		return;
@@ -184,7 +242,7 @@ const executeResize = () => {
 };
 
 /**
- * @function bindEvents
+ * Bind events for this module
  */
 const bindEvents = () => {
 	delegate( el.navContainer, '[data-js="c-nav-child-menu-trigger"]', 'click', toggleSubmenu );
