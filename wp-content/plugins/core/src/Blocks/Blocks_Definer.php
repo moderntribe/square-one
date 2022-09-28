35498196fc15ac107b4d31a36f76b64ee3b58a49
Jamie Paul <jamie@tri.be>
2022-09-28 16:04:59 -0500

    enable WP cover block

--- wp-content/plugins/core/src/Blocks/Blocks_Definer.php
+++ wp-content/plugins/core/src/Blocks/Blocks_Definer.php
@@ -67,7 +67,6 @@ class Blocks_Definer implements Definer_Interface {
 				'core/buttons',
 				'core/calendar',
 				'core/categories',
-				'core/cover',
 				'core/gallery',
 				'core/html',
 				'core/latest-comments',
